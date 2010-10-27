7cda36b5e35aee3cf1b27c64f502aae79ff3003e
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2010-10-27 13:43:54 +0000

    [dopar/encpar] Added process number control through environment variables and multiple occurences of the argument within the pattern.

--- trunk/dopar/encpar.py
+++ trunk/dopar/encpar.py
@@ -8,6 +8,9 @@ from Queue import Queue
 NUM_PROC = 2
 COMMAND_PATTERN = 'nice lame -h -b 64 "%s"'
 
+# example of multiple occurences of the pattern:
+# COMMAND_PATTERN = 'nice lame -q 1 -b 64 "%s"; mv "%s".mp3 "%s"'
+
 class EncThread(threading.Thread):
     def __init__(self, queue):
         threading.Thread.__init__(self)
@@ -15,10 +18,19 @@ class EncThread(threading.Thread):
     def run(self):
         while not self.queue.empty():
             tgt = self.queue.get()
+            # support for multiple occurences in the pattern
+            format_tuple = (tgt,) * (len(COMMAND_PATTERN.split("%s"))-1)
             print "Started",tgt
-            os.popen(COMMAND_PATTERN % tgt)
-            print "Finished",tgt
+            os.popen(COMMAND_PATTERN % format_tuple)
+            print "Finished", tgt
 
+def read_env():
+    """Change working mode base on environment variables."""
+    global COMMAND_PATTERN, NUM_PROC
+    COMMAND_PATTERN = os.getenv('COMMAND_PATTERN', COMMAND_PATTERN)
+    NUM_PROC = int(os.getenv('NUM_PROC', NUM_PROC))
+    print (COMMAND_PATTERN, NUM_PROC)
+    
 def process_files(files):
     """Run COMMAND_PATTERN for its arguments using NUM_PROC threads."""
     q = Queue()
@@ -30,5 +42,6 @@ def process_files(files):
     map(lambda p: p.join(), t)
 
 if __name__=='__main__':
+    read_env()
     process_files(sys.argv[1:])
     
