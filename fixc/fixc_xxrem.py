ce9c7e264335ed9fa3dba7026f55cd36e77d21b2
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2010-10-29 12:20:44 +0000

    [fixc] Updated other fixers to work inplace, like fixc_includes

--- fixc/fixc_xxrem.py
+++ fixc/fixc_xxrem.py
@@ -3,21 +3,24 @@
 import sys
 import re
 
-# determine the input
-try:
-    source = open(sys.argv[1])
-except:
-    source = sys.stdin
-
 # catch lines with trailing //-comments
-line = re.compile("([\t ]*)([^ \t].*)(// ?)([^\r\n]*)([\r\n]*)")
+line = re.compile("^([\t ]*)([^ \t].*)(// ?)([^\r\n]*)(\r?\n?)", re.M)
 
-# move to preceding line
-for l in source.readlines():
-    m = line.match(l)
-    if m:
-        sys.stdout.write(
-            "%s// %s%s%s%s%s" % tuple(m.group(g) for g in (1,4,5,1,2,5)))
-    else:
-        sys.stdout.write(l)
+def process_line_comments(bulk):
+    # move to preceding line
+    return line.sub(lambda m: "%s// %s%s%s%s%s" % tuple(m.group(g) for g in (1,4,5,1,2,5)), bulk)
+
+# determine the input
+
+if len(sys.argv) < 2:
+    sys.stdout.write(process_line_comments(sys.stdin.read()))
+else:
+    for f in sys.argv[1:]:
+        data = open(f).read()
+        changed = process_line_comments(data)
+        if data <> changed:
+            open(f+"~", "w").write(data)
+            open(f, "w").write(changed)
+
+        
 
