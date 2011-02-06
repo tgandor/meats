65f4178223c3b664708cdd1f653c9ebf2fbc5e8b
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2011-02-06 23:35:27 +0000

    wishlist tweaks

--- installer/wishlist.pl
+++ installer/wishlist.pl
@@ -10,41 +10,45 @@ while(<DATA>) {
 print "$comm\n";
 system($comm);
 
-$others = "
-php5 
-php-pear  
-youtube-dl
-";
-
 __DATA__
 
-vim clisp emacs 
+vim emacs 
+eclipse 
+kile texlive-lang-polish texlive-doc-pl
+texlive-fonts-extra
+ttf-mscorefonts-installer
 
 mc kdf aptitude kdesdk-scripts
-
+p7zip-full p7zip-rar unrar rar
+openvpn openssh-server
 numlockx
 
 subversion mercurial git-core
 
 idle python-numpy python-scipy python-matplotlib python-mpi
-python-argparse 
-python-numpy-doc python-argparse-doc python-matplotlib-doc
-python-doc
+python-matplotlib-doc python-doc python-docutils
+python-argparse python-argparse-doc
 
-eclipse 
+jython jython-doc
+clisp clisp-doc
+perl-doc
+
+mpich2 mpich2-doc mpichpython
+
+octave3.2 gnuplot qtoctave
+gnuplot-doc
 
 mplayer mencoder
-
-p7zip-full p7zip-rar unrar rar
-
-perl-doc doxygen doxygen-doc doxygen-gui graphviz
-
-openvpn openssh-server
-
-ttf-mscorefonts-installer
-
 dvdrip dvdrip-doc
 
-octave3.2
-
+chromium-browser 
 gimp inkscape
+audacity
+kteatime
+
+wine
+
+gnubik childsplay chromium-bsu tuxmath tuxpaint tuxtype
+lletters 
+
+
