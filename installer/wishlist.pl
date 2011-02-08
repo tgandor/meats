3fcb3bc0cb67af9a891dbf4294e066dd8ffff95f
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2011-02-08 23:22:24 +0000

    even more wishes in wishlist

--- installer/wishlist.pl
+++ installer/wishlist.pl
@@ -18,16 +18,21 @@ kile texlive-lang-polish texlive-doc-pl
 texlive-fonts-extra
 ttf-mscorefonts-installer
 
-mc kdf aptitude kdesdk-scripts
+mc kdf aptitude apt-rdepends kdesdk-scripts
 p7zip-full p7zip-rar unrar rar
 openvpn openssh-server
-numlockx
+numlockx cpufrequtils
 
-subversion mercurial git-core
+subversion mercurial git-core cvsnt
 
 idle python-numpy python-scipy python-matplotlib python-mpi
 python-matplotlib-doc python-doc python-docutils
 python-argparse python-argparse-doc
+python-profiler pylint graphviz
+python-pyrex pyrex-mode
+python-dialog
+swig swig-doc cableswig python-dev
+doxygen doxygen-doc doxygen-gui
 
 jython jython-doc
 clisp clisp-doc
