58d8160422d55f973a361e9aac5f311486f02ea1
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2011-03-24 10:54:04 +0000

    adding qt development stuff to wishlist

--- trunk/installer/wishlist.pl
+++ trunk/installer/wishlist.pl
@@ -18,7 +18,7 @@ kile texlive-lang-polish texlive-doc-pl
 texlive-fonts-extra
 ttf-mscorefonts-installer
 
-mc kdf aptitude apt-rdepends kdesdk-scripts
+mc kdf aptitude apt-rdepends kdesdk-scripts fdupes
 p7zip-full p7zip-rar unrar rar
 openvpn openssh-server
 numlockx cpufrequtils
@@ -29,10 +29,12 @@ idle python-numpy python-scipy python-matplotlib python-mpi
 python-matplotlib-doc python-doc python-docutils
 python-argparse python-argparse-doc
 python-profiler pylint graphviz
+python-codespeak-lib
 python-pyrex pyrex-mode
 python-dialog
 swig swig-doc cableswig python-dev
 doxygen doxygen-doc doxygen-gui
+indent
 
 jython jython-doc
 clisp clisp-doc
@@ -40,14 +42,17 @@ perl-doc
 
 mpich2 mpich2-doc mpichpython
 
-octave3.2 gnuplot qtoctave
+qt4-dev-tools qt4-designer qt4-doc-html
+pyqt4-dev-tools
+
+octave3.2 gnuplot qtoctave ipython
 gnuplot-doc
 
-mplayer mencoder
+mplayer mencoder vlc
 dvdrip dvdrip-doc
 
 chromium-browser 
-gimp inkscape
+gimp inkscape simple-scan xsane
 audacity
 kteatime
 
@@ -55,5 +60,4 @@ wine
 
 gnubik childsplay chromium-bsu tuxmath tuxpaint tuxtype
 lletters 
-
 
