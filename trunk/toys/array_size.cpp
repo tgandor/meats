363c33f49bb11836e86313e78aaacb730e38e4a2
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2011-03-29 12:49:37 +0000

    a programming example - array size aware function template

---
+++ trunk/toys/array_size.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+template <int N> 
+void array_info(char (&c)[N]) {
+    cout << "Array is " << N << " elements long." << endl;
+}
+
+int main() {
+  char a[4], b[10];
+  array_info(a);
+  array_info(b);
+  return 0;
+}
