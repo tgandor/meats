83cf292d578981a78e7cd14d4cc62d56acb06fa7
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2011-04-27 14:18:30 +0000

    VCardImport - just starting a contact importer. may never finish...

---
+++ trunk/VCardImport/src/com/googlecode/meats/VCardImport.java
@@ -0,0 +1,292 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.googlecode.meats;
+
+import javax.microedition.midlet.*;
+import javax.microedition.lcdui.*;
+import javax.microedition.pim.PIM;
+import org.netbeans.microedition.lcdui.WaitScreen;
+import org.netbeans.microedition.lcdui.pda.FileBrowser;
+import org.netbeans.microedition.lcdui.pda.PIMBrowser;
+import org.netbeans.microedition.util.SimpleCancellableTask;
+
+/**
+ * @author olaija
+ */
+public class VCardImport extends MIDlet implements CommandListener {
+
+    private boolean midletPaused = false;
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Fields ">//GEN-BEGIN:|fields|0|
+    private Command exitCommand;
+    private Command okCommand;
+    private Form welcome;
+    private StringItem stringItem;
+    private FileBrowser fileBrowser;
+    private Alert alert;
+    private SimpleCancellableTask task;
+    //</editor-fold>//GEN-END:|fields|0|
+
+    /**
+     * The VCardImport constructor.
+     */
+    public VCardImport() {
+    }
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Methods ">//GEN-BEGIN:|methods|0|
+    //</editor-fold>//GEN-END:|methods|0|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: initialize ">//GEN-BEGIN:|0-initialize|0|0-preInitialize
+    /**
+     * Initilizes the application.
+     * It is called only once when the MIDlet is started. The method is called before the <code>startMIDlet</code> method.
+     */
+    private void initialize() {//GEN-END:|0-initialize|0|0-preInitialize
+        // write pre-initialize user code here
+//GEN-LINE:|0-initialize|1|0-postInitialize
+        // write post-initialize user code here
+    }//GEN-BEGIN:|0-initialize|2|
+    //</editor-fold>//GEN-END:|0-initialize|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: startMIDlet ">//GEN-BEGIN:|3-startMIDlet|0|3-preAction
+    /**
+     * Performs an action assigned to the Mobile Device - MIDlet Started point.
+     */
+    public void startMIDlet() {//GEN-END:|3-startMIDlet|0|3-preAction
+        // write pre-action user code here
+        switchDisplayable(null, getWelcome());//GEN-LINE:|3-startMIDlet|1|3-postAction
+        // write post-action user code here
+    }//GEN-BEGIN:|3-startMIDlet|2|
+    //</editor-fold>//GEN-END:|3-startMIDlet|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: resumeMIDlet ">//GEN-BEGIN:|4-resumeMIDlet|0|4-preAction
+    /**
+     * Performs an action assigned to the Mobile Device - MIDlet Resumed point.
+     */
+    public void resumeMIDlet() {//GEN-END:|4-resumeMIDlet|0|4-preAction
+        // write pre-action user code here
+//GEN-LINE:|4-resumeMIDlet|1|4-postAction
+        // write post-action user code here
+    }//GEN-BEGIN:|4-resumeMIDlet|2|
+    //</editor-fold>//GEN-END:|4-resumeMIDlet|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: switchDisplayable ">//GEN-BEGIN:|5-switchDisplayable|0|5-preSwitch
+    /**
+     * Switches a current displayable in a display. The <code>display</code> instance is taken from <code>getDisplay</code> method. This method is used by all actions in the design for switching displayable.
+     * @param alert the Alert which is temporarily set to the display; if <code>null</code>, then <code>nextDisplayable</code> is set immediately
+     * @param nextDisplayable the Displayable to be set
+     */
+    public void switchDisplayable(Alert alert, Displayable nextDisplayable) {//GEN-END:|5-switchDisplayable|0|5-preSwitch
+        // write pre-switch user code here
+        Display display = getDisplay();//GEN-BEGIN:|5-switchDisplayable|1|5-postSwitch
+        if (alert == null) {
+            display.setCurrent(nextDisplayable);
+        } else {
+            display.setCurrent(alert, nextDisplayable);
+        }//GEN-END:|5-switchDisplayable|1|5-postSwitch
+        // write post-switch user code here
+    }//GEN-BEGIN:|5-switchDisplayable|2|
+    //</editor-fold>//GEN-END:|5-switchDisplayable|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: commandAction for Displayables ">//GEN-BEGIN:|7-commandAction|0|7-preCommandAction
+    /**
+     * Called by a system to indicated that a command has been invoked on a particular displayable.
+     * @param command the Command that was invoked
+     * @param displayable the Displayable where the command was invoked
+     */
+    public void commandAction(Command command, Displayable displayable) {//GEN-END:|7-commandAction|0|7-preCommandAction
+        // write pre-action user code here
+        if (displayable == fileBrowser) {//GEN-BEGIN:|7-commandAction|1|24-preAction
+            if (command == FileBrowser.SELECT_FILE_COMMAND) {//GEN-END:|7-commandAction|1|24-preAction
+                // write pre-action user code here
+                switchDisplayable(getAlert(), getWelcome());//GEN-LINE:|7-commandAction|2|24-postAction
+                // write post-action user code here
+            }//GEN-BEGIN:|7-commandAction|3|19-preAction
+        } else if (displayable == welcome) {
+            if (command == exitCommand) {//GEN-END:|7-commandAction|3|19-preAction
+                // write pre-action user code here
+                exitMIDlet();//GEN-LINE:|7-commandAction|4|19-postAction
+                // write post-action user code here
+            } else if (command == okCommand) {//GEN-LINE:|7-commandAction|5|29-preAction
+                // write pre-action user code here
+                switchDisplayable(null, getFileBrowser());//GEN-LINE:|7-commandAction|6|29-postAction
+                // write post-action user code here
+            }//GEN-BEGIN:|7-commandAction|7|7-postCommandAction
+        }//GEN-END:|7-commandAction|7|7-postCommandAction
+        // write post-action user code here
+    }//GEN-BEGIN:|7-commandAction|8|
+    //</editor-fold>//GEN-END:|7-commandAction|8|
+
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: exitCommand ">//GEN-BEGIN:|18-getter|0|18-preInit
+    /**
+     * Returns an initiliazed instance of exitCommand component.
+     * @return the initialized component instance
+     */
+    public Command getExitCommand() {
+        if (exitCommand == null) {//GEN-END:|18-getter|0|18-preInit
+            // write pre-init user code here
+            exitCommand = new Command("Exit", Command.EXIT, 0);//GEN-LINE:|18-getter|1|18-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|18-getter|2|
+        return exitCommand;
+    }
+    //</editor-fold>//GEN-END:|18-getter|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: welcome ">//GEN-BEGIN:|14-getter|0|14-preInit
+    /**
+     * Returns an initiliazed instance of welcome component.
+     * @return the initialized component instance
+     */
+    public Form getWelcome() {
+        if (welcome == null) {//GEN-END:|14-getter|0|14-preInit
+            // write pre-init user code here
+            welcome = new Form("Welcome", new Item[] { getStringItem() });//GEN-BEGIN:|14-getter|1|14-postInit
+            welcome.addCommand(getExitCommand());
+            welcome.addCommand(getOkCommand());
+            welcome.setCommandListener(this);//GEN-END:|14-getter|1|14-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|14-getter|2|
+        return welcome;
+    }
+    //</editor-fold>//GEN-END:|14-getter|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: stringItem ">//GEN-BEGIN:|16-getter|0|16-preInit
+    /**
+     * Returns an initiliazed instance of stringItem component.
+     * @return the initialized component instance
+     */
+    public StringItem getStringItem() {
+        if (stringItem == null) {//GEN-END:|16-getter|0|16-preInit
+            // write pre-init user code here
+            stringItem = new StringItem("Info", "\nPress OK to pick a file and read its first line. \nThe import of vcf is not yet fully supported ;).");//GEN-LINE:|16-getter|1|16-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|16-getter|2|
+        return stringItem;
+    }
+    //</editor-fold>//GEN-END:|16-getter|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: okCommand ">//GEN-BEGIN:|28-getter|0|28-preInit
+    /**
+     * Returns an initiliazed instance of okCommand component.
+     * @return the initialized component instance
+     */
+    public Command getOkCommand() {
+        if (okCommand == null) {//GEN-END:|28-getter|0|28-preInit
+            // write pre-init user code here
+            okCommand = new Command("Ok", Command.OK, 0);//GEN-LINE:|28-getter|1|28-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|28-getter|2|
+        return okCommand;
+    }
+    //</editor-fold>//GEN-END:|28-getter|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: fileBrowser ">//GEN-BEGIN:|22-getter|0|22-preInit
+    /**
+     * Returns an initiliazed instance of fileBrowser component.
+     * @return the initialized component instance
+     */
+    public FileBrowser getFileBrowser() {
+        if (fileBrowser == null) {//GEN-END:|22-getter|0|22-preInit
+            // write pre-init user code here
+            fileBrowser = new FileBrowser(getDisplay());//GEN-BEGIN:|22-getter|1|22-postInit
+            fileBrowser.setTitle("fileBrowser");
+            fileBrowser.setCommandListener(this);
+            fileBrowser.addCommand(FileBrowser.SELECT_FILE_COMMAND);//GEN-END:|22-getter|1|22-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|22-getter|2|
+        return fileBrowser;
+    }
+    //</editor-fold>//GEN-END:|22-getter|2|
+
+
+
+
+
+
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: task ">//GEN-BEGIN:|39-getter|0|39-preInit
+    /**
+     * Returns an initiliazed instance of task component.
+     * @return the initialized component instance
+     */
+    public SimpleCancellableTask getTask() {
+        if (task == null) {//GEN-END:|39-getter|0|39-preInit
+            // write pre-init user code here
+            task = new SimpleCancellableTask();//GEN-BEGIN:|39-getter|1|39-execute
+            task.setExecutable(new org.netbeans.microedition.util.Executable() {
+                public void execute() throws Exception {//GEN-END:|39-getter|1|39-execute
+                    // write task-execution user code here
+                }//GEN-BEGIN:|39-getter|2|39-postInit
+            });//GEN-END:|39-getter|2|39-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|39-getter|3|
+        return task;
+    }
+    //</editor-fold>//GEN-END:|39-getter|3|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: alert ">//GEN-BEGIN:|48-getter|0|48-preInit
+    /**
+     * Returns an initiliazed instance of alert component.
+     * @return the initialized component instance
+     */
+    public Alert getAlert() {
+        if (alert == null) {//GEN-END:|48-getter|0|48-preInit
+            // write pre-init user code here
+            alert = new Alert("Read line:", new VCardParser(fileBrowser.getSelectedFileURL()).readline(), null, null);//GEN-BEGIN:|48-getter|1|48-postInit
+            alert.setTimeout(Alert.FOREVER);//GEN-END:|48-getter|1|48-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|48-getter|2|
+        return alert;
+    }
+    //</editor-fold>//GEN-END:|48-getter|2|
+
+    /**
+     * Returns a display instance.
+     * @return the display instance.
+     */
+    public Display getDisplay () {
+        return Display.getDisplay(this);
+    }
+
+    /**
+     * Exits MIDlet.
+     */
+    public void exitMIDlet() {
+        switchDisplayable (null, null);
+        destroyApp(true);
+        notifyDestroyed();
+    }
+
+    /**
+     * Called when MIDlet is started.
+     * Checks whether the MIDlet have been already started and initialize/starts or resumes the MIDlet.
+     */
+    public void startApp() {
+        if (midletPaused) {
+            resumeMIDlet ();
+        } else {
+            initialize ();
+            startMIDlet ();
+        }
+        midletPaused = false;
+    }
+
+    /**
+     * Called when MIDlet is paused.
+     */
+    public void pauseApp() {
+        midletPaused = true;
+    }
+
+    /**
+     * Called to signal the MIDlet to terminate.
+     * @param unconditional if true, then the MIDlet has to be unconditionally terminated and all resources has to be released.
+     */
+    public void destroyApp(boolean unconditional) {
+    }
+
+}
