58c623d1a99874dd55be114d9310047006bf68f6
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2011-06-02 22:18:59 +0000

    utfproxy - primitive proxying with latin2 -> utf8 conversion for HTML

---
+++ trunk/utfproxy/index.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import cgi
+import cgitb; cgitb.enable()
+import os
+import urllib2
+import sys
+
+requested = os.getenv("PATH_INFO") or '/'
+
+try:
+    base = open('config.ini').read().strip()
+except:
+    print "Content-type: text/plain"
+    print
+    print "Error: create a file config.ini with the base server URL as only content."
+    exit()
+    
+url = base + requested
+resp = urllib2.urlopen(urllib2.Request(url))
+contype = resp.info()['Content-Type']
+del resp.info()['transfer-encoding']
+
+
+
+if contype.startswith('text/html'):
+    data = resp.read()
+    data = data.replace('URL=/', 'URL=')
+    data = data.replace('iso-8859-2', 'UTF-8')
+    print resp.info()
+    sys.stdout.write(data.decode('ISO-8859-2').encode('UTF-8'))
+else:
+    print resp.info()
+    sys.stdout.write(resp.read())
+
+"""
+print "Content-type: text/plain"
+print
+print "Hi, there"
+print resp.info()
+print dir(resp.info())
+print resp.info().dict
+
+"""
