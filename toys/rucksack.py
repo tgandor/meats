03a0f5ff5c25784cf7f4c92a616f80698a5b854a
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2012-02-28 10:43:49 +0000

    rucksack.py - your shopping assistant

---
+++ toys/rucksack.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import sys
+
+if len(sys.argv) < 3:
+    print sys.argv, 'Usage: %s limit item1 ... itemN' % sys.argv[0]
+    exit()
+
+limit = float(sys.argv[1])
+items = sorted(set(map(float, sys.argv[2:])))
+
+sums = {0: []}
+surpassed = set()
+
+print limit, items
+
+for item in items:
+    while True:
+        changes = False
+        for s in sums.keys()[:]:
+            if item+s <= limit:
+                surpassed.add(s)
+                if item+s not in sums:
+                    sums[item+s] = sums[s]+[item]
+                    changes = True
+        if not changes:
+            break
+
+for k in sorted(sums.keys(), reverse=True):
+    if k not in surpassed:
+        print k, sums[k]
+
