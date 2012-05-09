375ac6af8a58b6fe40af56073a9949ef12516981
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2012-05-09 07:47:39 +0000

    rucksack.py update - broader search

--- trunk/toys/rucksack.py
+++ trunk/toys/rucksack.py
@@ -5,25 +5,23 @@ if len(sys.argv) < 3:
     print sys.argv, 'Usage: %s limit item1 ... itemN' % sys.argv[0]
     exit()
 
+def search(money_left, item_prices, bag=[]):
+    if len(item_prices) == 0:
+        yield bag
+        return
+    current_price = item_prices.pop()
+    max_current_count = int(money_left / current_price)
+    for i in xrange(max_current_count, 0, -1):
+        for result in search(money_left-i*current_price, item_prices[:],
+                bag+[(current_price, i)]):
+            yield result
+    for result in search(money_left, item_prices[:], bag[:]):
+        yield result
+
 limit = float(sys.argv[1])
-items = sorted(set(map(float, sys.argv[2:])))
+items = sorted(set(map(float, sys.argv[2:])), reverse=True)
 
-sums = {0: []}
-surpassed = set()
-
-for item in items:
-    while True:
-        changes = False
-        for s in sums.keys()[:]:
-            if item+s <= limit:
-                surpassed.add(s)
-                if item+s not in sums:
-                    sums[item+s] = sums[s]+[item]
-                    changes = True
-        if not changes:
-            break
-
-for k in sorted(sums.keys(), reverse=True):
-    if k not in surpassed:
-        print k, sums[k]
+for result in search(limit, items):
+    print sum(price * count for price, count in result), len(result), sum(count for _, count
+            in result), result
 
