4b832616b778d25b7c00a592e80efacdb25e27ac
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2012-07-30 05:54:18 +0000

    calibration refectored, with some info imprinted

--- trunk/toys/K800i_calib/calib.py
+++ trunk/toys/K800i_calib/calib.py
@@ -37,91 +37,123 @@ def unrotatedTvec(unrotMat, tvec):
     MatMul(unrotMat, ttvec, rotatedTvec)
     return rotatedTvec
 
-NamedWindow('wnd')
+def main():
+    def pause(im, text=None):
+        if text:
+            try:
+                im = CloneMat(im)
+            except:
+                im = CloneImage(im)
+            PutText(im, text, (100, 100), InitFont(CV_FONT_HERSHEY_SIMPLEX, 2, 2), 28)
+        ShowImage('wnd', im)
+        WaitKey(0)
 
-for i in glob.glob('*.JPG'):
-    print i
-    img = LoadImage(i, CV_LOAD_IMAGE_GRAYSCALE)
-    print GetSize(img)
-    size = (9, 6)
-    ShowImage('wnd', img)
-    WaitKey(0)
+    import sys
+    max_stage = None
 
-    found, corners = FindChessboardCorners(img, size)
-    # DrawChessboardCorners(img, size, corners, found)
-    print len(corners)
+    if len(sys.argv) > 1:
+        max_stage = int(sys.argv[1])
 
-    cameraMatrix = CreateMat(3, 3, CV_32F)
-    rvecs = CreateMat(1, 3, CV_32F)
-    tvecs = CreateMat(1, 3, CV_32F)
-    distCoefs = CreateMat(1, 5, CV_32F)
-    objectPoints = createChessboardMatrix(size[1], size[0])
+    NamedWindow('wnd')
 
-    error = CalibrateCamera2(
-            objectPoints,
-            list2mat(corners),
-            list2mat([[9*6]], CV_32S),
-            GetSize(img),
-            cameraMatrix,
-            distCoefs,
-            rvecs,
-            tvecs,
-            0)
+    for i in glob.glob('*.JPG'):
 
-    print "Calibrated!"
-    out(cameraMatrix)
-    out(distCoefs)
-    out(rvecs)
-    out(tvecs)
+        # STAGE 1: show original image 
+
+        print i
+        img = LoadImage(i, CV_LOAD_IMAGE_GRAYSCALE)
+        pause(img, "Original")
+        if max_stage == 1:
+            continue
+
+        # STAGE 2: calibrate, then undistort
+
+        size = (9, 6)
+        found, corners = FindChessboardCorners(img, size)
+        # DrawChessboardCorners(img, size, corners, found)
+        print len(corners)
+
+        cameraMatrix = CreateMat(3, 3, CV_32F)
+        rvecs = CreateMat(1, 3, CV_32F)
+        tvecs = CreateMat(1, 3, CV_32F)
+        distCoefs = CreateMat(1, 5, CV_32F)
+        objectPoints = createChessboardMatrix(size[1], size[0])
+
+        CalibrateCamera2(
+                objectPoints,
+                list2mat(corners),
+                list2mat([[9*6]], CV_32S),
+                GetSize(img),
+                cameraMatrix,
+                distCoefs,
+                rvecs,
+                tvecs,
+                0)
+
+        print "Calibrated!"
+        out(cameraMatrix)
+        out(distCoefs)
+        out(rvecs)
+        out(tvecs)
 
 
-    w, h = GetSize(img)
-    mapx = CreateMat(h, w, CV_32FC1)
-    mapy = CreateMat(h, w, CV_32FC1)
-    outimg = CreateMat(h, w, CV_8U)
+        w, h = GetSize(img)
+        mapx = CreateMat(h, w, CV_32FC1)
+        mapy = CreateMat(h, w, CV_32FC1)
+        outimg = CreateMat(h, w, CV_8U)
 
-    # plain undistort
-    InitUndistortMap(cameraMatrix, distCoefs, mapx, mapy)
-    Remap(img, outimg, mapx, mapy)
-    ShowImage('wnd', outimg)
-    WaitKey(0)
+        # plain undistort
+        InitUndistortMap(cameraMatrix, distCoefs, mapx, mapy)
+        Remap(img, outimg, mapx, mapy)
+        pause(outimg, "Undistort")
 
-    # undistort with unrotate
-    InitUndistortRectifyMap(
-            cameraMatrix,
-            distCoefs,
-            unrotMap(rvecs, tvecs),
-            cameraMatrix,
-            mapx, mapy)
-    Remap(img, outimg, mapx, mapy)
-    ShowImage('wnd', outimg)
-    WaitKey(0)
+        if max_stage == 2:
+            continue
 
-    print mapx[0, 0]
-    print mapy[0, 0]
+        # STAGE 3: undistort + rectify
 
-    newvec = unrotatedTvec(unrotMap(rvecs, tvecs), tvecs)
-    newvec2 = CloneMat(newvec)
-    MatMul(cameraMatrix, newvec, newvec2)
-    out(newvec2)
+        InitUndistortRectifyMap(
+                cameraMatrix,
+                distCoefs,
+                unrotMap(rvecs, tvecs),
+                cameraMatrix,
+                mapx, mapy)
+        Remap(img, outimg, mapx, mapy)
+        pause(outimg, "Undistort+Rectify")
 
-    newx = newvec2[0,0]/newvec2[2,0]
-    newy = newvec2[1,0]/newvec2[2,0]
-    print 'new x', newx
-    print 'new y', newy
+        if max_stage == 3:
+            continue
 
-    newCam = CloneMat(cameraMatrix)
-    newCam[0, 2] -= newx
-    newCam[1, 2] -= newy
-    InitUndistortRectifyMap(
-            cameraMatrix,
-            distCoefs,
-            unrotMap(rvecs, tvecs),
-            newCam,
-            mapx, mapy)
-    Remap(img, outimg, mapx, mapy)
-    ShowImage('wnd', outimg)
-    WaitKey(0)
+        # STAGE 4: undistort + rectify + change new camera matrix
 
-    print mapx[0, 0]
-    print mapy[0, 0]
+        print mapx[0, 0]
+        print mapy[0, 0]
+
+        newvec = unrotatedTvec(unrotMap(rvecs, tvecs), tvecs)
+        newvec2 = CloneMat(newvec)
+        MatMul(cameraMatrix, newvec, newvec2)
+        out(newvec2)
+
+        newx = newvec2[0,0]/newvec2[2,0]
+        newy = newvec2[1,0]/newvec2[2,0]
+        print 'new x', newx
+        print 'new y', newy
+
+        newCam = CloneMat(cameraMatrix)
+        newCam[0, 2] -= newx
+        newCam[1, 2] -= newy
+        InitUndistortRectifyMap(
+                cameraMatrix,
+                distCoefs,
+                unrotMap(rvecs, tvecs),
+                newCam,
+                mapx, mapy)
+        Remap(img, outimg, mapx, mapy)
+        pause(outimg, "Undistort+Rectify+newCameraMatrix")
+
+        print mapx[0, 0]
+        print mapy[0, 0]
+
+if __name__ == '__main__':
+    main()
+
