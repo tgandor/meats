54d24a10faac95b43d7e341b83fc60dd2055de08
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2012-12-17 21:44:26 +0000

    grep_any.pl - grepping through logs for any string

---
+++ svnstats/grep_any.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+die("Usage:  <string to find>\n") if $#ARGV == -1;
+my $search = $ARGV[0];
+my $print = 1; # for logs starting with "--------...-------" anyway
+my @data = ();
+while(<STDIN>)
+{
+	if (/$search/) {
+		$print = 1;
+	}
+	push (@data, $_);
+	if (/^---/)
+	{
+		if ($print) 
+		{
+			print @data;
+		}
+		@data = ();
+		$print = 0;
+	}
+}
+# print @ARGV;
+
