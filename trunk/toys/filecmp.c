f1bfcea9e6332fee34f986ced43a3a286e93148e
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-01-15 16:23:26 +0000

    filecmp.c - a primitive binary file compare utility

---
+++ trunk/toys/filecmp.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Usage: %s <file1> <file2>\n", argv[0]);
+		return 1;
+	}
+	FILE *f1 = fopen(argv[1], "rb");
+	FILE *f2 = fopen(argv[2], "rb");
+	int c1, c2;
+	int total = 0;
+	for (;;)
+	{
+		c1 = fgetc(f1);
+		c2 = fgetc(f2);
+		if (c1 != c2)
+		{
+			printf("Files differ at byte %d: %d vs %d\n", total, c1, c2);
+			break;
+		}
+		++total;
+		if (c1 == EOF)
+		{
+			puts("Files same.");
+		}
+	}
+	fclose(f1); 
+	fclose(f2);
+	return 0;
+}
