3f867d36298fceb1a4e64829b6464ca6152a5f25
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-01-20 00:30:30 +0000

    MuiPageGen - starting with the NSH: writing language strings. Had bugs before, btw.

--- trunk/MuiPageGen/mui_page_gen.py
+++ trunk/MuiPageGen/mui_page_gen.py
@@ -4,11 +4,6 @@ class Item(object):
     LABEL = 0
     CHECKBOX = 1
     SPACER = 2
-    langs = [
-        ('en', 1033),
-        ('de', 1031),
-        ('pl', 1045),
-    ]
     def __init__(self, kind, left=2, right=330):
         self.kind = kind
         self.left = left
@@ -16,6 +11,12 @@ class Item(object):
         self.field_id = 0
 
 class I18nItem(Item):
+    langs = [
+        ('en', 1033),
+        ('de', 1031),
+        ('pl', 1045),
+    ]
+
     def __init__(self, kind, texts=[], **kwargs):
         super(I18nItem, self).__init__(kind, **kwargs)
         self.texts = texts
@@ -30,6 +31,15 @@ class I18nItem(Item):
         out.write("Left=%d\r\nRight=%d\r\nTop=%d\r\nBottom=%d\r\n\r\n" % (
             self.left, self.right, top, bottom
         ))
+
+    def write_translations(self, out, name):
+        for message in self.texts:
+            for lang in self.langs:
+                if message[1] in lang:
+                    out.write("LangString %s_FIELD_%d_TEXT %d \"%s\"\n" % (
+                        name.upper(), self.field_id, lang[1], message[0]
+                        ))
+
 
 class Label(I18nItem):
     def __init__(self, **kwargs):
@@ -56,6 +66,7 @@ class Form(object):
 
     def __init__(self, name):
         self.items = []
+        self.fields = []
         self.name = name
         self.fieldnum = 0
 
@@ -66,20 +77,29 @@ class Form(object):
         if isinstance(item, I18nItem):
             self.fieldnum += 1
             item.field_id = self.fieldnum
+            self.fields.append(item)
         self.items.append(item)
 
     def write_ini_file(self):
         f = open(self.name+'.ini', 'wb')
         f.write("[Settings]\r\n")
-        f.write("NumFields=%d\r\n\r\n" % len(self.items))
+        f.write("NumFields=%d\r\n\r\n" % self.fieldnum)
         top = Form.topmost
         for item in self.items:
             if item.kind == Item.SPACER:
                 top += item.height
             elif isinstance(item, I18nItem):
-                self.fieldnum += 1
                 item.render(f, top, top + self.item_height)
                 top += self.item_height + self.item_below
+        f.close()
+
+    def write_nsh_file(self):
+        f = open(self.name+'.nsh', 'w')
+        f.write("; This file was generated with mui_page_gen.py\n\n")
+        f.write("; Language strings\n\n")
+        for field in self.fields:
+            field.write_translations(f, self.name)
+        f.write("\n")
         f.close()
 
 def test():
@@ -87,8 +107,9 @@ def test():
     form.append(Label())
     form.append(Checkbox('shortcut'))
     form.append(Spacer())
-    form.append(Label(texts=[('fiu', 'pl')]))
+    form.append(Label(texts=[('fiu', 'pl'), ('phew', 'en')]))
     form.write_ini_file()
+    form.write_nsh_file()
 
 if __name__ == '__main__':
     test()
