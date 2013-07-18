62cc04c3466b492df85d9c6f6359ed961fef420a
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-07-18 09:55:15 +0000

    libdirs.py - analyzing vcxproj (vs2010) projects for includes/libs/libdirs

---
+++ trunk/toys/libdirs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from itertools import groupby
+from operator import itemgetter
+from xml.dom import minidom
+import sys
+
+def configs(doc, name='AdditionalDependencies'):    
+    for el in  doc.getElementsByTagName('ItemDefinitionGroup'):
+        condition = el.attributes['Condition'].value
+        for libs in el.getElementsByTagName(name):
+            result = libs.firstChild.nodeValue
+            yield (result, condition)
+
+def group_report(doc, element='AdditionalDependencies'):
+    gn = 0
+    print "=" * 60
+    print "Grouping by", element
+    print "=" * 60
+    for k, g in groupby(sorted(configs(doc), key=itemgetter(0)), key=itemgetter(0)):
+        gn += 1
+        print "Group", gn, "\n" + "-" * 40
+        cn = 0
+        for cond in g:
+            cn += 1
+            print "%2d %s" % (cn, cond[1])
+        print "Value:"
+        print k
+        print "-"*40    
+    print "End of grouping by", element
+    print "=" * 60, "\n"
+
+if __name__ == '__main__':
+    doc = minidom.parse(sys.argv[1])
+    group_report(doc, 'AdditionalDependencies')
+    group_report(doc, 'AdditionalLibraryDirectories')
+    group_report(doc, 'AdditionalIncludeDirectories')
