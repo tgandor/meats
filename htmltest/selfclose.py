962ff9cdd412515b31c43190d11e979cb4b4827d
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2013-08-02 07:45:29 +0000

    selfclose.py - small utility to close img's input's etc. htm.l - amendments.

---
+++ htmltest/selfclose.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+def main():
+    import re
+    import sys
+    unselfclosed = re.compile('<(img|br|hr|input) [^>]+[^/]>')
+    def repl(match):
+        orig = match.group()
+        print '   ', orig
+        if orig[-2] == ' ':
+            return orig[:-1] + '/>'
+        return orig[:-1] + ' />'
+    for f in sys.argv[1:]:
+        print "--- Processing: %s ---" % f
+        body = open(f).read()
+        body, changes = unselfclosed.subn(repl, body)
+        print "... %d changes ..." % changes
+        if changes > 0:
+            open(f, 'w').write(body)
+
+if __name__=='__main__':
+    main()
