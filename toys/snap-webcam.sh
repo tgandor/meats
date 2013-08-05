8bfc9af94819134036ea58b2039381d76244d7d7
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2013-08-05 22:27:08 +0000

    webcam: more configurable via env variables w=<width> h=<height>, with defaults

--- toys/snap-webcam.sh
+++ toys/snap-webcam.sh
@@ -3,6 +3,8 @@ subdir=`date +'%Y-%m-%d_%H-%M-%S'`
 mkdir $subdir
 cd $subdir
 device=`ls /dev/video? | head -n 1`
-mplayer -vo jpeg -frames 75 -fps 30 -tv driver=v4l2:width=1280:height=1024:device=$device tv://
+if [ -z $w ]; then w=1280; fi
+if [ -z $h ]; then h=1024; fi
+mplayer -vo jpeg -frames 15 -fps 15 -tv driver=v4l2:width=$w:height=$h:device=$device tv://
 echo "Frames stored in $subdir"
 
--- toys/view-webcam.sh
+++ toys/view-webcam.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 device=`ls /dev/video? | head -n 1`
-mplayer -fps 30 -tv driver=v4l2:width=1280:height=1024:device=$device tv://
+if [ -z $w ]; then w=1280; fi
+if [ -z $h ]; then h=1024; fi
+mplayer -fps 30 -tv driver=v4l2:width=$w:height=$h:device=$device tv://
