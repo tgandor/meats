a1914120eb054ee1e28112a6a31ca9f31de96e77
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-08-05 20:51:07 +0000

    some improvements on webcam (higher resolution), finding in /dev/

--- trunk/toys/snap-webcam.sh
+++ trunk/toys/snap-webcam.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 subdir=`date +'%Y-%m-%d_%H-%M-%S'`
 mkdir $subdir
 cd $subdir
-mplayer -vo jpeg -frames 75 -fps 30 -tv driver=v4l2:width=640:height=480:device=/dev/video0 tv://
+device=`ls /dev/video? | head -n 1`
+mplayer -vo jpeg -frames 75 -fps 30 -tv driver=v4l2:width=1280:height=1024:device=$device tv://
 echo "Frames stored in $subdir"
 
--- trunk/toys/view-webcam.sh
+++ trunk/toys/view-webcam.sh
@@ -1,1 +1,3 @@
-mplayer -fps 30 -tv driver=v4l2:width=1280:height=1024:device=/dev/video1 tv://
+#!/bin/bash
+device=`ls /dev/video? | head -n 1`
+mplayer -fps 30 -tv driver=v4l2:width=1280:height=1024:device=$device tv://
