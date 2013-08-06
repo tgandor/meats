dcd1ead01910c2d5149cf51df37fe9c212d1edbb
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-08-06 11:58:10 +0000

    webcam: fps and device configurable, and num of dumped frames

--- trunk/toys/view-webcam.sh
+++ trunk/toys/view-webcam.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-device=`ls /dev/video? | head -n 1`
+if [ -z $device ]; then device=`ls /dev/video? | head -n 1`; fi
 if [ -z $w ]; then w=1280; fi
 if [ -z $h ]; then h=1024; fi
-mplayer -fps 30 -tv driver=v4l2:width=$w:height=$h:device=$device tv://
+if [ -z $fps ]; then fps=30; fi
+mplayer -fps $fps -tv driver=v4l2:width=$w:height=$h:device=$device tv://
