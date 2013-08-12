99d1b41145464d15ba5daaca0ce911a37c2d20a5
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-08-12 15:05:26 +0000

    pagesize and a couple of old boost examples

---
+++ trunk/lang_lawyer/boosterr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <boost/system/error_code.hpp>
+
+int main() {
+	boost::system::error_code ec;
+	if ( ec != boost::system::errc::success ) 
+		std::cout << "Error default." << std::endl;
+	else
+		std::cout << "Success default." << std::endl;
+	return 0;
+}
+
