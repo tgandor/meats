49f7d3ea4c83a531ebbd03bf6d062d904a991a58
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-08-12 15:04:52 +0000

    easier device selection for webcam toys

--- trunk/toys/snap-webcam.sh
+++ trunk/toys/snap-webcam.sh
@@ -2,7 +2,15 @@
 subdir=`date +'%Y-%m-%d_%H-%M-%S'`
 mkdir $subdir
 cd $subdir
-if [ -z $device ]; then device=`ls /dev/video? | head -n 1`; fi
+if [ -z $device ]
+  then device=`ls /dev/video? | head -n 1`
+elif [ $device == 0 ]
+  then device=/dev/video0
+elif [ $device == 1 ]
+  then device=/dev/video1
+elif [ $device == 2 ]
+  then device=/dev/video2
+fi
 if [ -z $w ]; then w=1280; fi
 if [ -z $h ]; then h=1024; fi
 if [ -z $fps ]; then fps=15; fi
