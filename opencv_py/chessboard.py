97b2c64d44e85e87a61d17d13ad2181b0a48ac27
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2013-09-08 12:58:18 +0000

    chessboard.py - running camera calibration on the found chessboard

--- opencv_py/chessboard.py
+++ opencv_py/chessboard.py
@@ -7,15 +7,18 @@ Draw the found corners.
 
 import cv
 import cv2 # only flag value
+import numpy
 import pygame
 import sys
+import time
 
 from pygame.locals import *
+
 
 # Settings:
 
 PAT_SIZE = (9, 6)
-
+IMG_SIZE = (640, 480)
 findFlags = (
     cv.CV_CALIB_CB_ADAPTIVE_THRESH
     | cv.CV_CALIB_CB_NORMALIZE_IMAGE
@@ -37,19 +40,37 @@ def cv2pygame(im):
 
 def get_image():
     im = cv.QueryFrame(camera)
-    cv.Flip(im, flipMode=1) # mirror effect
+    # cv.Flip(im, flipMode=1) # mirror effect
     success, corners = cv.FindChessboardCorners(im, PAT_SIZE, findFlags)
     if success:
         cv.DrawChessboardCorners(im, PAT_SIZE, corners, success)
         cv.PutText(im, "Found: (%.1f, %.1f)" %  corners[0], infoOrigin,
             infoFont, (0, 255, 0))
+        cam_matrix = cv.CreateMat(3, 3, cv.CV_32F)
+        dist_coeff = cv.CreateMat(1, 4, cv.CV_32F)
+        rvecs = cv.CreateMat(1, 9, cv.CV_32F)
+        tvecs = cv.CreateMat(1, 3, cv.CV_32F)
+        pointArr = numpy.array([(x, y, 0) for y in xrange(PAT_SIZE[1]) for x in xrange(PAT_SIZE[0])], numpy.float32)
+        objectPoints = cv.fromarray(pointArr)
+        imgPointArr = numpy.array(corners, numpy.float32)
+        imagePoints = cv.fromarray(imgPointArr)
+        pointCounts = cv.CreateMat(1, 1, cv.CV_32S)
+        pointCounts[0, 0] = PAT_SIZE[0] * PAT_SIZE[1]
+        cv.CalibrateCamera2(objectPoints, imagePoints, pointCounts, IMG_SIZE, cam_matrix, dist_coeff, rvecs, tvecs)
+        print "TVecs:", numpy.asarray(tvecs)
+        rmat = numpy.asarray(rvecs).reshape((3, 3), order='C')
+        print "RVecs:"
+        print rmat
+        # print "Azimuth:", rmat.dot(numpy.array([0, 0, 1]))
+        # time.sleep(1)
+        #import pdb; pdb.set_trace()
     else:
-		cv.PutText(im, "Not found.", infoOrigin, infoFont, (0, 255, 0))
+        cv.PutText(im, "Not found.", infoOrigin, infoFont, (0, 255, 0))
     return cv2pygame(im)
 
 fps = 30.0
 pygame.init()
-window = pygame.display.set_mode((640,480))
+window = pygame.display.set_mode(IMG_SIZE)
 pygame.display.set_caption("WebCam: Chessboard")
 screen = pygame.display.get_surface()
 
@@ -61,6 +82,6 @@ while True:
             pygame.quit()
             sys.exit(0)
     pg_img = get_image()
-    screen.blit(pg_img, (0,0))
+    screen.blit(pg_img, (0, 0))
     pygame.display.flip()
     # pygame.time.delay(int(1000 * 1.0/fps))
