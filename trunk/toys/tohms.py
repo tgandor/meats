5041c17a65e4be6a16254a27175418c03e278a1c
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-09-16 10:34:36 +0000

    tohms.py - added days for large counts, also weeks and approximate years

--- trunk/toys/tohms.py
+++ trunk/toys/tohms.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 import sys
 
+def print_secs(secs):
+	h, s = divmod(secs, 3600)
+	m, s = divmod(s, 60)
+	print "%d seconds - %02d:%02d:%02d" % (secs, h, m, s)
+
 if len(sys.argv) != 2:
     print "Usage:"
     print "  %s SECONDS" % sys.argv[0]
@@ -12,7 +17,19 @@ secs = int(hms[0])
 for limb in hms[1:]:
     secs *= 60
     secs += int(limb)
-h, s = divmod(secs, 3600)
-m, s = divmod(s, 60)
-print "%d seconds - %02d:%02d:%02d" % (secs, h, m, s)
+
+print_secs(secs)
+
+if secs > 24 * 3600: 
+	days, rest = divmod(secs, 24 * 3600)
+	print days, 'days',
+	print_secs(rest)
+	if days > 7:
+		weeks, rdays = divmod(days, 7)
+		print weeks, 'weeks', rdays, 'days',
+		print_secs(rest)
+		if weeks > 52: # rough!
+			years, rweeks = divmod(weeks, 52)
+			print years, 'years', rweeks, 'weeks', rdays, 'days',
+			print_secs(rest)
 
