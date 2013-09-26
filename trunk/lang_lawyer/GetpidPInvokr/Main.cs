92318047f1b03efaaf71eaa1bef9d8a804313644
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-09-26 16:23:33 +0000

    Initial check-in of module GetpidPInvokr

---
+++ trunk/lang_lawyer/GetpidPInvokr/Main.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace GetpidPInvokr
+{
+	class MainClass
+	{
+		[DllImport ("libc.so.6")]
+		private static extern int getpid();
+		
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World! My pid is: " + getpid());
+		}
+	}
+}
