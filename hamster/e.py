621783565080d1a825f470a5cc36686a20f45854
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2013-10-06 11:30:18 +0000

    e.py, g.py - added time and download speed reporting

--- hamster/e.py
+++ hamster/e.py
@@ -1,20 +1,32 @@
 
-import sys, urllib, os, re
+import sys, urllib, os, re, time
 
-URL= sys.argv[1]
+def human(x):
+    for sufix in ['', 'k', 'M', 'G']:
+        if x < 1024:
+            return "%.1f %s" % (x, sufix)
+        x /= 1024
+    return "%.1f P" % x
 
-def get (URL):
-  return urllib.urlopen(URL).read ()
+def get(URL):
+  return urllib.urlopen(URL).read()
 
 def down(URL):
-  f=os.path.basename(URL)
-  print "downloading", f
-  Data = get (URL)
-  print "saving"
-  open (f,"w").write (Data)
+  f = os.path.basename(URL)
+  if os.path.exists(f):
+      print  "skipped", f
+      return
+  print "getting", f
+  start = time.time()
+  data = get(URL)
+  size = len(data)
+  elap = time.time() - start
+  print "got %sB in %.1f s (%sB/s), saving" % (human(size), elap, human(size/elap))
+  open(f,"w").write(data)
+
+URL = sys.argv[1]
 
 for link in re.findall("http[^\"]+", get (URL)):
-  if re.search (sys.argv [2], link):
+  if re.search(sys.argv[2], link):
     down (link)
 
-# http://www.archive.org/download/tale_of_two_cities_1012_librivox/taleoftwocities_01_dickens_64kb.mp3
--- hamster/g.py
+++ hamster/g.py
@@ -1,10 +1,20 @@
 
-import sys, urllib, os
+import sys, urllib, os, time
 
-URL= sys.argv[1]
-f=os.path.basename(URL)
+def human(x):
+    for sufix in ['', 'k', 'M', 'G']:
+        if x < 1024:
+            return "%.1f %s" % (x, sufix)
+        x /= 1024
+    return "%.1f P" % x
 
+URL = sys.argv[1]
+f = os.path.basename(URL)
+
+start = time.time()
 print "downloading", f
-Data = urllib.urlopen(URL).read ()
-print "saving"
-open (f,"w").write (Data)
+data = urllib.urlopen(URL).read()
+size = len(data)
+elap = time.time() - start
+print "got %sB in %.1f s (%sB/s), saving" % (human(size), elap, human(size/elap))
+open(f, "w").write(data)
