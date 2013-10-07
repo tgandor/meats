efa852585e471485ce2cc315bfcbc4061a6e557b
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-10-07 07:06:12 +0000

    e.py - more robust downloading, reporting total transfer.

--- trunk/hamster/e.py
+++ trunk/hamster/e.py
@@ -1,21 +1,30 @@
 
-import sys, urllib, os, re, time
+import sys, urllib, os, re, time, socket, errno
 
 def human(x):
     for sufix in ['', 'k', 'M', 'G']:
         if x < 1024:
             return "%.1f %s" % (x, sufix)
-        x /= 1024
+        x /= 1024.0
     return "%.1f P" % x
 
 def get(URL):
-  return urllib.urlopen(URL).read()
+    """Stubborn retrieve data."""
+    while True:
+        try:
+            return urllib.urlopen(URL).read()
+        except socket.timeout:
+            print "Timed out. Retrying."
+        except socket.error as e:
+            print "Other socket error:", e
+            print "Retry in 5 seconds..."
+            time.sleep(5)
 
 def down(URL):
   f = os.path.basename(URL)
   if os.path.exists(f):
       print  "skipped", f
-      return
+      return 0
   print "getting", f
   start = time.time()
   data = get(URL)
@@ -23,10 +32,14 @@ def down(URL):
   elap = time.time() - start
   print "got %sB in %.1f s (%sB/s), saving" % (human(size), elap, human(size/elap))
   open(f,"w").write(data)
+  return size
 
 URL = sys.argv[1]
 
-for link in re.findall("http[^\"]+", get (URL)):
-  if re.search(sys.argv[2], link):
-    down (link)
-
+total = 0
+try:
+    for link in re.findall("http[^\"]+", get (URL)):
+        if re.search(sys.argv[2], link):
+            total += down(link)
+finally:
+    print "Total downloaded: %sB" % human(total)
