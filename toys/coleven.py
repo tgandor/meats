5f6f9fcf93fedfdae13ad3a91611bfa5f0c2a921
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2013-10-22 12:56:53 +0000

    coleven.py - new features: preserving leading indentation, no space at EOL, limiting number of columns.

--- toys/coleven.py
+++ toys/coleven.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
-def main():
+def main(maxcols = 0):
     """Read input lines, split and pad to have even columns."""
+    import re
+    import sys
 
     def pad(s, n):
         return s + (n-len(s))*' ' if len(s) < n else s
 
     rows = []
+    indents = []
     try:
         while True:
-            rows.append(raw_input().split())
+            line = raw_input()
+            rows.append(line.split(None, maxcols-1))
+            indents.append(re.match('\s+', line))
     except EOFError:
         pass
     max_cols = max(map(len, rows))
@@ -17,11 +22,18 @@ def main():
     for row in rows:
         for i in xrange(len(row)):
             max_lens[i] = max(max_lens[i], len(row[i]))
-    for row in rows:
-        for i in xrange(len(row)):
+    for row, indent in zip(rows, indents):
+        if indent:
+            sys.stdout.write(indent.group())
+        for i in xrange(len(row)-1):
             print pad(row[i], max_lens[i]),
-        print
+        if len(row) > 0:
+            print row[-1]
+        else:
+            print
 
 if __name__ == '__main__':
-    main()
+    import sys
+    maxcols = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+    main(maxcols)
 
