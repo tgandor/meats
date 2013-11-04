9d67b114d199f9ed5219469b5ddbf3526d7aa584
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-11-04 15:48:24 +0000

    proplog.py - a tool written some time ago. display log of svn property.

---
+++ trunk/svnstats/proplog.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+
+prop = sys.argv[1]
+
+run = lambda cmd: os.popen(cmd).read()
+val = lambda rev: run('svn pg %s -r %d' % (prop, rev))
+
+maxrev = int(re.search('r(\d+)', run('svn log -r HEAD:1 -l 1')).group(1))
+maxval = val(maxrev)
+
+def find_end(startrev):
+    curr = val(startrev)
+    if curr == maxval:
+        return maxrev
+    endrev = maxrev
+    while True:
+        currev = (startrev + endrev) / 2
+        # print >>sys.stderr, "Checking %d" % currev
+        if val(currev) != curr:
+            endrev = currev
+        else:
+            if val(currev+1) != curr:
+                return currev
+            startrev = currev
+
+left = 1
+while True:
+    right = find_end(left)
+    print "%d-%d" % (left, right)
+    print val(left)
+    if right == maxrev:
+        break
+    left = right + 1
+
