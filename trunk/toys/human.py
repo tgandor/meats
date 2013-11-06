ad66a0c48eb933c6ad0507fc006c2ba57512b366
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-11-06 15:17:34 +0000

    human.py - popular functionality: now as simple command line (format 'human-readable' units)

---
+++ trunk/toys/human.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+
+def human(x):
+    for sufix in ['', 'K', 'M', 'G']:
+        if x < 1024:
+            return "%.2f %s" % (x, sufix) if x - int(x) > 0.001 else "%d %s" % (int(x), sufix)
+        x /= 1024.0
+    return "%.1f P" % x
+
+if __name__ == '__main__':
+	print human(int(sys.argv[1]))
+
