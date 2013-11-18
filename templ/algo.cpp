795c370cbef48806cf108b66dba78bacf955be4e
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2013-11-18 06:43:13 +0000

    algo.cpp - short template for simple programs (no 'optional' stuff)

---
+++ templ/algo.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define FOR(ii, ll, uu)  for(int ii##lim = (uu), ii = (ll); ii < ii##lim; ++ii)
+#define REP(ii, nn) FOR(ii, 0, nn)
+#define GI ({int t; scanf("%d", &t); t;})
+
+int main()
+{
+
+	return 0;
+}
