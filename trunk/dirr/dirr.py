3d3c8577925c6242e73857d9ec41255e7887714e
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2013-11-22 15:34:21 +0000

    dirr.py - interactive mode for exploration.

--- trunk/dirr/dirr.py
+++ trunk/dirr/dirr.py
@@ -15,7 +15,7 @@ def getr(obj, like=''):
     for i in dir(obj):
         if test.search(i):
             return getattr(obj, i)
-
+    return None
 
 def hlp(obj, like=''):
     """Print help for first attribute matching regular expression."""
@@ -23,4 +23,31 @@ def hlp(obj, like=''):
     for i in dir(obj):
         if test.search(i):
             help(getattr(obj, i))
-            return
+            return True
+    return False
+
+def interactive():
+    modules = []
+    try:
+        while True:
+            cmd = raw_input()
+            if cmd == 'q':
+                break
+            if cmd.startswith('i '):
+                modules.append(__import__(cmd[2:]))
+                print "Loaded modules:", ", ".join([m.__name__ for m in modules])
+            elif cmd.startswith('h '):
+                for m in modules:
+                    if hlp(m, cmd[2:]):
+                        break
+            else:
+                for m in modules:
+                    if getr(m, cmd) is not None:
+                        print "  ---  In module %s:  ---  " % (m.__name__,)
+                        dirr(m, cmd)
+    except EOFError:
+        pass
+    print "Be seeing you..."
+
+if __name__ == '__main__':
+    interactive()
