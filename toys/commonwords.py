957273ca8bff93de2c8253f9d3aaa4d8ea4ea840
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2013-11-28 14:21:21 +0000

    toys/commonwords.py - a tool for finding common (and mosty - unique) n-grams ('words') between files.

---
+++ toys/commonwords.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import re
+import sys
+import locale
+
+TOO_COMMON = 2
+
+lhs = open(sys.argv[1]).read().decode('UTF-8')
+rhs = open(sys.argv[2]).read().decode('UTF-8')
+
+try:
+	ngram = int(sys.argv[3])
+	if ngram < 1:
+		ngram = 1
+except:
+	ngram = 1
+
+matcher = re.compile(' '.join(['\w+']*ngram), re.UNICODE)
+
+lwords = matcher.findall(lhs)
+rwords = matcher.findall(rhs)
+
+common = set(lwords) & set(rwords)
+
+numunique = 0
+for w in sorted(common):
+	if lhs.count(w) < TOO_COMMON and rhs.count(w) < TOO_COMMON:
+		numunique += 1
+		print numunique, w   
+
