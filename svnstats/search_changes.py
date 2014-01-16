6ad59b8d152fe91d974d6c6d73456bb720ad2a18
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2014-01-16 13:05:13 +0000

    search_changes.py - searching through diff of specific file.

---
+++ svnstats/search_changes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+
+# arguments: path to search, substring to find in changes
+
+target, query = sys.argv[1:]
+
+rev = re.compile('^r(\d+)', re.MULTILINE)
+
+log = os.popen('svn log -q %s' % target).read()
+
+revisions = rev.findall(log)
+
+print 'Searching through %d revisions...' % len(revisions)
+
+
+for r in revisions:
+    print "Revision %s:" % r,
+    # -x -U4 --diff-cmd=diff
+    cmd = 'svn diff -c %s %s' % (r, target)
+    print cmd
+    diff = os.popen(cmd).read()
+    # print '\n', diff
+    if diff.find(query) == -1:
+        print 'not found.'
+        continue
+    os.system('svn log -r %s %s' % (r, target))
+    print diff
+    print '-' * 60
+
