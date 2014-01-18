0b872d9a2a66ac1938c9d63141860877c26ee1d1
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-01-18 16:14:32 +0000

    pipequeue/server.py - after long time: got it working in a primitive way.

--- trunk/pipequeue/server.py
+++ trunk/pipequeue/server.py
@@ -1,23 +1,30 @@
+#!/usr/bin/env python
+
 import os
 import tempfile
 import time
 
-operation = "./youtube-dl -t "
-try:
-    pipe = tempfile.mktemp()
+operation = 'youtube-dl '
+
+pipe = 'quepipe' # tempfile.mktemp()
+if not os.path.exists(pipe):
     os.mkfifo(pipe)
-    open('pipename', 'w').write(pipe)
+
+# open('pipename', 'w').write(pipe)
+
+print "> Listening on pipe:", pipe
+
+try:
     que = open(pipe, 'r')
+    print "> Pipe opened! (something connected)"
     while True:
         file = que.readline()
-	if not file:
-	    print "Queue empty"
-            time.sleep(5)
-	else:
+        if not file:
+            print "Queue empty"
+            break
+        else:
             print file
-	    os.system(operation + file)
+            os.system(operation + file)
 finally:
-    que.close()
     os.remove(pipe)
-    os.remove('pipename')
-    
+    # os.remove('pipename')
