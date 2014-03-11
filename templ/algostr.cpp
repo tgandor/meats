34196d6a196db3817e98ba4a1039a2f347063a91
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2014-03-11 17:15:15 +0000

    templ/algostr.cpp - a template using C++ iostream and string. only for some string problems; slower.

---
+++ templ/algostr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define FOR(ii, ll, uu)  for(int ii##lim = (uu), ii = (ll); ii < ii##lim; ++ii)
+#define REP(ii, nn) FOR(ii, 0, nn)
+#define GI ({int t; cin >> t; t;})
+
+using namespace std;
+
+int main()
+{
+	cin.sync_with_stdio(false);
+	return 0;
+}
