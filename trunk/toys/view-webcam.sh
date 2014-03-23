1b8405b1ce97f950f9f57c9e4a17408af33ad166
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-03-23 09:31:38 +0000

    view-webcam.sh - enables screenshots from webcam with 's'

--- trunk/toys/view-webcam.sh
+++ trunk/toys/view-webcam.sh
@@ -11,4 +11,4 @@ fi
 if [ -z $w ]; then w=1280; fi
 if [ -z $h ]; then h=1024; fi
 if [ -z $fps ]; then fps=30; fi
-mplayer -fps $fps -tv driver=v4l2:width=$w:height=$h:device=$device tv://
+mplayer -fps $fps -vf screenshot -tv driver=v4l2:width=$w:height=$h:device=$device tv://
