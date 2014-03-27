14c24dc505317a2461b04edac1740f7f332e6c91
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-03-27 23:48:36 +0000

    toys/frequency.py - added a lagged average (more stable).

--- trunk/toys/frequency.py
+++ trunk/toys/frequency.py
@@ -2,16 +2,22 @@
 
 import time
 
-print 'Keep pressing <Enter> or enter something to quit.'
+def go():
+    print 'Keep pressing <Enter> or enter something to quit.'
 
-last_hit = time.time()
-line = ''
+    last_hit = time.time()
+    line = ''
+    lagavg = 0
 
-while line == '':
-    line = raw_input()
-    hit = time.time()
-    delay = hit - last_hit
-    print '%6.2f ms, %3.2f Hz (FPS), pulse %2.1f (RPM)' % (delay * 1000, 1/delay, 60/delay)
-    last_hit = hit
+    while line == '':
+        line = raw_input()
+        hit = time.time()
+        delay = hit - last_hit
+        lagavg = (4*lagavg + delay) / 5
+        print '%6.2f ms, %3.2f Hz (FPS), pulse %2.1f (RPM) | last 5 avg: %.1f ms %.1f Hz %.1f bpm' % (
+            delay * 1000, 1/delay, 60/delay, lagavg*1000, 1/lagavg, 60/lagavg)
+        last_hit = hit
 
-print 'Bye!'
+    print 'Bye!'
+
+go()
