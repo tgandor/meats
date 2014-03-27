bd8c1fceb207d55d3c1d35c2c6fac612001de474
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2014-03-27 21:38:32 +0000

    configs/F12_time.sh - source this to prepend time and run line in shell.

---
+++ configs/F12_time.sh
@@ -0,0 +1,5 @@
+#!/bin/echo This is for sourcing, not running:
+
+# from: http://superuser.com/questions/117227/a-command-before-every-bash-command
+# bind 'RETURN: "\e[1~time \e[4~\n"'
+bind '"\e[24~": "\e[1~time \e[4~\n"'
