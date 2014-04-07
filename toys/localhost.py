c416992d613ad577ced4f53fe061668ec024cd51
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2014-04-07 05:20:34 +0000

    updates to e.py and toys/localhost.py - print localhost URLs for all active ifaces.

---
+++ toys/localhost.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+else:
+    path = '/'
+
+if len(sys.argv) > 2:
+    port = sys.argv[2]
+    if not port.startswith(':'):
+        port = ':' + port
+else:
+    port = ''
+
+if not path.startswith('/'):
+    path = '/' + path
+
+config = os.popen('ifconfig').read()
+
+for ip in re.findall("inet addr:([0-9.]+)", config):
+    print "http://%s%s%s" % (ip, port, path)
