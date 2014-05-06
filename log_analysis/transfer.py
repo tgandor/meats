4c19d99551c6c2582732b081eb0a5d8e7a527038
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2014-05-06 10:58:49 +0000

    log analysis - fixed exceptions, plugged in human, changed formatting.

--- log_analysis/transfer.py
+++ log_analysis/transfer.py
@@ -3,15 +3,27 @@
 import re
 import sys
 
+
+def human(x):
+    for suffix in ['', 'K', 'M', 'G', 'T']:
+        if x < 1024:
+            return "%.2f %s" % (x, suffix) if x - int(x) > 0.001 else "%d %s" % (int(x), suffix)
+        x /= 1024.0
+    return "%.1f P" % x
+
 paths = {}
 
 for line in sys.stdin.readlines():
-    path = re.search("GET ([^ ]+) HTTP", line).group(1)
+    try:
+        path = re.search("GET ([^ ]+) HTTP", line).group(1)
+    except AttributeError:
+        continue
+
     try:
         size = re.search('200 (\d+) ', line).group(1)
-    except:
-        raise
-        # continue
+    except AttributeError:
+        continue
+
     if path in paths:
         paths[path]['total'] += int(size)
         paths[path]['count'] += 1
@@ -19,5 +31,5 @@ for line in sys.stdin.readlines():
         paths[path] = {'count': 1, 'total': int(size)}
 
 for total, num, path in sorted((paths[path]['total'], paths[path]['count'], path) for path in paths):
-    print "{0}: {1} bytes x {2} times, {3} total.".format(path, total / num, num, total)
+    print "{3}B : {0} : {1} bytes x {2} times.".format(path, total / num, num, human(total))
 
