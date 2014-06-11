2e987bdf8b90c8957af182889c0c11e2678a8e12
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-06-11 09:26:17 +0000

    log_analysis/msvc_sort.py - end of line autodetection, used in output.

--- trunk/log_analysis/msvc_sort.py
+++ trunk/log_analysis/msvc_sort.py
@@ -8,13 +8,19 @@ import sys
 projects = defaultdict(list)
 epilogue = []
 
-for line in sys.stdin.readlines():
+input_data = sys.stdin.readlines()
+for line in input_data:
     m = re.match('(\d+)>', line)
     if m:
         projects[m.group(1)].append(line)
     else:
         epilogue.append(line)
 
+try:
+    eol = re.search('\s+$', input_data[0]).group()
+except:
+    eol = '\n'
+
 for num in sorted(projects.keys(), key=int):
-    sys.stdout.write(''.join(projects[num])+'\n')
+    sys.stdout.write(''.join(projects[num])+eol)
 sys.stdout.write(''.join(epilogue))
