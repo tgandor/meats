2e750776a69eea0d075bead35dac57abc0019b3e
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-06-25 12:16:31 +0000

    full-power.sh - slightly updating (was often only tuning first core)

--- trunk/toys/full-power.sh
+++ trunk/toys/full-power.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sudo cpufreq-set -g performance
-
+if [ "$1" == "off" ]; then
+	echo "Activating ondemand governor..."
+	# sudo cpufreq-set -r -g ondemand
+	echo ondemand | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
+else
+	echo "Activating performance governor..."
+	# sudo cpufreq-set -r -g performance
+	echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
+fi
