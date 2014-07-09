05196289016e10003869dba88820e226f1c99a1a
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-07-09 10:11:11 +0000

    showing results of turning on governor.

--- trunk/toys/full-power.sh
+++ trunk/toys/full-power.sh
@@ -12,3 +12,5 @@ echo "Activating $governor governor..."
 # sudo cpufreq-set -r -g $governor
 echo $governor | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
 
+echo "Checking:"
+head /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor 
