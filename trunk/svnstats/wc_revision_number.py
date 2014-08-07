47947933878188a6f62e47a463fe96c5ff3ade15
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-08-07 08:46:15 +0000

    svnstats/wc_revision_number.py - implemented wc.db variant (needs to be first, entries file still exists in 1.7+ working copies.

--- trunk/svnstats/wc_revision_number.py
+++ trunk/svnstats/wc_revision_number.py
@@ -2,13 +2,22 @@
 
 import os
 import sys
+import sqlite3
 
 def get_current_revision(wc_root):
+    wcdb_file = os.path.join(wc_root, '.svn', 'wc.db')
+    if os.path.exists(wcdb_file):
+        conn = sqlite3.connect(wcdb_file)
+        c = conn.cursor()
+        c.execute('select max(revision) from nodes')
+        revision = c.fetchone()[0]
+        conn.close()
+        return revision
+
     entries_file = os.path.join(wc_root, '.svn', 'entries')
     if os.path.exists(entries_file):
         # pre 1.7 format
         return list(open(entries_file))[3].strip()
-    wcdb_file = os.path.join(wc_root, '.svn', 'wc.db')
 
 def main():
     if len(sys.argv) < 2:
@@ -18,7 +27,7 @@ def main():
     elif len(sys.argv) == 4:
         version = get_current_revision(sys.argv[1])
         template = open(sys.argv[2]).read()
-        open(sys.argv[3], 'w').write(template.replace('$WCREV$', version))
+        open(sys.argv[3], 'w').write(template.replace('$WCREV$', str(version)))
 
 if __name__ == '__main__':
     main()
