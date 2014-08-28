ea141e509d9073849b1014eec9e98bf19fa126d0
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-08-28 22:55:36 +0000

    proplog.py - added options: short (just changed lines) and maxrev (limit output to changed lines). Removed now go before all.

--- trunk/svnstats/proplog.py
+++ trunk/svnstats/proplog.py
@@ -9,17 +9,24 @@ try:
     parser = argparse.ArgumentParser()
     parser.add_argument("--property", type=str, help="SVN property name (default: svn:mergeinfo)", default='svn:mergeinfo')
     parser.add_argument('--minrev', type=int, default=0) 
+    parser.add_argument('--maxrev', type=int, default=0) 
+    parser.add_argument('--short', action='store_true')
     args = parser.parse_args()
     prop = args.property
     minrev = args.minrev
+    maxrev = args.maxrev
+    short = args.short
 except ImportError:
     prop = sys.argv[1]
     minrev = 0
+    maxrev = 0
+    short = False
 
 run = lambda cmd: os.popen(cmd).read()
 val = lambda rev: run('svn pg %s -r %d' % (prop, rev))
 
-maxrev = int(re.search('r(\d+)', run('svn log -r HEAD:1 -l 1')).group(1))
+if not maxrev:
+    maxrev = int(re.search('r(\d+)', run('svn log -r HEAD:1 -l 1')).group(1))
 if not minrev:
     minrev = int(re.search('r(\d+)', run('svn log -r 1:HEAD -l 1')).group(1))
 maxval = val(maxrev)
@@ -45,8 +52,10 @@ def diff(newval, oldval):
     ins = set(nlines) - set(olines)
     out = set(olines) - set(nlines)
     return '\n'.join(
-        [ ('+%s' if line in ins else ' %s') % line for line in nlines ] +
-        [ '-%s' % line for line in olines if line in out ]
+        [ '-%s' % line for line in olines if line in out ] +
+        [ ('+%s' if line in ins else ' %s') % line 
+            for line in (ins if short else nlines) 
+        ]
     )
 
 history = [(0, 0, '')]
