83afb5aaaa1059185f43158ef18053b24760ef7d
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2014-08-29 14:45:07 +0000

    svnstats/grep_user.pl - simple counter for the matched commits.

--- svnstats/grep_user.pl
+++ svnstats/grep_user.pl
@@ -6,12 +6,18 @@ use warnings;
 die("Usage:  <username>\n") if $#ARGV == -1;
 my $user = $ARGV[0];
 my $print = 0;
+my $counter = 0;
 
 while(<STDIN>)
 {
 	if (/^r[1-9]/) { 
 		my @data = split(/\s+/);
 		$print = 1 if ($data[2] eq $user);
+		if ($data[2] eq $user)
+		{
+			$counter++;
+			print "($counter)\n";
+		}
 	}
 	if ($print) 
 	{
