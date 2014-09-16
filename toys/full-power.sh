9b0a54a43c6f4448664529dbe5e10714a9dcad69
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2014-09-16 06:36:24 +0000

    toys/full-power.sh - added commands 'show', and 'list' - to display current governors only.

--- toys/full-power.sh
+++ toys/full-power.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-if [ "$1" == "off" ]; then
+if [ "$1" = "off" ]; then
 	governor=ondemand
-elif [ "$1" == "down" ]; then
+elif [ "$1" = "down" ]; then
 	governor=powersave
 else
 	governor=performance
 fi
 
-echo "Activating $governor governor..."
-# sudo cpufreq-set -r -g $governor
-echo $governor | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
+if [ "$1" != "show" -a "$1" != "list" ] ; then
+	echo "Activating $governor governor..."
+	# sudo cpufreq-set -r -g $governor
+	echo $governor | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
+fi
 
 echo "Checking:"
 head /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor 
