1af47345cddb8f8b3f8acd69041c79fe1974cd9c
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2014-09-17 19:04:45 +0000

    lang_lawyer/firstprivate_obj.cpp - showcasing using threadprivate objects initialized from local variables. Expected n-1 copies. Found n.

---
+++ lang_lawyer/firstprivate_obj.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class ThreadPrivate_OrNot
+{
+public:
+    ThreadPrivate_OrNot() { cout << "Constructed normally at " << this << endl; }
+    ThreadPrivate_OrNot(const ThreadPrivate_OrNot& rhs) {
+        cout << "Copy-constructed from " << (void*)&rhs << " to " << this << endl;
+    }
+    void use(int n) { cout << "Used at " << this << " in iteration " << n << endl; }
+};
+
+int main()
+{
+    ThreadPrivate_OrNot tpon;
+    #pragma omp parallel for firstprivate(tpon)
+    for (int i=0; i < 8; ++i)
+    {
+        tpon.use(i);
+    }
+    return 0;
+}
