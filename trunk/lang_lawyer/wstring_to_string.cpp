72ff3f133fac045f7f5ec1952027ef99b1161c61
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-10-01 08:34:21 +0000

    lang_lawyer/wstring_to_string.cpp - simplest, low reliability wstring->string conversion.

---
+++ trunk/lang_lawyer/wstring_to_string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::wstring wide( L"Wide" ); 
+	std::string str( wide.begin(), wide.end() );
+	std::cout << str << std::endl;
+	return 0;
+}
