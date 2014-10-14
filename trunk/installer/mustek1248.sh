0d145fc8c101208dd105f1786829af5bbe55e0a5
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-10-14 16:43:13 +0000

    installer/mustek1248.sh - simple script for installing/downloading one particular SANE firmware.

---
+++ trunk/installer/mustek1248.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -f /usr/share/sane/gt68xx/SBSfw.usb ] ; then
+	echo Scanner already installed.
+	exit
+fi
+
+sudo mkdir -p /usr/share/sane/gt68xx
+if [ -f ~/SBSfw.usb ] ; then
+	echo Installing firmware from home directory...
+	sudo cp ~/SBSfw.usb /usr/share/sane/gt68xx
+else
+	echo Trying to download firmware...
+	cd /usr/share/sane/gt68xx
+	sudo wget http://www.meier-geinitz.de/sane/gt68xx-backend/firmware/SBSfw.usb
+fi
