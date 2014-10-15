707ce07c1e97432bea75ce6486a7aeec5c955808
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2014-10-15 11:54:39 +0000

    toys/qr.sh - added helper code for installing dependencies (on *buntu, at least).

--- toys/qr.sh
+++ toys/qr.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+missing=""
+
+if ! which qrencode >/dev/null ; then
+	missing="qrencode"
+fi
+
+if ! which display >/dev/null ; then
+	missing="$missing imagemagick"
+fi
+
+if [ -n "$missing" ] ; then
+	echo "Missing packages: $missing"
+	echo "Trying to install:"
+	if ! sudo apt-get install $missing ; then
+		echo Install failed
+		exit
+	fi
+fi
+
 for i in "$@"; do
 	echo "Showing $i..."
 	qrencode "$i"  -o -| display -resize 300x300% -
