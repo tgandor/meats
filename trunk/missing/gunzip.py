4e6b97d0788467dcc750ba7ef334c53f901a9b91
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-10-20 07:28:26 +0000

    missing/gunzip(.py|.bat) - added gunzip to missing; fixed newline handling in compression (could corrupt binary files.)

--- trunk/missing/gunzip.py
+++ trunk/missing/gunzip.py
@@ -9,12 +9,15 @@ if sys.version_info[0] == 2:
     input = raw_input
 
 for f in sys.argv[1:]:
-    target_f = f+'.gz'
+    if not f.endswith('.gz'):
+        print ('{0}: not a .gz file.'.format(f))
+        continue
+    target_f = f[:-3]
     if os.path.exists(target_f):
         if input('{0} :\n - file exists, overwrite (y/N)? '.format(target_f)) != 'y':
             continue
-    input_ = open(f, 'rb')
-    output = gzip.open(target_f, 'wb')
+    input_ = gzip.open(f, 'rb')
+    output = open(target_f, 'wb')
     shutil.copyfileobj(input_, output)
     input_.close()
     output.close()
