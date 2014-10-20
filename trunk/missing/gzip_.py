f0dcbc708bcc42e8f7c1a851c3cf896ddc1e3239
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-10-20 07:19:48 +0000

    missing/gzip_.py + gzip.bat - a functional pair for gzipping on Windows, even with drag-n-drop.

--- trunk/missing/gzip_.py
+++ trunk/missing/gzip_.py
@@ -1,13 +1,21 @@
 # for windows
 
 import gzip
+import os
 import shutil
 import sys
 
+if sys.version_info[0] == 2:
+    input = raw_input
+
 for f in sys.argv[1:]:
-	input_ = open(f)
-	output = gzip.open(f+'.gz', 'wb')
-	shutil.copyfileobj(input_, output)
-	input_.close()
-	output.close()
-	print('{0} -> {1}'.format(f, f+'.gz'))
+    target_f = f+'.gz'
+    if os.path.exists(target_f):
+        if input('{0} :\n - file exists, overwrite (y/N)? '.format(target_f)) != 'y':
+            continue
+    input_ = open(f)
+    output = gzip.open(target_f, 'wb')
+    shutil.copyfileobj(input_, output)
+    input_.close()
+    output.close()
+    print('{0} -> {1}'.format(f, target_f))
