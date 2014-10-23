f022c149223ab7e7de031eebd6f6975b9a1f739c
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-10-23 08:45:58 +0000

    lang_lawyer/static_const_member.cpp - analyzing the need to define static const members.

---
+++ trunk/lang_lawyer/static_const_member.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+#ifdef _MSC_VER
+
+class Foo 
+{
+public:
+	static const int MEMBER = 1; 
+};
+
+#else
+
+// e.g. GCC
+
+class Foo 
+{
+public:
+	static const int MEMBER;
+};
+
+const int Foo::MEMBER = 1; 
+
+#endif
+
+int main()
+{
+	std::vector<int> v;
+	v.push_back( Foo::MEMBER );       // by reference
+	v.push_back( (int) Foo::MEMBER ); // by value
+	return 0;
+}
