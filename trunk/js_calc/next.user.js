3563f272709476062e08365ed1e1f12f3bc2c306
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-11-06 20:15:58 +0000

    js_calc/next.user.js - another userscript, for jumping to next and previous pages on some websites.

---
+++ trunk/js_calc/next.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name           PrevNext
+// @description    Add buttons to navigate to pages with successive numbers.
+// @include        *
+// @version        1.0
+// ==/UserScript==
+
+function next(delta)
+{
+	var match = /\d*$/.exec(window.location.href)[0];
+	if (match)
+	{
+		next_id = parseInt(match) + delta;
+		window.location = window.location.href.replace(/\d+$/, next_id);
+	}
+	else
+	{
+		alert('Cannot deduce next page.');
+	}
+}
+
+function make_button(value, left)
+{
+	var button = document.createElement('input');
+	button.type = 'button';
+	button.value = value;
+	button.style.position = 'fixed';
+	button.style.top = 0;
+	button.style.left = left;
+	document.body.appendChild(button);
+	return button;
+}
+
+prevButton = make_button('<', 0);
+nextButton = make_button('>', '1em');
+
+function make_handler(delta)
+{
+	return function(e) {
+		if (e.which == 2)
+		{
+			document.body.removeChild(prevButton);
+			document.body.removeChild(nextButton);
+		}
+		else
+		{
+			next(delta);
+		}
+	};
+}
+
+nextButton.onclick = make_handler(1);
+prevButton.onclick = make_handler(-1);
