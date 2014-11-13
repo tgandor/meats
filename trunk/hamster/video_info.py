fcba94d70188de63cce3d693fdfd4b5853d40259
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-11-13 14:33:56 +0000

    hamster/video_info.py - added playlist handling.

--- trunk/hamster/video_info.py
+++ trunk/hamster/video_info.py
@@ -8,10 +8,28 @@ if len(sys.argv) < 2:
     print 'Usage: %s video_id...' % (sys.argv[0],)
     exit()
 
-for video_id in sys.argv[1:]:
+def video_info(video_id):
     json_info = urllib2.urlopen('http://gdata.youtube.com/feeds/api/videos/%s?v=2&alt=jsonc' % video_id).read()
     info = json.loads(json_info)
     print json.dumps(info, indent=2)
     print 'Specifically,'
     print 'Title: %s' % info['data']['title']
     print 'Description:\n%s' % info['data']['description']
+
+def playlist_info(playlist_id):
+    json_info = urllib2.urlopen('http://gdata.youtube.com/feeds/api/playlists/%s?v=2&alt=jsonc' % playlist_id).read()
+    info = json.loads(json_info)
+    print 'Playlist: "%s"' % info['data']['title']
+    print info['data']['description']
+    print 'Items:'
+    for item in info['data']['items']:
+        print '-'*50
+        print 'Position:', item['position']
+        print 'Title:', item['video']['title']
+        print item['video']['description']
+
+for some_id in sys.argv[1:]:
+    if len(some_id) >= 16:
+        playlist_info(some_id)
+    else:
+        video_info(some_id)
