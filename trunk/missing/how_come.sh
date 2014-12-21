991c7dd59bf10c4fd3f22602c36d1989056187f0
Tomasz.Gandor@gmail.com <Tomasz.Gandor@gmail.com@631cc9f1-9db8-3932-6d94-d95ba223ddb7>
2014-12-21 22:46:45 +0000

    how_come.sh - little script to retrieve which package a command comes from.

---
+++ trunk/missing/how_come.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+	echo "Usage: $0 <command>"
+	echo "Prints package and apt-get command for installing it."
+	exit
+fi
+
+if ! which $1; then
+	echo "Command not found."
+	exit
+fi
+
+cmd_path=`which $1`
+
+
+dpkg -S $cmd_path | awk '{ gsub(/:$/, "", $1); print "sudo apt-get install " $1;}'
+echo "---"
+echo 'missing=""'
+dpkg -S $cmd_path | awk '{ gsub(/:$/, "", $1); print "if ! which '$1' >/dev/null; then\n\tmissing=\"$missing " $1 "\"\nfi"; }'
+echo 'sudo apt-get install $missing'
