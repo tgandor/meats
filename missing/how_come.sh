2cbd921ace58959a32cb9377a2604a644feccecb
Tomasz.Gandor@gmail.com <tgandor@users.noreply.github.com>
2014-12-23 11:36:16 +0000

    svn_co - checkout from SVN right directory with showing time

--- missing/how_come.sh
+++ missing/how_come.sh
@@ -18,4 +18,9 @@ dpkg -S $cmd_path | awk '{ gsub(/:$/, "", $1); print "sudo apt-get install " $1;
 echo "---"
 echo 'missing=""'
 dpkg -S $cmd_path | awk '{ gsub(/:$/, "", $1); print "if ! which '$1' >/dev/null; then\n\tmissing=\"$missing " $1 "\"\nfi"; }'
-echo 'sudo apt-get install $missing'
+
+echo 'if [ -n "$missing" ] ; then'
+echo '    echo "Missing packages: $missing"'
+echo '    sudo apt-get install $missing'
+echo 'fi'
+
