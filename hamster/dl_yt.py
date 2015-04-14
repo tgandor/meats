ab70303dc504081dab1d2e139d1fafe4d62988f8
tgandor <tomasz.gandor@gmail.com>
2015-04-14 12:55:35 +0200

    dl_yt - simplified, no kivy-mode needed
    Handles utf-8 OK, relies on ctypes and youtube_dl being installed
    in QPython's site-packages (looks more serious that way).

--- hamster/dl_yt.py
+++ hamster/dl_yt.py
@@ -1,24 +1,12 @@
 #!/usr/bin/env python
-from __future__ import unicode_literals
 
-import sys
+# install youtube_dl and fake ctypes in lib/python2.7/site-packages
 
-if __package__ is None and not hasattr(sys, "frozen"):
-    # direct call of __main__.py
-    import os.path
-    path = os.path.realpath(os.path.abspath(__file__))
-    sys.path.append(os.path.dirname(path))
-
+import os
 import youtube_dl
-
 import androidhelper
+
 print "trying to retrieve from clipboard"
-'''
-try:
-	os.chdir('/mnt/extSdCard/Music')
-except:
-    print 'failed chdir /mnt/extSdCard/Music'
-'''
 
 try:
     os.chdir('/mnt/sdcard/Download')
@@ -30,10 +18,6 @@ print 'working in', os.getcwd()
 the_url = androidhelper.Android().getClipboard().result
 
 the_url = the_url.split()[-1]
-
-# ignoring hacks:
-sys.stderr.isatty = lambda: False
-sys.stderr.flush = lambda: None
 
 if __name__ == '__main__':
     youtube_dl.main([the_url])
