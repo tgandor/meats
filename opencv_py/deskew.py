db0b03ae804fb8cfae830d0ad081f127144814a9
tgandor <tomasz.gandor@gmail.com>
2015-04-29 22:58:20 +0200

    deskew.py - finally finding a promising angle to rotate around
    - find median angle of all lines (ignoring point order, probably
    determined correctly by cv2 anyway), rotate by it
    Additionally:
    - show_fit made 'interactive' zoom in and out by powers of 2
    - cv_center for image center

--- opencv_py/deskew.py
+++ opencv_py/deskew.py
@@ -12,6 +12,11 @@ def cv_size(img):
     return tuple(img.shape[1::-1])
 
 
+def cv_center(img):
+    w, h = cv_size(img)
+    return (w/2, h/2)
+
+
 def get_screen_res():
     if sys.platform.startswith('linux'):
         import os
@@ -46,7 +51,24 @@ def show_fit(img, name='preview', expand=False, rotate=False):
         to_show = cv2.resize(img, (w1, h1))
     else:  # expand ...
         raise NotImplementedError
-    cv2.imshow(name, to_show)
+
+    scale = 1.0
+    while True:
+        cv2.imshow(name, to_show)
+        key = cv2.waitKey(0)
+        char_code = key % 256
+        if char_code == ord(' '):
+            break
+        if char_code == ord('+'):
+            scale *= 2
+            to_show = cv2.resize(img, (int(w1*scale), int(h1*scale)))
+            continue
+        if char_code == ord('-'):
+            scale /= 2
+            to_show = cv2.resize(img, (int(w1*scale), int(h1*scale)))
+            continue
+        if char_code == ord('q'):
+            exit()
 
 
 def main():
@@ -56,11 +78,16 @@ def main():
     # _, region = cv2.threshold(img, 128.0, 255.0, cv2.THRESH_BINARY_INV)
     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
     lines = cv2.HoughLinesP(edges, rho=1, theta=math.pi/180.0, threshold=80, minLineLength=60, maxLineGap=10)
-    print lines
+    # print lines
+    n = len(lines[0])
     for x1, y1, x2, y2 in lines[0]:
         cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    show_fit(img, rotate=True)
-    cv2.waitKey(0)
+    angles = sorted([math.atan2(y2-y1, x2-x1) for x1, y1, x2, y2 in lines[0]])
+    print("There's {0} lines.".format(n))
+    print('The middle angle is: {0}'.format(angles[n/2]))
+    img_rotated = cv2.warpAffine(img, cv2.getRotationMatrix2D(cv_center(img), angles[n/2]*180/math.pi, 1.0), cv_size(img))
+    show_fit(img_rotated, rotate=False)
+
 
 if __name__ == '__main__':
     main()
