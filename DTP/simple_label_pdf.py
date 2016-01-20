db3b5440ad8054d07c2900a2d1bb4da4d5645106
tgandor <tomasz.gandor@gmail.com>
2016-01-20 16:00:44 +0100

    DTP/simple_label_pdf.py - refactoring, fonts, thinner line, defaults (need better still), launching result.

--- DTP/simple_label_pdf.py
+++ DTP/simple_label_pdf.py
@@ -6,14 +6,17 @@ from reportlab.lib.pagesizes import A4
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 import datetime
+import os
+import sys
 
 font_size = 16
-width = 11*cm
-height = 16*cm
+default_width = 20*cm
+default_height = 10*cm
+default_output_file = 'label_output.pdf'
 fonts_to_try = ['Ubuntu-L', 'Verdana', 'Arial']
 
 
-def _setup_canvas(outfile='hello.pdf'):
+def _setup_canvas(outfile=default_output_file):
     for font_name in fonts_to_try:
         try:
             font = TTFont(font_name, font_name+'.ttf')
@@ -30,10 +33,11 @@ def _setup_canvas(outfile='hello.pdf'):
     return c
 
 
-def label(c, text, width=width, height=height):
+def label(c, text, width=default_width, height=default_height):
     c.saveState()
     c.setDash(2, 8)
     inv = lambda x: A4[1]-x
+    c.setLineWidth(0.25)
     c.line(0, inv(height), width, inv(height))
     c.line(width, inv(0), width, inv(height))
     c.drawCentredString(width/2, inv((height+font_size)/2), text)
@@ -42,8 +46,21 @@ def label(c, text, width=width, height=height):
     c.restoreState()
 
 
-custom = raw_input('Enter label text: ') or 'Default label...'
-c = _setup_canvas()
-label(c, custom)
-c.showPage()
-c.save()
+def main():
+    custom = raw_input('Enter label text: ') or '.' * 30
+    in_width = raw_input('Width (%.1f): ' % (default_width/cm))
+    in_height = raw_input('Height (%.1f): ' % (default_height/cm))
+    width = float(in_width)*cm if in_width else default_width
+    height = float(in_height)*cm if in_height else default_height
+    c = _setup_canvas()
+    label(c, custom, width, height)
+    c.showPage()
+    c.save()
+    if sys.platform.startswith('linux'):
+        os.system('xdg-open "%s"' % default_output_file)
+    else:
+        os.system('start "%s"' % default_output_file)
+
+
+if __name__=='__main__':
+    main()
