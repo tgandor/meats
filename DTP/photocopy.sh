dd041046a44dc77a876ac89ac4fbfa1115dd125d
tgandor <tomasz.gandor@gmail.com>
2016-03-09 23:54:09 +0100

    photocopy.sh - added "savable" option, with possible description.

--- DTP/photocopy.sh
+++ DTP/photocopy.sh
@@ -20,9 +20,21 @@ if [ -n "$missing" ] ; then
 	fi
 fi
 
-tmpdf=/tmp/photocopy.tif
+if [ "$1" == "-s" ] ; then
+	if [ -n "$2" ] ; then
+		extra="_$2"
+	fi
+	tmpdf=`date +"%Y-%m-%d_%H-%M-%S"`$extra.tif
+	title=$tmpdf
+	
+else
+	tmpdf=/tmp/photocopy.tif
+	title=Photocopy
+fi
+
+echo "Scanning to: $tmpdf"
+
 scanimage -p --format=tiff > $tmpdf
 # shaving and thresholding may be scanner-specific
 mogrify -level-colors "gray(35%),gray(85%)" -shave 80x30-80-30 $tmpdf
-lpr -T "Photocopy" $tmpdf
-# rm $tmpdf
+lpr -T $title $tmpdf
