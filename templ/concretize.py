35f0f265e74524820c75fecee58f6ae58152e161
tgandor <tomasz.gandor@gmail.com>
2016-03-18 12:38:52 +0100

    templ/concretize.py - a flexible file templating engine

---
+++ templ/concretize.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+replacement = next((arg for arg in sys.argv if not os.path.exists(arg)), None)
+
+if not replacement:
+    print("Usage: {0} REPLACEMENT FILE...".format(sys.argv[0]))
+    exit()
+
+for target in filter(os.path.exists, sys.argv[1:]):
+    destination = os.path.basename(target).replace('Empty', replacement)
+    contents = open(target).read().replace('Empty', replacement)
+    print("Generating {0}...".format(destination))
+    open(destination, 'w').write(contents)
