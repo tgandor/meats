0008984aefd0d3a2a5e54bdce3b4ff8140628756
tgandor <tomasz.gandor@gmail.com>
2016-04-04 13:17:35 +0200

    toys/any2mp3.sh - minor update; dirty tool

--- toys/any2mp3.sh
+++ toys/any2mp3.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+if which avconv ; then 
+	conv=avconv
+else
+	conv=ffmpeg
+fi
+
 for f in "$@"; do
-	echo "$f -> `basename "$f"`.mp3"
-	ffmpeg -loglevel panic -i "$f" -vn -ab 128k -y `basename "$f"`.mp3 2>&1 | grep -v "configuration:" --line-buffered
+	new_name=`echo $f | sed 's/.ogg$\|.m4a$\|.flv$\|.mp4$/.mp3/'`
+	if [ "$f" == "$new_name" ] ; then
+		echo "$f - target same as source"
+	else
+		echo "$f -> $new_name"
+		$conv -i "$f" -vn -y "`basename "$new_name"`" 2>&1
+	fi
 done
