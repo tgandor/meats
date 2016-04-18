14c8750981730c1589c98bba94857e34cbde1cf6
tgandor <tomasz.gandor@gmail.com>
2016-04-18 14:51:13 +0200

    user_hz.cpp - added explaining comments

--- lang_lawyer/posix/user_hz.cpp
+++ lang_lawyer/posix/user_hz.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <unistd.h>
 
+// actually this can be done in the shell:
+// $ getconf CLK_TCK
+// and this just shows the jiffies per second, arbitrarily configured as 100 Hz (10 ms)
+// and not related to internal kernel interrupts
 int main()
 {
 	std::cout << sysconf(_SC_CLK_TCK) << std::endl;
