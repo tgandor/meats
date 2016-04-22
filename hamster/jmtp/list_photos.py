7759c9eb0ca6b9f796bcd4281d4967f098a3eeb0
tgandor <tomasz.gandor@gmail.com>
2016-04-22 23:46:59 +0200

    jmtp/list_photos.py - listing both phone and SD card.

--- hamster/jmtp/list_photos.py
+++ hamster/jmtp/list_photos.py
@@ -4,13 +4,20 @@ import glob
 import json
 import os
 
+
+def dump_photos(photo_dir, mounted_dir, target_json):
+    files = list(sorted(glob.glob(os.path.join(photo_dir, '*'))))
+    mounted_files = [f.replace(photo_dir, mounted_dir) for f in files]
+    with open(target_json, 'w') as f:
+        json.dump(mounted_files, f, indent=4)
+
 photo_dir = '/mnt/extSdCard/DCIM/Camera'
 mounted_dir = 'mnt/Card/DCIM/Camera'
+target_json = os.path.join(os.path.dirname(__file__), 'photos_card.json')
+dump_photos(photo_dir, mounted_dir, target_json)
 
-files = list(sorted(glob.glob(os.path.join(photo_dir, '*'))))
-mounted_files = [f.replace(photo_dir, mounted_dir) for f in files]
+photo_dir = '/mnt/sdcard/DCIM/Camera'
+mounted_dir = 'mnt/Phone/DCIM/Camera'
+target_json = os.path.join(os.path.dirname(__file__), 'photos_phone.json')
+dump_photos(photo_dir, mounted_dir, target_json)
 
-target_json = os.path.join(os.path.dirname(__file__), 'photos.json')
-
-with open(target_json, 'w') as f:
-    json.dump(mounted_files, f, indent=4)
