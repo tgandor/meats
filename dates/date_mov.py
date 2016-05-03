7f75e33b3089798d731de3db206bbeef6f0e19a8
tgandor <tomasz.gandor@gmail.com>
2016-05-03 09:35:15 +0200

    date_mov.py - unified format. Results are off by about 25 min from exiftool.

--- dates/date_mov.py
+++ dates/date_mov.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
+# Description outdated:
 # This is a oneliner to print the creation date in a mov file
 # developed with python -c ;)
 
 # usage: date_mov.py dscn0001.mov
 
-if __name__=='__main__':
-    import time, datetime, struct, re, sys; print datetime.datetime.fromtimestamp(float(struct.unpack(">I", re.search("mvhd.{4}(.{4})", open(sys.argv[1]).read()).group(1))[0])+time.mktime(datetime.datetime(1904,1,1).timetuple()))
+import time, datetime, struct, re, sys
 
+def info(f):
+    dt = datetime.datetime.fromtimestamp(
+        float(struct.unpack(">I", re.search("mvhd.{4}(.{4})", open(f).read()).group(1))[0])
+        + time.mktime(datetime.datetime(1904,1,1).timetuple())
+    )
+    print('{} ; {}'.format(f, time.strftime("%Y-%m-%d (%a) %H:%M:%S", dt.timetuple())))
+
+if __name__=='__main__':
+    map(info, sys.argv[1:])
