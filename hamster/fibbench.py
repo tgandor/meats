84b500e16d2b00c24233e7f4d98434a62544681a
tgandor <tomasz.gandor@gmail.com>
2016-05-17 15:36:54 +0200

    hamster/fibbench.py - python benchmarking calls per second via Fibonacci.

---
+++ hamster/fibbench.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+import time
+
+def fib(n):
+    if n < 2:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+if __name__=='__main__':
+    start = time.time()
+    n = int(sys.argv[1]) if len(sys.argv) >= 2 else 34
+    fib_n = fib(n)
+    elapsed = time.time() - start
+    calls = 2*fib_n-1
+    print("fib({}) = {}; {:.2f} s elapsed. {:,} calls, {:,.0f} c/s.".format(
+        n, fib_n, elapsed, calls, calls/elapsed
+    ))
