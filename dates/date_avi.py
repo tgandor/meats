55072c2a4ac8c930da99b190f0a985e827205a12
tgandor <tomasz.gandor@gmail.com>
2016-07-20 20:28:59 +0200

    dates/date_avi.py - Py3 compatibility.

--- dates/date_avi.py
+++ dates/date_avi.py
@@ -9,11 +9,11 @@ import sys
 import time
 
 def info(f):
-    some_data = open(f).read(2**11)
-    date_string = re.search('([A-Z][a-z]{2} ){2}\d\d?([ :]\d\d){4}\d\d', some_data).group()
+    some_data = open(f, 'rb').read(2**11)
+    date_string = re.search(b'([A-Z][a-z]{2} ){2}\d\d?([ :]\d\d){4}\d\d', some_data).group().decode()
     # print(date_string)
     parsed_date = time.strptime(date_string, "%a %b %d %H:%M:%S %Y")
     print('{} ; {}'.format(f, time.strftime("%Y-%m-%d (%a) %H:%M:%S", parsed_date)))
 
 if __name__=='__main__':
-    map(info, sys.argv[1:])
+    list(map(info, sys.argv[1:]))
