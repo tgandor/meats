367aeaa8bdbab541a295b9ee1e929db749f0b015
tgandor <tomasz.gandor@gmail.com>
2016-08-12 16:17:25 +0200

    toys/name_incr.py - increment all decimal ints in argument filename(s).

---
+++ toys/name_incr.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import re
+import shutil
+import sys
+
+
+def increment_ints(s):
+    return re.sub('\d+', lambda s: str(int(s.group()) + 1), s)
+
+
+if __name__ == '__main__':
+    for f in sys.argv[1:]:
+        fpp = increment_ints(f)
+        if not os.path.exists(fpp):
+            print('{} -> {}'.format(f, fpp))
+            shutil.copy(f, fpp)
