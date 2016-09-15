fef6e20ffe389d86d96091bdb9a6eabdaeff3c91
tgandor <tomasz.gandor@gmail.com>
2016-09-15 11:55:17 +0200

    missing/archof.bat - print architecture of the argument (using dumpbin)

---
+++ missing/archof.bat
@@ -0,0 +1,22 @@
+@echo off
+
+if "%1"=="" (
+    echo Usage: %0 file.exe
+    exit /b
+)
+
+:: find newest VS tools
+
+set BEST_TOOLS=none
+for /F "usebackq tokens=1,2 delims==" %%x in (`set vs`) do (
+    echo %%x | findstr "TOOLS" >nul && set BEST_TOOLS=%%y
+)
+
+if "%BEST_TOOLS%"=="none" (
+    echo No VS tools found.
+    exit /b
+)
+
+:: echo Best tools found %BEST_TOOLS%
+
+"%BEST_TOOLS%..\..\VC\bin\dumpbin.exe" /headers "%1" | findstr "machine"
