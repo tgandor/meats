d503609a69dcb052ddee4c961aaeda6b58a162c0
tgandor <tomasz.gandor@gmail.com>
2016-10-07 12:37:00 +0200

    check_vs_cmd - setlocal: preserving existing environment; calling VS2015 scripts if missing

--- missing/check_vs_cmd.bat
+++ missing/check_vs_cmd.bat
@@ -1,2 +1,4 @@
 @rem Check CL in path without output 
-@where cl >nul 2>nul && echo present || echo missing
+@setlocal
+@where cl >nul 2>nul && echo present || echo missing, adding && call "%VS140COMNTOOLS%vsvars32.bat"
+@where cl >nul 2>nul && cl || echo VS 2015 still not found
