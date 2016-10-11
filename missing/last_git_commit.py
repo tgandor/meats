f455c866037d4ebf7ed1968acadefe1f4bc1a2c7
tgandor <tomasz.gandor@gmail.com>
2016-10-11 14:14:47 +0200

    missing/last_git_commit.py - added clipboard copy (via Tk), improved repo URL.

--- missing/last_git_commit.py
+++ missing/last_git_commit.py
@@ -1,28 +1,51 @@
 #!/usr/bin/env python
 
 import os
+import sys
 
 
 def make_commit_url(repo_url, commit_hash):
     return '{}/commit/{}'.format(repo_url, commit_hash)
-    
-    
+
+
 def repo_name(repo_url):
     return repo_url.split('/')[-1]
-    
-   
+
+
+def make_repo_url(fetch_url):
+    return fetch_url[:-4] if fetch_url.endswith('.git') else fetch_url
+
+
 last_commit = os.popen("git log -1").readlines()
 remote = os.popen("git remote -vv").readlines()
 
 # print last_commit, remote
 
 commit_hash = last_commit[0].split()[1]
-repo_url = remote[0].split()[1]
+repo_url = make_repo_url(remote[0].split()[1])
 description = ''.join(last_commit[4:])
 
-print("[{} @ {}|{}] {}".format(
+message = "[{} @ {}|{}] {}".format(
     commit_hash[:10], 
     repo_name(repo_url), 
     make_commit_url(repo_url, commit_hash),
-    description
-))
+    description.lstrip()
+)
+print(message)
+
+try:
+    if sys.version_info.major == 2:
+        import Tkinter as Tk
+    else:
+        import tkinter as Tk
+        raw_input = input
+    r = Tk.Tk()
+    r.withdraw()
+    r.clipboard_clear()
+    r.clipboard_append(message)
+    print('(copied to clipboard)')
+    if sys.platform == 'linux2':
+        raw_input('Press ENTER after pasting... ')
+    r.destroy()
+except ImportError:
+    pass
