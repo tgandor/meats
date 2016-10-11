fd31a3eaa5fee8a4c9097dcd224ed062b4a2d749
tgandor <tomasz.gandor@gmail.com>
2016-10-11 12:18:00 +0200

    jmtp/install.sh - added condition if jmtpfs mount failed.

--- hamster/jmtp/install.sh
+++ hamster/jmtp/install.sh
@@ -2,7 +2,10 @@
 
 source=`realpath $1`
 pushd `dirname $0`/..
-jmtp/mount.sh
+if ! jmtp/mount.sh ; then
+	echo "Error: not able to mount phone (nothing installed)"
+	exit
+fi
 cp -v $source mnt/Phone/com.hipipal.qpyplus/scripts/
 jmtp/umount.sh
 popd
