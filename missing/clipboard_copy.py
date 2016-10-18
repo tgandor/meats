ffb89db82605f6d9d7a06c704ef73dccf8f62ddb
tgandor <tomasz.gandor@gmail.com>
2016-10-19 00:04:50 +0200

    clipboard_copy.py - reorganized (extracted function)

--- missing/clipboard_copy.py
+++ missing/clipboard_copy.py
@@ -2,35 +2,42 @@
 
 import sys
 
-try:
-    import Tkinter as Tk
-except ImportError:
-    # welcome to Python3
-    import tkinter as Tk
-    raw_input = input
 
-r = Tk.Tk()
-r.withdraw()
-r.clipboard_clear()
+def copy(data, wait_gui=False):
+    try:
+        import Tkinter as Tk
+    except ImportError:
+        # welcome to Python3
+        import tkinter as Tk
+        raw_input = input
 
-if len(sys.argv) < 2:
-    data = sys.stdin.read()
-else:
-    data = ' '.join(sys.argv[1:])
+    r = Tk.Tk()
+    r.withdraw()
+    r.clipboard_clear()
+    r.clipboard_append(data)
 
-r.clipboard_append(data)
-
-if sys.platform != 'win32':
-    if len(sys.argv) > 1:
-        raw_input('Data was copied into clipboard. Paste and press ENTER to exit...')
+    if sys.platform != 'win32':
+        if not wait_gui:
+            raw_input('Data was copied into clipboard. Paste and press ENTER to exit...')
+            r.destroy()
+        else:
+            # stdin already read; use GUI to exit
+            print('Data was copied into clipboard. Paste, then close popup to exit...')
+            Tk.Button(r, text='Click or press key to exit', command=r.destroy, width=40, height=10).pack(fill=Tk.BOTH)
+            r.title('Clipboard Copy')
+            r.bind('<Key>', lambda e: r.destroy() if e.keysym != 'Alt_L' else None)
+            r.deiconify()
+            r.mainloop()
     else:
-        # stdin already read; use GUI to exit
-        print('Data was copied into clipboard. Paste, then close popup to exit...')
-        Tk.Button(r, text='Click or press key to exit', command=r.destroy, width=40, height=10).pack(fill=Tk.BOTH)
-        r.title('Clipboard Copy')
-        r.bind('<Key>', lambda e: r.destroy() if e.keysym != 'Alt_L' else None)
-        r.deiconify()
-        r.mainloop()
-else:
-    r.destroy()
+        r.destroy()
 
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        data = sys.stdin.read()
+        copy(data, True)
+    else:
+        data = ' '.join(sys.argv[1:])
+        copy(data)
