f7569e184b6a22feee1e1a4a2354909751421947
tgandor <tomasz.gandor@gmail.com>
2016-10-20 09:49:21 +0200

    wcgrep.bat - documented call, excluded binary files (/P) and filtered against ".git\"

--- missing/wcgrep.bat
+++ missing/wcgrep.bat
@@ -2,7 +2,13 @@
 
 if "%1"=="" goto :help
 
-findstr /S /R "%1" *
+:: /S - search recursively through directories
+:: /R - regex
+:: /P - only files with printable characters ("text files")
+:: /L - literal pattern (not regex)
+:: /V - (grep -v) negate selection
+
+findstr /S /R /P "%1" * | findstr /L /V ".git\\"
 goto:eof
 
 :help
