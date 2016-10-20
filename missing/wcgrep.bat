86070210ed29940de6ee913389bca7b5d3bf4bc8
tgandor <tomasz.gandor@gmail.com>
2016-10-20 13:09:16 +0200

    wcgrep.bat - added line numbers, passing all arguments to findstr

--- missing/wcgrep.bat
+++ missing/wcgrep.bat
@@ -7,9 +7,11 @@ if "%1"=="" goto :help
 :: /P - only files with printable characters ("text files")
 :: /L - literal pattern (not regex)
 :: /V - (grep -v) negate selection
+:: /N - print matching line number after filename
 
-findstr /S /R /P "%1" * | findstr /L /V ".git\\"
+findstr /S /R /P /N %* * | findstr /L /V ".git\\"
 goto:eof
 
 :help
-echo Usage: wcgrep[.bat] PATTERN
+echo Usage: wcgrep[.bat] [/I] PATTERN
+echo   /I - case insensitive
