e3392d58fcc803a31d66ad0163df7feb705909aa
tgandor <tomasz.gandor@gmail.com>
2016-10-26 11:58:21 +0200

    xargs.py - added handling of batch sizes (default all arguments, instead of 1 by 1)

--- missing/xargs.py
+++ missing/xargs.py
@@ -3,12 +3,26 @@
 import os
 import sys
 
+# take all, arguments[:None] == arguments[:]
+batch_size = None
+
 if len(sys.argv) < 2:
     print('Usage: xargs.py <command_prefix>')
     exit()
 
-prefix = ' '.join(sys.argv[1:]) + ' '
-for argument in sys.stdin.readlines():
-    command = prefix + argument.strip()
+argv = sys.argv[1:]
+
+if argv[0] == '-n' and len(argv) > 1:
+    batch_size = int(argv[1])
+    argv = argv[2:]
+
+prefix = ' '.join(argv) + ' '
+
+arguments = [argument.strip() for argument in sys.stdin.readlines()]
+
+while arguments:
+    batch = arguments[:batch_size]
+    command = prefix + ' '.join(batch)
     print(command)
     os.system(command)
+    arguments = arguments[len(batch):]
