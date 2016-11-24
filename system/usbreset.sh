c17b03ccb0ad7cd7d0f2bea3a8410b2c85b5368d
tgandor <tomasz.gandor@gmail.com>
2016-11-24 10:13:42 +0000

    system/usbreset - reset USB devices programmatically in Linux.

---
+++ system/usbreset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+usbreset_bin=$HOME/bin/usbreset 
+
+if [ -z "$1" ] ; then 
+	echo "Usage: $0 device-filename"
+	echo "Example $0 /dev/bus/usb/001/011"
+	exit
+fi
+
+if [ ! -f $usbreset_bin ] ; then
+	gcc `dirname $0`/usbreset.c -o $usbreset_bin 
+fi
+
+sudo $usbreset_bin "$@"
+
