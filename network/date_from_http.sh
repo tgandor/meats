59f9ee788c6af93f258c3f198ccc51a1eb481f97
tgandor <tomasz.gandor@gmail.com>
2016-11-27 18:04:29 +0100

    network/date_from_http.sh - only really set when -s is passed as .

--- network/date_from_http.sh
+++ network/date_from_http.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo "Usage: $0 URL"
+	echo "Usage: $0 [-s] URL"
 	exit
 fi
 
-# warm up sudo
-sudo pwd > /dev/null
+
+if [ "$2" == "-s" ]; then
+	# warm up sudo
+	sudo pwd > /dev/null
+	elevate=sudo
+	switch="-u -s"
+else
+	elevate=""
+	switch="-d"
+fi
 
 utcdate=`wget -S --spider $1 2>&1 | awk '/Date:/ { sub(" *Date: +", ""); print; exit }'`
-echo "$utcdate"
-sudo date -u -s "$utcdate"
 
+echo "Local: "`date`
+$elevate date $switch "$utcdate"
