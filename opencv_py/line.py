0b1e7f46069ccaedcc7c4610a031b7ecf9a89589
tgandor <tomasz.gandor@gmail.com>
2016-12-19 11:27:01 +0100

    opencv_py/line.py - show image with semi-autosize (adjust once).

--- opencv_py/line.py
+++ opencv_py/line.py
@@ -3,7 +3,9 @@ import cv2
 # Create a black image
 img = np.zeros((512,512,3), np.uint8)
 # Draw a diagonal blue line with thickness of 5 px
-cv2.line(img,(0,0),(511,511),(255,0,0),5)
-cv2.imshow('img',img)
+cv2.line(img, (0,0), (511,511), (255,0,0), 5)
+cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('img', 512, 512)
+cv2.imshow('img', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
