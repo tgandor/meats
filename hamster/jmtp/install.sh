cbf50f24514a9511f5936f67411a7400570dbfdf
tgandor <tomasz.gandor@gmail.com>
2016-12-28 12:21:53 +0100

    jmtp/install.sh - adjusted for other phones (known ones)

--- hamster/jmtp/install.sh
+++ hamster/jmtp/install.sh
@@ -6,10 +6,27 @@ if ! jmtp/mount.sh ; then
 	echo "Error: not able to mount phone (nothing installed)"
 	exit
 fi
-if [ -z "$1" ] ; then
-	mc mnt/Phone/com.hipipal.qpyplus/scripts/
-else
-	cp -v $source mnt/Phone/com.hipipal.qpyplus/scripts/
+
+target_dir=mnt/Phone/com.hipipal.qpyplus/scripts
+if [ ! -d $target_dir ] ; then
+	target_dir=mnt/*/com.hipipal.qpyplus/scripts
 fi
+if [ ! -d $target_dir ] ; then
+	target_dir=mnt/*/qpython/scripts
+fi
+
+if [ ! -d $target_dir ] ; then
+	echo "Sorry, scripts directory not found, opening mountpoint"
+	mc mnt
+	jmtp/umount.sh
+	exit
+fi
+
+if [ -z "$1" ] ; then
+	mc $target_dir
+else
+	cp -v $source $target_dir
+fi
+
 jmtp/umount.sh
 popd
