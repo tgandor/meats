b758cee161dcb3e5dda7770e8b6448c0c050e004
tgandor <tomasz.gandor@gmail.com>
2017-01-02 20:51:40 +0100

    DTP/stripes.py - added argparse, and top and bottom margins.

--- DTP/stripes.py
+++ DTP/stripes.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
+import argparse
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import cm
 import os
 import sys
 
+parser = argparse.ArgumentParser()
+parser.add_argument('count', type=int, default=11, nargs='?', help='Number of stripes')
+parser.add_argument('--top', type=float, default=0.0, help='Margin top [cm]')
+parser.add_argument('--bottom', type=float, default=0.0, help='Margin bottom [cm]')
+args = parser.parse_args(sys.argv[1:])
 
-count = int(sys.argv[1]) if (len(sys.argv) > 1) else 11
+count = args.count
 print('Will print {} lines (for {} stripes)'.format(count-1, count))
 
-distance = A4[1] / count
+distance = (A4[1] - args.top*cm - args.bottom*cm) / count
 print('Each stripe will be {:.3f} cm wide.'.format(distance / cm))
 
 c = canvas.Canvas('out.pdf')
@@ -18,7 +24,8 @@ c.setDash(4, 8)
 c.setLineWidth(0.25)
 
 for i in range(1, count):
-    c.line(0, i*distance, A4[0], i*distance)
+    y = args.bottom*cm + i*distance
+    c.line(0, y, A4[0], y)
 
 c.save()
 
