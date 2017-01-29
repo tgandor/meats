92fbb049f5b9bcb6e78fad24871d19df68b07334
tgandor <tomasz.gandor@gmail.com>
2017-01-29 22:50:41 +0100

    scan.py - custom scan size in mm, lazy loaded sane, PEP8/lint fixes etc.

--- DTP/scan.py
+++ DTP/scan.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 import os
 import sys
+import time
 import threading
 
 try:
     if sys.version_info.major == 2:
         import Tkinter as tk
-        from Tkinter import Frame, Tk, Button, BOTH, Label, Entry, StringVar, Spinbox, NORMAL, DISABLED
         import tkMessageBox
     else:
         import tkinter as tk
-        from tkinter import Frame, Tk, Button, BOTH, Label, Entry, StringVar, Spinbox, NORMAL, DISABLED
         from tkinter import messagebox as tkMessageBox
 except ImportError:
+    tk, tkMessageBox = None, None
     os.system('sudo apt-get install python-tk')
     print('Please re-run to use Tkinter')
     exit()
 
 
-
 try:
     import sane
 except ImportError:
+    sane = None
     print('Could not load sane')
     cmd = 'sudo apt-get install sane sane-utils python{}-imaging-sane'.format('3' if sys.version_info.major == 3 else '')
     print(cmd)
@@ -30,32 +31,26 @@ except ImportError:
     print('Please re-run to use sane scanners')
     exit()
 
-version = sane.init()
-print('SANE version: {}'.format(version))
-
-available = sane.get_devices()
-print('Available devices = {}'.format(available))
-
-if not available:
-    print("NO DEVICES FOUND.")
-    s = None
-else:
-    s = sane.open(available[0][0])
-
 
 class Settings(object):
     def __init__(self, tk_root):
-        self.resolution = tk.StringVar(tk_root, value='300') # 300
+        self.resolution = tk.StringVar(tk_root, value='300')
         self.width = 210.0
         self.height = 297.0
         self.scale = tk.DoubleVar(tk_root, value=1.0)
         self.extension = tk.StringVar(tk_root, value='.png')
         self.scan_mode = tk.StringVar(tk_root, value='color')
+        self.custom_width = tk.StringVar(tk_root, value='210.0')
+        self.custom_height = tk.StringVar(tk_root, value='297.0')
 
     def br_x(self):
+        if self.scale.get() == 0:
+            return float(self.custom_width.get())
         return self.width * self.scale.get()
 
     def br_y(self):
+        if self.scale.get() == 0:
+            return float(self.custom_height.get())
         return self.height * self.scale.get()
 
     def ext(self):
@@ -78,14 +73,14 @@ class Settings(object):
             try:
                 import cv2
                 import numpy
-                from PIL import Image
-                cv_img = numpy.array(image)
-                theresh, result = cv2.threshold(cv_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-                print('Determined threshold: {0}'.format(theresh))
-                return Image.fromarray(result)
             except ImportError:
                 print('Error - OpenCV (cv2) missing. Cannot postprocess.')
                 return image
+            else:
+                cv_img = numpy.array(image)
+                thresh, result = cv2.threshold(cv_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                print('Determined threshold: {0}'.format(thresh))
+                return image.fromarray(result)
         else:
             return image
 
@@ -114,9 +109,9 @@ class ScanWorker(threading.Thread):
         do_scan(self.output_filename, self.settings)
 
 
-class ScanDialog(Frame):
+class ScanDialog(tk.Frame):
     def __init__(self, parent):
-        Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
         self.parent = parent
         self.worker = None
         self.elapsed = 0
@@ -125,35 +120,36 @@ class ScanDialog(Frame):
         # self.initUI() follows
 
         self.parent.title("Scan Images")
-        self.pack(fill=BOTH, expand=1)
+        self.pack(fill=tk.BOTH, expand=1)
 
         r = 0  # current grid row
 
-        Label(self, text="Name prefix:").grid(row=r, column=0)
-        Label(self, text="Number suffix:").grid(row=r, column=1)
+        tk.Label(self, text="Name prefix:").grid(row=r, column=0)
+        tk.Label(self, text="Number suffix:").grid(row=r, column=1)
         r += 1
 
-        self.newName = StringVar()
+        self.newName = tk.StringVar()
         self.newName.set('Scan_')
-        newName = Entry(self, textvariable=self.newName, width=60)
-        newName.grid(row=1, column=0)
-        newName.bind("<Return>",   lambda event: self.scan())
-        newName.bind("<KP_Enter>", lambda event: self.scan())
-        newName.bind("<Escape>", lambda event: self.parent.destroy())
-        newName.focus_set()
-        self.newNameEntry = newName
+        new_name = tk.Entry(self, textvariable=self.newName, width=60)
+        new_name.grid(row=1, column=0)
+        new_name.bind("<Return>", lambda event: self.scan())
+        new_name.bind("<KP_Enter>", lambda event: self.scan())
+        new_name.bind("<Escape>", lambda event: self.parent.destroy())
+        new_name.select_from(0)
+        new_name.select_to(len(self.newName.get())-1)
+        new_name.focus_set()
+        self.newNameEntry = new_name
 
-        self.numberSuffix = Spinbox(self, from_=1, to=999)
-        self.numberSuffix.bind("<Return>",   lambda event: self.scan())
+        self.numberSuffix = tk.Spinbox(self, from_=1, to=999)
+        self.numberSuffix.bind("<Return>", lambda event: self.scan())
         self.numberSuffix.bind("<KP_Enter>", lambda event: self.scan())
         self.numberSuffix.grid(row=r, column=1)
         r += 1
 
-        self.okButton = Button(self, text="Scan", command=self.scan, width=60, height=5)
+        self.okButton = tk.Button(self, text="Scan", command=self.scan, width=55, height=5)
         self.okButton.grid(row=r, column=0)
 
-        cancelButton = Button(self, text="Cancel", command=self.parent.destroy)
-        cancelButton.grid(row=r, column=1)
+        tk.Button(self, text="Exit", command=self.parent.destroy, width=12, height=5).grid(row=r, column=1)
         r += 1
 
         settings_panel = tk.Frame(self)
@@ -163,6 +159,9 @@ class ScanDialog(Frame):
         tk.Radiobutton(panel, text="A4", value=1.0, variable=self.settings.scale).pack(anchor=tk.W)
         tk.Radiobutton(panel, text="A5", value=2 ** (-0.5), variable=self.settings.scale).pack(anchor=tk.W)
         tk.Radiobutton(panel, text="A6", value=0.5, variable=self.settings.scale).pack(anchor=tk.W)
+        tk.Radiobutton(panel, text="Custom (W x H) [mm]", value=0.0, variable=self.settings.scale).pack(anchor=tk.W)
+        tk.Entry(panel, textvariable=self.settings.custom_width).pack(anchor=tk.W)
+        tk.Entry(panel, textvariable=self.settings.custom_height).pack(anchor=tk.W)
         panel.pack(side=tk.LEFT, anchor=tk.N)
 
         panel = tk.Frame(settings_panel)
@@ -188,20 +187,19 @@ class ScanDialog(Frame):
         settings_panel.grid(row=r, column=0, columnspan=2)
         r += 1
 
-
-        self.statusLabel = Label(self, text="Idle")
+        self.statusLabel = tk.Label(self, text="Please wait, initializing sane...")
         self.statusLabel.grid(row=r, column=0, columnspan=2)
 
-    def _checkAlive(self):
+    def _check_alive(self):
         if self.worker is None:
             return
         if self.worker.is_alive():
-            self.after(100, self._checkAlive)
+            self.after(100, self._check_alive)
             self.elapsed += 1
             self.statusLabel.config(text='Scanning, please wait... (%.1f s)' % (self.elapsed/10.0))
         else:
             self.worker = None
-            self.okButton.config(state=NORMAL)
+            self.okButton.config(state=tk.NORMAL)
             self.numberSuffix.invoke('buttonup')
             self.newNameEntry.focus_set()
             self.statusLabel.config(text='Idle (last scan: %.1f s)' % (self.elapsed/10.0))
@@ -234,14 +232,35 @@ class ScanDialog(Frame):
             self.worker = ScanWorker(target, self.settings)
             self.worker.start()
             self.elapsed = 0
-            self.after(100, self._checkAlive)
-            self.okButton.config(state=DISABLED)
+            self.after(100, self._check_alive)
+            self.okButton.config(state=tk.DISABLED)
             self.statusLabel.config(text='Scanning, please wait...')
         else:
             print("Error - not started, worker exists.")
 
 
-root = Tk()
-ex = ScanDialog(root)
-root.geometry("+300+300")
-root.mainloop()
+def _init_sane(dialog):
+    global s
+    s = None
+    start = time.time()
+    version = sane.init()
+    print('SANE version: {}'.format(version))
+
+    available = sane.get_devices()
+    print('Available devices = {}'.format(available))
+
+    if not available:
+        print("NO DEVICES FOUND.")
+        s = None
+        dialog.statusLabel.config(text='Idle (no sane devices found) {:.1f} s'.format(time.time() - start))
+    else:
+        s = sane.open(available[0][0])
+        dialog.statusLabel.config(text='Idle (sane loaded in {:.1f} s)'.format(time.time() - start))
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    ex = ScanDialog(root)
+    root.geometry("+300+300")
+    root.after(1, lambda: threading.Thread(target=_init_sane, args=(ex,)).start())
+    root.mainloop()
