42cc6498f6883c0562cd11b19f15c05396da8f34
tgandor <tomasz.gandor@gmail.com>
2017-03-21 18:25:14 +0100

    becoming JS lang lawyer - shadowing undefined. Py3 for fit_format.py

---
+++ lang_lawyer/js/not_undefined.js
@@ -0,0 +1,6 @@
+var a= {};
+undefined = "nodeJsBadParts";
+console.log(a.notExists === undefined)
+console.log(undefined, a.notExists)
+console.log(undefined === undefined)
+console.log(undefined == undefined)
--- toys/fit_format.py
+++ toys/fit_format.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python
 
+from __future__ import division
+from __future__ import print_function
+
 import re
 import sys
 
 if len(sys.argv) < 3:
-	print 'Usage: %s <w_from>x<h_from> <w_to>x<h_to>' % sys.argv[0]
+	print('Usage: %s <w_from>x<h_from> <w_to>x<h_to>' % sys.argv[0])
 	exit()
 
-# w_from, h_from = map(int, sys.argv[1].split('x'))
-# w_to, h_to = map(int, sys.argv[2].split('x'))
-
-w_from, h_from = map(int, re.split('\D', sys.argv[1]))
-w_to, h_to = map(int, re.split('\D', sys.argv[2]))
+w_from, h_from = list(map(int, re.split('\D', sys.argv[1])))
+w_to, h_to = list(map(int, re.split('\D', sys.argv[2])))
 
 if any(x <= 0 for x in (w_from, h_from, w_to, h_to)):
-	print 'All dimensions must be greater than zero.'
+	print('All dimensions must be greater than zero.')
 	exit()
 
 if w_from * h_to == w_to * h_from:
-	print 'Equal aspect. Scale directly to %dx%d.' % (w_to, h_to)
+	print('Equal aspect. Scale directly to %dx%d Scale: %.3f' % (w_to, h_to, h_to / h_from))
 elif w_from * h_to < w_to * h_from:
-	print 'Aspect wider in output (%.2f vs %.2f in input).' % (
-		float(w_to)/h_to,
-		float(w_from)/h_from
-	)
-	print 'Max horizontal fit: %dx%d.' % (w_to, h_from * w_to / w_from)
+	print('Aspect wider in output (%.3f vs %.3f in input).' % (
+            w_to/h_to, w_from/h_from))
+	print('Max horizontal fit: %dx%d Scale: %.3f' % (w_to, int(h_from * w_to / w_from), w_to / w_from))
 else:
-	print 'Aspect narrower in output (%.2f vs %.2f in input).' % (
-		float(w_to)/h_to,
-		float(w_from)/h_from
-	)
-	print 'Max vertical fit: %dx%d.' % (w_from * h_to / w_to, h_to)
+	print('Aspect narrower in output (%.3f vs %.3f in input).' % (w_to/h_to, w_from/h_from))
+	print('Max vertical fit: %dx%d Scale %.3f' % (int(w_from * h_to / w_to), h_to, h_to / h_from))
 
