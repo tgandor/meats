7f8aa2524175da11031fdd0b315a892b00b477d0
tgandor <tomasz.gandor@gmail.com>
2017-04-11 11:52:38 +0200

    dot(di)pairs.py - Py3, added label option for dotpairs.py

--- missing/dotpairs.py
+++ missing/dotpairs.py
@@ -2,13 +2,20 @@
 
 import sys
 
-print 'graph {'
+force = len(sys.argv) > 1 and sys.argv[1] == '2'
+labels = len(sys.argv) > 1 and sys.argv[1] == 'L'
+
+print('graph {')
 for line in sys.stdin.readlines():
     chunks = line.split()
-    if len(chunks) == 1:
-        print '  ' + line + ';'
-    elif len(chunks) == 2:
-        print '  {0} -- {1};'.format(chunks[0], chunks[1])
+    if len(chunks) == 0:
+        continue
+    elif len(chunks) == 1:
+        print('  ' + line + ';')
+    elif len(chunks) == 2 or force:
+        print('  {0} -- {1};'.format(*chunks[:2]))
+    elif len(chunks) == 3 and labels:
+        print('  {0} -- {1} [label="{2}"];'.format(*chunks))
     elif len(chunks) > 0:
-        print '  %s -- {%s};' % (chunks[0], ' '.join(chunks[1:]))
-print '}'
+        print('  %s -- {%s};' % (chunks[0], ' '.join(chunks[1:])))
+print('}')
