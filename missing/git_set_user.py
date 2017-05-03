8869132e6400f51d6c5b00a6245194cc170ddf1e
tgandor <tomasz.gandor@gmail.com>
2017-05-03 07:06:05 +0200

    missing/git_set_user.py - added optional source directory.

--- missing/git_set_user.py
+++ missing/git_set_user.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
 import os
+import sys
 
+if len(sys.argv) > 1:
+    source_dir = sys.argv[1]
+    target_dir = os.getcwd()
+else:
+    source_dir = target_dir = os.getcwd()
+
+os.chdir(source_dir)
 last_commit = os.popen("git log -1").readlines()
 author = last_commit[1].split()
 
+os.chdir(target_dir)
 cmd = 'git config user.name "{}"'.format(' '.join(author[1:-1]))
 print(cmd)
 os.system(cmd)
