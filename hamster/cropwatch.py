77bf1829d42cc08d5194d22077b17111dff3a8c8
tgandor <tomasz.gandor@gmail.com>
2017-05-14 01:00:12 +0200

    hamster/cropwatch.py - added pop-up menu for url.

--- hamster/cropwatch.py
+++ hamster/cropwatch.py
@@ -16,11 +16,28 @@ def play(url_entry):
         os.system('mplayer -vf crop=900:500:0:220 "{}" -audiofile "{}"'.format(*[u.strip() for u in urls]))
 
 
+def show_menu(e):
+    """A variation on: http://stackoverflow.com/a/8476726/1338797."""
+    the_menu = tk.Menu(root, tearoff=0)
+    the_menu.add_command(label="Cut")
+    the_menu.add_command(label="Copy")
+    the_menu.add_command(label="Paste")
+    w = e.widget
+    the_menu.entryconfigure("Cut",
+    command=lambda: w.event_generate("<<Cut>>"))
+    the_menu.entryconfigure("Copy",
+    command=lambda: w.event_generate("<<Copy>>"))
+    the_menu.entryconfigure("Paste",
+    command=lambda: w.event_generate("<<Paste>>"))
+    the_menu.tk.call("tk_popup", the_menu, e.x_root, e.y_root)
+
+
 root = tk.Tk()
 root.title("Crop watch URL")
 #root.geometry("600x50")
 
 url = tk.Entry(root, width=80)
+url.bind('<Button-3><ButtonRelease-3>', show_menu)
 url.pack(anchor=tk.N)
 
 watch = tk.Button(root, text='Watch', command=lambda: play(url), width=80)
