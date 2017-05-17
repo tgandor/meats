2875c057d83f643cc0685496f21654772cb9bdbd
tgandor <tomasz.gandor@gmail.com>
2017-05-17 12:20:24 +0200

    missing/check_vs_cmd.bat - no setlocal: enable VS permanently

--- missing/check_vs_cmd.bat
+++ missing/check_vs_cmd.bat
@@ -1,4 +1,4 @@
 @rem Check CL in path without output 
-@setlocal
+:: @setlocal
 @where cl >nul 2>nul && echo present || echo missing, adding && call "%VS140COMNTOOLS%vsvars32.bat"
 @where cl >nul 2>nul && cl || echo VS 2015 still not found
