5738ad3b5a0fe683af197b8387d82cab55b6e77d
tgandor <tomasz.gandor@gmail.com>
2017-06-08 16:44:25 +0200

    wget_.py - add standard input URLs. PEP8 refactorings.

--- missing/wget_.py
+++ missing/wget_.py
@@ -12,47 +12,61 @@ except ImportError:
 
 CHUNK = 1024 * 1024
 
+
 def human(x):
-    for sufix in ['', 'K', 'M', 'G', 'T']:
+    for suffix in ['', 'K', 'M', 'G', 'T']:
         if x < 1024:
-            return "%.1f %s" % (x, sufix)
+            return "%.1f %s" % (x, suffix)
         x /= 1024.0
     return "%.1f P" % x
 
+
 def dot_report(total, elapsed):
     sys.stdout.write('.')
-    try: # QPython, has no flush...
+    try:  # QPython, has no flush...
         sys.stdout.flush()
-    except:
+    except AttributeError:
         pass
 
-def download(URL, report = dot_report):
-    f = os.path.basename(URL)
+
+def download(url, report=dot_report):
+    f = os.path.basename(url)
     print("downloading", f)
     start = time.time()
-    size = 0
+    elapsed = size = 0
     with open(f, "wb") as fp:
-        resp = urlopen(URL)
+        resp = urlopen(url)
         for data in iter(lambda: resp.read(CHUNK), b''):
             fp.write(data)
             size += len(data)
-            elap = time.time() - start
-            report(size, elap)
-    print("\ngot %sB in %.1f s (%sB/s), saved" % (human(size), elap, human(size/elap)))
+            elapsed = time.time() - start
+            report(size, elapsed)
+    print("\ngot %sB in %.1f s (%sB/s), saved" % (human(size), elapsed, human(size/elapsed)))
+
 
 if len(sys.argv) > 1:
-    for URL in sys.argv[1:]:
-        download(URL)
+    # arguments
+    for url in sys.argv[1:]:
+        download(url)
+    exit()
 else:
+    # QPython clipboard
     print("trying to retrieve from clipboard")
     try:
         import androidhelper
-    except:
-        print("Error: couldn't find androidhelper")
+
+        try:
+            os.chdir(os.path.dirname(__file__) + '/../../Download')
+        except OSError:
+            pass
+
+        url = androidhelper.Android().getClipboard().result
+        download(url)
         exit()
-    try:
-        os.chdir(os.path.dirname(__file__) + '/../../Download')
-    except:
-        pass
-    URL = androidhelper.Android().getClipboard().result
-    download(URL)
+
+    except ImportError:
+        print("Error: couldn't find androidhelper")
+
+print('Reading URLs from standard input')
+for url in sys.stdin:
+    download(url.strip())
