3de0f2b1141ab4dbf1e5fb4d7df4a0be1692f6ad
tgandor <tomasz.gandor@gmail.com>
2017-06-13 15:28:58 +0200

    pull_all.bat - added arguments like in sh version

--- missing/pull_all.bat
+++ missing/pull_all.bat
@@ -1,11 +1,24 @@
 @echo off
 
-for /D %%f in (*) do (
-    if exist %%f\.git (
-        pushd %%f
-        cd
-        git pull --ff-only
-        echo ----------------------------------
-        popd
+if [%1]==[] (
+    for /D %%f in (*) do (
+        if exist %%f\.git (
+            pushd %%f
+            cd
+            git pull --ff-only
+            echo ----------------------------------
+            popd
+        )
+    )
+    exit /b
+)
+
+for %%d in (%*) do (
+    if exist %%d\.git (
+            pushd %%d
+            cd
+            git pull --ff-only
+            echo ----------------------------------
+            popd
     )
 )
