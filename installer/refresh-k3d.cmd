562784c77b231643bc06895c5c96619682b5d98d
tgandor <tomasz.gandor@gmail.com>
2017-06-14 16:11:02 +0200

    K3D install helpers for windows

---
+++ installer/refresh-k3d.cmd
@@ -0,0 +1,10 @@
+pip install -e .
+
+if not defined VIRTUAL_ENV (
+    echo Not in VirtualEnv, exiting
+    exit /b
+)
+
+jupyter nbextension install --py --sys-prefix k3d
+jupyter nbextension enable --py --sys-prefix k3d
+jupyter nbextension list
