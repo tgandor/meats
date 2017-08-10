5141c76c66a71f0f8eb33901f3e550df1d30c67c
tgandor <tomasz.gandor@gmail.com>
2017-08-10 15:15:26 +0200

    updated k3d scripts; clipboard copy with gui on windows too.

--- installer/k3d-install.cmd
+++ installer/k3d-install.cmd
@@ -1,9 +1,9 @@
-pip install -e .
-
-if not defined VIRTUAL_ENV (
-    echo Not in VirtualEnv, exiting
+if not defined CONDA_DEFAULT_ENV if not defined VIRTUAL_ENV (
+    echo Not in VirtualEnv or Conda Env, exiting
     exit /b
 )
+
+pip install -e .
 
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 jupyter nbextension install --py --sys-prefix k3d
--- missing/clipboard_copy.py
+++ missing/clipboard_copy.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import sys
-
-
-def read(prompt):
-    if sys.version_info.major == 2:
-        return raw_input(prompt)
-    return input(prompt)
 
 
 def copy(data, wait_gui=False):
@@ -22,20 +18,19 @@ def copy(data, wait_gui=False):
     r.clipboard_clear()
     r.clipboard_append(data)
 
-    if sys.platform != 'win32':
-        if not wait_gui:
-            read('Data was copied into clipboard. Paste and press ENTER to exit...')
-            r.destroy()
-        else:
-            # stdin already read; use GUI to exit
-            print('Data was copied into clipboard. Paste, then close popup to exit...')
-            tk.Button(r, text='Click or press key to exit', command=r.destroy, width=40, height=10).pack(fill=tk.BOTH)
-            r.title('Clipboard Copy')
-            r.bind('<Key>', lambda e: r.destroy() if e.keysym != 'Alt_L' else None)
-            r.deiconify()
-            r.mainloop()
-    else:
+    if not wait_gui:
+        print('Data was copied into clipboard. Paste and press ENTER to exit...')
+        for _ in sys.stdin:
+            break
         r.destroy()
+    else:
+        # stdin already read; use GUI to exit
+        print('Data was copied into clipboard. Paste, then close popup to exit...')
+        tk.Button(r, text='Click or press key to exit', command=r.destroy, width=40, height=10).pack(fill=tk.BOTH)
+        r.title('Clipboard Copy')
+        r.bind('<Key>', lambda e: r.destroy() if e.keysym != 'Alt_L' else None)
+        r.deiconify()
+        r.mainloop()
 
 
 if __name__ == '__main__':
