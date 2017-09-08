9686028daa7efe42a1ed6279bb03cda2eced0078
tgandor <tomasz.gandor@gmail.com>
2017-09-08 06:09:06 +0000

    stream_camera.sh - script for streaming Picam; no success

---
+++ pi/stream_camera.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! which cvlc ; then
+	sudo apt-get install vlc-nox
+fi
+
+# Had problems on B+ ... ?
+# https://raspberrypi.stackexchange.com/a/26075/55667
+
+sudo modprobe bcm2835-v4l2
+
+sudo vlc-wrapper v4l2:///dev/video0 --v4l2-width 1920 --v4l2-height 1080 --v4l2-chroma h264 --sout '#standard{access=http,mux=ts,dst=0.0.0.0:12345}' # --http-host=0.0.0.0 :demux=264
