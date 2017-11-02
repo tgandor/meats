a050aca218e34d7347dbee5946fcb7cb0903433e
tgandor <tomasz.gandor@gmail.com>
2017-11-02 22:13:19 +0100

    missing/pip_show_all.py - list all installed packages with their information.

---
+++ missing/pip_show_all.py
@@ -0,0 +1,18 @@
+
+from __future__ import print_function
+
+import os
+import pip
+
+
+def main():
+    packages = [line.split()[0] for line in os.popen('pip list --format=legacy')]
+    for i, package in enumerate(packages):
+        # way slower, at least on windows:
+        # os.system('pip show {}'.format(package))
+        pip.main(['show', package])
+        print(i, '-'*70)
+
+
+if __name__ == '__main__':
+    main()
