a3bb2da27f659c3d7e8f8e3e5d77dd333038bc6a
tgandor <tomasz.gandor@gmail.com>
2017-11-06 13:43:47 +0100

    md5sum.py - added support for SHA1 and SHA256.

--- missing/md5sum.py
+++ missing/md5sum.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python
 
+import argparse
 import hashlib
 import sys
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--sha256', '-S', action='store_true', help='Use SHA256 instead of MD5')
+parser.add_argument('--sha', '-s', action='store_true', help='Use SHA-1 instead of MD5')
+parser.add_argument('files', nargs='*')
+args = parser.parse_args()
 
 # inspired by:
 # http://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
 
 
-def do_md5(fileobj):
-    hash_md5 = hashlib.md5()
-    for chunk in iter(lambda: fileobj.read(4096), b""):
-        hash_md5.update(chunk)
-    return hash_md5.hexdigest()
+def do_md5(file_obj):
+    if args.sha256:
+        hasher = hashlib.sha256()
+    elif args.sha:
+        hasher = hashlib.sha1()
+    else:
+        hasher = hashlib.md5()
+    for chunk in iter(lambda: file_obj.read(4096), b""):
+        hasher.update(chunk)
+    return hasher.hexdigest()
 
 
-def md5(fname):
-    if fname == '-':
+def md5(filename):
+    if filename == '-':
         digest = do_md5(sys.stdin)
     else:
-        with open(fname, "rb") as f:
+        with open(filename, "rb") as f:
             digest = do_md5(f)
-    print('{}  {}'.format(digest, fname))
+    print('{}  {}'.format(digest, filename))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(args.files) < 1:
         md5('-')
     else:
-        list(map(md5, sys.argv[1:]))
-
+        list(map(md5, args.files))
