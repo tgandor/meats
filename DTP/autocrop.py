a3c3ac4c1f59fefd26691c913b16165465d155b3
tgandor <tomasz.gandor@gmail.com>
2017-11-22 13:16:49 +0100

    DTP/autocrop.py - the idea was OK. Dead slow, but decent results.

--- DTP/autocrop.py
+++ DTP/autocrop.py
@@ -5,28 +5,67 @@ import glob
 import argparse
 import numpy as np
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('paths', nargs='+', help='Files to autocrop')
 parser.add_argument('--overwrite', '-w', action='store_true', help='Save cropped files over original')
+parser.add_argument('--preview', '-v', action='store_true', help='Show cv2 preview of cropped image')
 parser.add_argument('--step', '-s', type=int, help='Try to cut off as many rows/cols at a time', default=10)
+
 args = parser.parse_args()
+
+
+def crop_by_average(img):
+    y0 = 0
+    y1 = img.shape[0]
+    x0 = 0
+    x1 = img.shape[1]
+
+    def make_slices(array):
+        return slice(*array[:2]), slice(*array[2:])
+
+    def pick_best(slices, delta):
+        print('by delta', delta)
+        if sum(delta) > 0:
+            hot_idx = np.argmax(delta)
+            tries = (slices[hot_idx+1] - slices[hot_idx]) // 4 // delta[hot_idx]
+        else:
+            hot_idx = np.argmin(delta)
+            tries = (slices[hot_idx-1] - slices[hot_idx]) // 4 // delta[hot_idx]
+
+        results = []
+        for i in range(tries):
+            h_slice, w_slice = make_slices(slices)
+            avg = np.average(img[h_slice, w_slice, ...])
+            print(slices, avg)
+            results.append((avg, slices.copy()))
+            slices += delta
+        best = max(results)
+        print('best:', best)
+        return best[1]
+
+    slices_orig = np.array([0, img.shape[0], 0, img.shape[1]])
+
+    slices = np.diag([
+        pick_best(slices_orig, np.array([args.step, 0, 0, 0])),
+        pick_best(slices_orig, np.array([0, -args.step, 0, 0])),
+        pick_best(slices_orig, np.array([0, 0, args.step, 0])),
+        pick_best(slices_orig, np.array([0, 0, 0, -args.step]))
+    ])
+
+    return img[make_slices(slices)]
+
 
 for path in args.paths:
     for filename in glob.glob(path):
         img = cv2.imread(filename)
         print(filename)
-        prev = 1
-        results = []
-        for i in range(img.shape[0] // args.step // 2):
-            avg = np.average(img[args.step*i:, ...])
-            absolute = avg - prev
-            relative = avg / prev
-            print(i*args.step, 'average', avg, 'absolute', absolute, 'relative', relative)
-            results.append((avg, i * args.step))
-        best = max(results)
-        print('best:', best)
-        # cv2.namedWindow('crop', cv2.WINDOW_NORMAL)
-        cv2.imshow('crop', img[best[1]:, ...])
-        if cv2.waitKey(0) & 0xff == ord('q'):
-            break
+        img_cropped = crop_by_average(img)
+        if args.preview:
+            cv2.imshow('crop', img_cropped[::2, ::2])
+            if cv2.waitKey(0) & 0xff == ord('q'):
+                break
+        target = filename if args.overwrite else 'crop_' + filename
+        cv2.imwrite(target, img_cropped)
+
         print('-' * 60)
