535abea9e7b1d4d5101fd85b39afa904c166f957
tgandor <tomasz.gandor@gmail.com>
2017-12-08 16:03:56 +0000

    pi/stream_camera_tcp.sh - enabling binning and limiting FPS.

--- pi/stream_camera_tcp.sh
+++ pi/stream_camera_tcp.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-raspivid $* -w 1920 -h 1080 -t 0 -l -o tcp://0.0.0.0:3333
+
+# fullHD, default BTW, does not use binning / view full frame
+# raspivid $* -w 1920 -h 1080 -t 0 -l -o tcp://0.0.0.0:3333
+# full frame is 2592 x 1944, and 1/4 will use 2x2 binning:
+
+raspivid $* -fps 7.5 -w 1296 -h 972 -t 0 -l -o tcp://0.0.0.0:3333
