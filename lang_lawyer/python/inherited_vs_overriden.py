40385908b81c4b3a13251762f052ffbec21b5866
tgandor <tomasz.gandor@gmail.com>
2018-01-16 14:40:53 +0100

    inherited_vs_overriden.py - little example of detecting new (own) properties of a class.

---
+++ lang_lawyer/python/inherited_vs_overriden.py
@@ -0,0 +1,32 @@
+
+from __future__ import print_function
+
+class A:
+    def f(self):
+        pass
+    
+    def g(self):
+        pass
+    
+
+class B:
+    def f(self):
+        # overriden
+        pass
+
+    def h(self):
+        pass
+
+import inspect
+
+a = inspect.getmembers(A)
+# print(a)
+b = inspect.getmembers(B)
+# print(b)
+
+for k, v in b:
+    if k.startswith('__'):
+        continue
+    if k not in a or v != b[k]:
+        print('Not inherited:', k, v)
+
