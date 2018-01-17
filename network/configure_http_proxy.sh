007f0a631b98a1299a2690d157304232e9952d47
tgandor <tomasz.gandor@gmail.com>
2018-01-17 12:58:28 +0000

    WiP: configure_http_proxy.sh - help in http proxy configuration (e.g. for SSL)

---
+++ network/configure_http_proxy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# inspired by: https://www.digitalocean.com/community/tutorials/how-to-use-apache-as-a-reverse-proxy-with-mod_proxy-on-debian-8
+
+if [ ! -f /etc/apache2/apache2.conf  ] ; then
+	sudo apt-get install -y apache2
+fi
+
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+sudo systemctl restart apache2
+
+# TODO: automate, parametrize port and alias
+
+cat <<EOF
+Add this to /etc/apache2/sites-available/000-default.conf
+inside VirtualHost
+	
+    <Proxy *>
+        Order deny,allow
+        Allow from all
+    </Proxy>
+
+    ProxyRequests Off
+    ProxyPreserveHost On
+    ProxyPass /alias http://localhost:8080/
+    ProxyPassReverse /alias http://localhost:8080/ 
+EOF
+
