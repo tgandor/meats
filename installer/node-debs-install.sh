3ec8ec25977759f057be2a1f93a17a03a39f3951
tgandor <tomasz.gandor@gmail.com>
2018-01-22 23:00:32 +0100

    node-debs-install.sh - helper script for getting up-to-date Node

---
+++ installer/node-debs-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# https://github.com/nodesource/distributions
+
+dl=none
+
+if which curl ; then 
+    echo Using curl
+    dl="curl -sL" 
+fi
+
+if [ "$dl"=="none" ] && which wget ; then
+    echo Using wget
+    dl="wget -q -O-"
+fi
+    
+if echo $dl | grep none ; then
+    echo Missing any downloader: wget or curl.
+    exit
+fi
+
+$dl https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
