adc52298c7e3259bf389a4eb644c06c0035277f4
tgandor <tomasz.gandor@gmail.com>
2018-03-22 16:05:01 +0100

    toys/grep_set.py - fixed, can add arguments.

--- toys/grep_set.py
+++ toys/grep_set.py
@@ -3,20 +3,26 @@
 from __future__ import print_function
 
 import argparse
+import itertools
 import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('search_letters', type=str)
+parser.add_argument('files', type=str, nargs='*')
 parser.add_argument('--count', '-c', action='store_true', help='Print match numbers')
 args = parser.parse_args()
 
 template = set(args.search_letters.lower())
 
+def get_input(files):
+    if not files:
+        return sys.stdin
+    return itertools.chain.from_iterable(map(open, files))
+
+
 count = 0
-for line in sys.stdin:
+for line in get_input(args.files):
     for word in map(str.strip, line.split(',')):
-        if word.startswith('kasa'):
-            print('Comparing', set(word.lower()), template)
         if set(word.lower()) <= template:  # set(word.lower()).issubset(template) - better?
             count += 1
             if count: 
