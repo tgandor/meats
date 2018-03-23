0744cc6e614e614e8e503c3f6826383db883285b
tgandor <tomasz.gandor@gmail.com>
2018-03-23 20:45:56 +0100

    toys/grep_set.py - adding a few features - min lenght, uniqueness, no acronyms.

--- toys/grep_set.py
+++ toys/grep_set.py
@@ -7,9 +7,12 @@ import itertools
 import sys
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--count', '-c', action='store_true', help='Print match numbers')
+parser.add_argument('--no-acronyms', action='store_true', help='Exclude words in ALLCAPS')
+parser.add_argument('--unique', '-u', action='store_true', help='Ensure unique words')
+parser.add_argument('--min-length', type=int, help='Only print words at least that long', default=3)
 parser.add_argument('search_letters', type=str)
 parser.add_argument('files', type=str, nargs='*')
-parser.add_argument('--count', '-c', action='store_true', help='Print match numbers')
 args = parser.parse_args()
 
 template = set(args.search_letters.lower())
@@ -21,11 +24,21 @@ def get_input(files):
 
 
 count = 0
+seen = set()
+
 for line in get_input(args.files):
     for word in map(str.strip, line.split(',')):
+        if args.no_acronyms and word.upper() == word:
+            continue
+        if len(word) < args.min_length:
+            continue
         if set(word.lower()) <= template:  # set(word.lower()).issubset(template) - better?
+            if args.unique:
+                if word.lower() in seen:
+                    continue
+                seen.add(word.lower())
             count += 1
-            if count: 
+            if args.count:
                 print(count, word)
             else:
                 print(word)
