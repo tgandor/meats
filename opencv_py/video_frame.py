41ccb78778a2c09bc313b83848ca2d6b6007d086
tgandor <tomasz.gandor@gmail.com>
2018-05-11 00:07:21 +0100

    video_frame.py - simple single frame extractor in cv2.

---
+++ opencv_py/video_frame.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import argparse
+import cv2
+import re
+import glob
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--skip", help="initial frames to skip", default=0, type=int)
+parser.add_argument("-f", "--format", help="file format (extension) to use", default="jpg")
+parser.add_argument("-t", "--timestamp", help="initial frames to skip", action="store_true")
+parser.add_argument("videos", help="videos to extract frame from", nargs='*')
+args = parser.parse_args()
+
+
+def videos(videos_args):
+    """Generate VideoCapture source values based on arguments."""
+
+    if not videos_args:
+        yield 0  # default webcam
+        return
+
+    for arg in videos_args:
+        if '*' in arg or '?' in arg:
+            for file_name in glob.glob(arg):
+                yield file_name
+        elif re.match(r'\d+$', arg):
+            yield int(arg)
+        else:
+            yield arg
+
+
+for source in videos(args.videos):
+    cap = None
+    try:
+        cap = cv2.VideoCapture(source)
+        for _ in range(args.skip):
+            cap.read()
+        status, frame = cap.read()
+        if not status:
+            raise RuntimeError('Error reading frame')
+        file_name = ''
+        if args.timestamp:
+            file_name += time.strftime('%Y%m%d_%H%M%S_')
+        file_name += str(source) + '.' + args.format
+        cv2.imwrite(file_name, frame)
+        print('Source: {} - Saved frame to: {}'.format(source, file_name))
+    finally:
+        if cap:
+            cap.release()
