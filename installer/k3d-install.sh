f556c56f4289b7b55a846128ed3c01f4a7c9d018
tgandor <tomasz.gandor@gmail.com>
2018-05-21 13:54:36 +0200

    cleanup in Linux k3d installer scripts (maybe soon obsolete altogether. hopefully).

--- installer/k3d-install.sh
+++ installer/k3d-install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd `dirname $0`
-
 function die() {
   echo "ERROR $*"
   exit
@@ -9,6 +7,6 @@ function die() {
 
 pip install -e . || die pip
 
-jupyter nbextension enable --py widgetsnbextension --sys-prefix || die widgets
+# widgets nbextension now installs automatically
 jupyter nbextension install --py --sys-prefix k3d || die install
 jupyter nbextension enable --py --sys-prefix k3d || die enable
