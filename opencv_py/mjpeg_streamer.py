bd58cf1a879d9481ecffa8cfaafbb827403d7db1
tgandor <tomasz.gandor@gmail.com>
2018-08-07 21:02:51 +0000

    opencv_py/mjpeg_streamer.py - adding parameters and some verbosity

--- opencv_py/mjpeg_streamer.py
+++ opencv_py/mjpeg_streamer.py
@@ -1,17 +1,18 @@
-#!/usr/bin/python
-
-"""
-Original Author: Igor Maculan - n3wtron@gmail.com
-A Simple mjpg stream http server
-https://gist.github.com/n3wtron/4624820
-"""
-
-from __future__ import absolute_import
+#!/usr/bin/python """ Original Author: Igor Maculan - n3wtron@gmail.com A Simple mjpg stream http server https://gist.github.com/n3wtron/4624820 """ from __future__ import absolute_import
 from __future__ import print_function
+
+import argparse
 import time
+
 from six.moves.BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from six.moves.socketserver import ThreadingMixIn
 import cv2
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--global', '-g', action='store_true', help='Bind to 0.0.0.0 instead of localhost')
+parser.add_argument('--port', '-p', type=int, default=8080, help='Port to bind to')
+parser.add_argument('--verbose', '-v', action='store_true', help='Report every frame sent')
+args = parser.parse_args()
 
 capture = None
 
@@ -32,17 +33,16 @@ class CamHandler(BaseHTTPRequestHandler):
                 if not status:
                     print('error encoding frame')
                     break
-                self.wfile.write(b'--boundary\r\n')
+                self.wfile.write(b'\r\n--boundary\r\n')
                 self.send_header('Content-type', 'image/jpeg')
                 self.send_header('Content-length', len(buffer))
                 self.end_headers()
                 self.wfile.write(buffer)
                 time.sleep(0.25)
+                if args.verbose:
+                    print('frame (shape {}) sent'.format(img.shape))
             except KeyboardInterrupt:
                 print('keyboard interrupt')
-                break
-            except ConnectionError:
-                print('connection error')
                 break
 
 
@@ -53,14 +53,11 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 def main():
     global capture
     capture = cv2.VideoCapture(0)
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    # why was is B/W?
-    # capture.set(cv2.CAP_PROP_SATURATION, 0.2)
 
     try:
-        server = ThreadedHTTPServer(('localhost', 8080), CamHandler)
-        print("server started")
+        address = '0.0.0.0' if getattr(args, 'global') else 'localhost'
+        server = ThreadedHTTPServer((address, args.port), CamHandler)
+        print('server started on {}:{}'.format(address, args.port))
         server.serve_forever()
     except KeyboardInterrupt:
         capture.release()
