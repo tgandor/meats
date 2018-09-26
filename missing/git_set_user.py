e9b724b7656693e7dac08f62d1ded350f567ffab
tgandor <tomasz.gandor@gmail.com>
2018-09-26 06:17:58 +0200

    playing with setuptools' entry_points in missing

--- missing/git_set_user.py
+++ missing/git_set_user.py
@@ -3,20 +3,26 @@
 import os
 import sys
 
-if len(sys.argv) > 1:
-    source_dir = sys.argv[1]
-    target_dir = os.getcwd()
-else:
-    source_dir = target_dir = os.getcwd()
 
-os.chdir(source_dir)
-last_commit = os.popen("git log -1").readlines()
-author = last_commit[1].split()
+def main():
+    if len(sys.argv) > 1:
+        source_dir = sys.argv[1]
+        target_dir = os.getcwd()
+    else:
+        source_dir = target_dir = os.getcwd()
 
-os.chdir(target_dir)
-cmd = 'git config user.name "{}"'.format(' '.join(author[1:-1]))
-print(cmd)
-os.system(cmd)
-cmd = 'git config user.email "{}"'.format(author[-1][1:-1])
-print(cmd)
-os.system(cmd)
+    os.chdir(source_dir)
+    last_commit = os.popen("git log -1").readlines()
+    author = last_commit[1].split()
+
+    os.chdir(target_dir)
+    cmd = 'git config user.name "{}"'.format(' '.join(author[1:-1]))
+    print(cmd)
+    os.system(cmd)
+    cmd = 'git config user.email "{}"'.format(author[-1][1:-1])
+    print(cmd)
+    os.system(cmd)
+
+
+if __name__ == '__main__':
+    main()
--- missing/setup.py
+++ missing/setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name="missing",
-      version="0.0.1",
-      packages=find_packages())
+      version="0.0.2",
+      packages=find_packages(),
+      license='MIT',
+      entry_points={
+          'console_scripts': [
+              'du_by_ext=du_by_ext:main',
+              'git_set_user=git_set_user:main',
+          ]
+      }
+)
 
