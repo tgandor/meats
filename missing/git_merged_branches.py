36cd625c3245cb9171df14446c797169f1454683
tgandor <tomasz.gandor@gmail.com>
2018-09-27 10:27:09 +0200

    git_merged_branches - made more practical, with exclude option. Script always quiet.

--- missing/git_merged_branches.py
+++ missing/git_merged_branches.py
@@ -2,20 +2,21 @@
 
 """
 Print branches which are merged or have children.
+Can generate script to remove them from the working copy and remote.
 """
 from __future__ import print_function
 
-import glob
-import os
 import argparse
+import glob
 import itertools
-
+import os
+import re
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--script', '-s', action='store_true', help='Output commands for deleting remote branches')
-    parser.add_argument('--quiet', '-q', action='store_true', help='Suppress printing branch names (to use with -s)')
-    parser.add_argument('--limit', '-n', type=int, help='Max branches to ')
+    parser.add_argument('--limit', '-n', type=int, help='Max branches to delete')
+    parser.add_argument('--exclude', '-x', help='ignore branches containing a regular expression')
 
     args = parser.parse_args()
 
@@ -43,20 +44,31 @@ if __name__ == '__main__':
             parents.add(commit)
 
     candidates = [name for commit, name in branches.items() if commit in parents]
+    if args.exclude:
+        excluded = list(filter(re.compile(args.exclude).search, candidates))
+        if not args.script:
+            for name in excluded:
+                print('Skipping {} (matches "{}")'.format(name, args.exclude))
+        candidates = list(set(candidates) - set(excluded))
+        print()
 
-    if not args.quiet:
+    if not args.script:
+        if not candidates:
+            print('No merged branches found.')
+            exit()
+
         print('Potentially deletable branches:\n'
               '(remember to have everything pushed & pulled)\n'
               '(also recommended: git fetch -p)\n')
         for c in candidates:
             print(c)
-        if len(candidates):
-            print()
-
-    if args.script:
+        print()
+        print('Execute with -s to generate script to remove them.')
+    else:
         for i, name in enumerate(candidates):
             if args.limit and i == args.limit:
                 break
+
             if '/' in name:
                 print('git push {} --delete {}'.format(*name.split('/', 1)))
                 print('git branch -dr {}'.format(name))
