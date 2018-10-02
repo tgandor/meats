d9d744314575b098e3ba01de27491c8a77f9fec8
tgandor <tomasz.gandor@gmail.com>
2018-10-02 11:51:03 +0200

    list_tf_devices.py - adding CUDA_VISIBLE_DEVICES override argument.

--- opencv_py/list_tf_devices.py
+++ opencv_py/list_tf_devices.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
+import os
+import sys
 from tensorflow.python.client.device_lib import list_local_devices
+
+if len(sys.argv) > 1:
+    if sys.argv[1] in '--help':
+        print('Usage: {} [CUDA_VISIBLE_DEVICES]'.format(sys.argv[0]))
+        exit()
+    os.environ['CUDA_VISIBLE_DEVICES'] = sys.argv[1]
 
 devices = list_local_devices()
 
