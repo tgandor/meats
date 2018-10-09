e78af874b6e8ec165515d37223ddcd4fe3767c90
tgandor <tomasz.gandor@gmail.com>
2018-10-09 22:41:29 +0200

    opencv_py\mjpeg_streamer.py - custom delay, better logging (time, counter, size).

--- opencv_py/mjpeg_streamer.py
+++ opencv_py/mjpeg_streamer.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python """ Original Author: Igor Maculan - n3wtron@gmail.com A Simple mjpg stream http server https://gist.github.com/n3wtron/4624820 """ from __future__ import absolute_import
+#!/usr/bin/env python
+
+"""
+Original Author: Igor Maculan - n3wtron@gmail.com
+A Simple mjpg stream http server https://gist.github.com/n3wtron/4624820
+"""
+
+from __future__ import absolute_import
 from __future__ import print_function
 
 import argparse
@@ -7,11 +14,13 @@ import time
 from six.moves.BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from six.moves.socketserver import ThreadingMixIn
 import cv2
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--global', '-g', action='store_true', help='Bind to 0.0.0.0 instead of localhost')
 parser.add_argument('--port', '-p', type=int, default=8080, help='Port to bind to')
 parser.add_argument('--verbose', '-v', action='store_true', help='Report every frame sent')
+parser.add_argument('--delay', '-s', type=float, default=1./30, help='Seconds to sleep between frames')
 args = parser.parse_args()
 
 capture = None
@@ -23,24 +32,29 @@ class CamHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--boundary')
         self.end_headers()
+        counter = 0
+        start = time.time()
         while True:
             try:
                 rc, img = capture.read()
                 if not rc:
                     print('Error reading frame!')
                     continue
-                status, buffer = cv2.imencode('.jpg', img)
+                status, buffer = cv2.imencode('.jpg', img)  # type: bool, np.ndarray
                 if not status:
-                    print('error encoding frame')
+                    print('Error encoding frame!')
                     break
+                counter += 1
                 self.wfile.write(b'\r\n--boundary\r\n')
                 self.send_header('Content-type', 'image/jpeg')
                 self.send_header('Content-length', len(buffer))
                 self.end_headers()
                 self.wfile.write(buffer.tostring())
-                time.sleep(0.25)
+                time.sleep(args.delay)
                 if args.verbose:
-                    print('frame (shape {}) sent'.format(img.shape))
+                    print('[{}] frame {} (shape {}, compressed {:,}) sent'.format(
+                        time.time() - start, counter, img.shape, len(buffer)
+                    ))
             except KeyboardInterrupt:
                 print('keyboard interrupt')
                 break
