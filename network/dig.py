5bc33e4f4a5adfb305afb125d74e076a0130cb37
tgandor <tomasz.gandor@gmail.com>
2018-10-16 21:44:29 +0200

    saving to hosts in dig.py

--- network/dig.py
+++ network/dig.py
@@ -28,6 +28,7 @@ def ensure_resolver():
 parser = argparse.ArgumentParser()
 parser.add_argument('--ip', '-a', action='store_true', help='Only output IP address')
 parser.add_argument('--no-config', '-n', action='store_true', help='Don\'t use resolv.conf even if it exists.')
+parser.add_argument('--save', '-s', action='store_true', help='Save to /etc/hosts (needs writing access)')
 parser.add_argument('url', type=str, help='The name to look up')
 args = parser.parse_args(sys.argv[1:])
 
@@ -45,4 +46,14 @@ for rdata in answers:
         print(rdata)
     else:
         print(rdata, args.url)
- 
+
+if args.save:
+    current_config = open('/etc/hosts').read()
+    address = str(answers[0])
+    if address in current_config or args.url in current_config:
+        print('Name or IP already configured in /etc/hosts')
+        print('\n'.join(line for line in current_config.split('\n') if address in line or args.url in line))
+        exit(1)
+    with open('/etc/hosts', 'a') as hosts:
+        hosts.write('\n{} {}\n'.format(address, args.url))
+        print('Host saved')
--- system/reboot_to_windows.sh
+++ system/reboot_to_windows.sh
@@ -3,7 +3,12 @@
 # source:
 # https://unix.stackexchange.com/questions/43196/how-can-i-tell-grub-i-want-to-reboot-into-windows-before-i-reboot
 
-grep GRUB_DEFAULT=saved /etc/default/grub || ( echo "Not configured" ; exit 1 )
+if ! grep GRUB_DEFAULT=saved /etc/default/grub ; then
+    echo "Not configured"
+    echo "Set GRUB_DEFAULT=saved in /etc/default/grub and then run:"
+    echo "sudo grub-update"
+    exit 1
+fi
 
 WINDOWS_TITLE=`grep -i "^menuentry 'Windows" /boot/grub/grub.cfg|head -n 1|cut -d"'" -f2`
 
