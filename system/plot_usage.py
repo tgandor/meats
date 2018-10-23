6ab53ca5760fafee628e6f4b6a26ffed792074f8
tgandor <tomasz.gandor@gmail.com>
2018-10-23 11:30:53 +0300

    trying to add ascii plotting in plot_usage. Depends on gnuplot (not great).

--- system/plot_usage.py
+++ system/plot_usage.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 import sqlite3
 import pandas as pd
 from matplotlib import pyplot as plt
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--ascii', '-t', action='store_true')
+args = parser.parse_args()
 
 conn = sqlite3.connect(os.path.expanduser('~/usage.db'))
 df = pd.read_sql_query("select * from df where mountpoint='/'", conn)
 
-df.plot(x='df_date', y='used')
-plt.show()
+if args.ascii:
+    import asciiplotlib
+    fig = asciiplotlib.figure()
+    fig.plot(list(pd.to_datetime(df['df_date'])), list(df['used']), label='used', width=70, height=50)
+    fig.show()
+else:
+    df.plot(x='df_date', y='used')
+    plt.show()
