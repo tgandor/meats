cfe2277eb020e5125db160e3c893a4c9a5f1571e
tgandor <tomasz.gandor@gmail.com>
2018-11-06 10:04:12 +0100

    dates/jpeg_quality.sh - added links and warnings.

--- dates/jpeg_quality.sh
+++ dates/jpeg_quality.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# identifies JPEG quality (compression level)
+# this may not always work very reliably, see:
+# https://stackoverflow.com/questions/2024947/is-it-possible-to-tell-the-quality-level-of-a-jpeg
+# https://patrakov.blogspot.com/2008/12/jpeg-quality-is-meaningless-number.html
+# https://blogs.gnome.org/raphael/2007/10/23/mapping-jpeg-compression-levels-between-adobe-photoshop-and-gimp-24/
+
 identify -format '%f %Q\n' "$@"
