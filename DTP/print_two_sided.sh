8a44fae8c0e717dcb91067ef57830a06af4e6392
tgandor <tomasz.gandor@gmail.com>
2018-11-09 17:55:10 +0100

    DTP/print_two_sided.sh - helper script to print [many] [PDF] files two-sided. On first printer.

---
+++ DTP/print_two_sided.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+destination_spec=""
+
+if lpstat -p -d | grep -q 'no system default'
+then 
+    destination=`lpstat -p | awk 'NR==1 { print $2 }'`
+    echo "Warning: guessing destination: $destination"
+    destination_spec="-d $destination"
+fi
+
+lp -o media=a4 -o sides=two-sided-long-edge $destination_spec "$@"
