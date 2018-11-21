d60edfe4225a8778e15dc3dded37ae16cf77265a
tgandor <tomasz.gandor@gmail.com>
2018-11-21 06:51:29 +0100

    toys/math/factorize.py - prettier output for single primes.

--- toys/math/factorize.py
+++ toys/math/factorize.py
@@ -24,12 +24,18 @@ def divisor_count(factorization):
     return product - 2
 
 
+def _format_factor(prime, power):
+    if power > 1:
+        return '({} ** {})'.format(prime, power)
+    return str(prime)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('n', type=int)
     args = parser.parse_args()
 
     factors = list(factorize(args.n))
-    print('n = {}'.format(' * '.join('({} ** {})'.format(prime, power) for prime, power in factors)))
+    print('n = {}'.format(' * '.join(_format_factor(prime, power) for prime, power in factors)))
     print('proper divisor count: {:,}'.format(divisor_count(factors)))
 
