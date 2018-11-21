f0bff8cd660fd3e826497446c58a14dcc8a9e114
tgandor <tomasz.gandor@gmail.com>
2018-11-21 06:36:25 +0100

    toys/math/factorize.py - little replacement for unix factor command.

---
+++ toys/math/factorize.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import argparse
+
+
+def factorize(n):
+    d = 2
+    while n > 1:
+        power = 0
+        while n % d == 0:
+            n = n // d
+            power += 1
+        if power:
+            yield (d, power)
+        d += 1
+
+
+def divisor_count(factorization):
+    product = 1
+
+    for _, power in factorization:
+        product *= (power + 1)
+
+    return product - 2
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('n', type=int)
+    args = parser.parse_args()
+
+    factors = list(factorize(args.n))
+    print('n = {}'.format(' * '.join('({} ** {})'.format(prime, power) for prime, power in factors)))
+    print('proper divisor count: {:,}'.format(divisor_count(factors)))
+
