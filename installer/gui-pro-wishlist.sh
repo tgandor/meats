3310f8ed4602c2c84d3205b737bcd07fb4b23fd2
tgandor <tomasz.gandor@gmail.com>
2018-11-23 14:19:25 +0100

    installer/gui-pro-wishlist.sh - splitting up the basic wishlists into headless and GUI.

---
+++ installer/gui-pro-wishlist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install -y \
+    kdf \
+    chromium-browser \
+    gimp \
+    mplayer \
+    terminator \
+    ttf-mscorefonts-installer \
+    vlc \
+# end.
--- installer/pro-wishlist.sh
+++ installer/pro-wishlist.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-sudo apt install -y aptitude mc screen tmux htop iotop kdesdk-scripts fdupes ffmpeg subversion lm-sensors cifs-utils vim dstat python-pip python3-pip
-
+sudo apt install -y \
+    aptitude \
+    apt-rdepends \
+    cifs-utils \
+    dstat \
+    fdupes \
+    ffmpeg \
+    htop \
+    iotop \
+    kdesdk-scripts \
+    lm-sensors \
+    mc \
+    python3-pip \
+    python-pip \
+    rar \
+    screen \
+    smbclient \
+    sqlite3 \
+    subversion \
+    tmux \
+    vim \
+# end.
