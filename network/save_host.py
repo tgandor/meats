4339390e66413f91ff8e571b4848acb60555cd48
tgandor <tomasz.gandor@gmail.com>
2019-01-11 15:06:45 +0100

    network/save_host.py - OK exit status if already exists (not checking if exact). For scripting.

--- network/save_host.py
+++ network/save_host.py
@@ -8,10 +8,12 @@ import sys
 current_config = open('/etc/hosts').read()
 address = sys.argv[1]
 hostname = sys.argv[2]
+
 if address in current_config or hostname in current_config:
     print(address, hostname, '- name or IP already configured in /etc/hosts:')
     print('\n'.join(line for line in current_config.split('\n') if address in line or hostname in line))
-    exit(1)
+    exit()
+
 with open('/etc/hosts', 'a') as hosts:
     hosts.write('\n{} {}\n'.format(address, hostname))
     print(address, hostname, '- host saved')
