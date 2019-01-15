ef34786838b2c442de5c886ad87a3c2060029d9f
tgandor <tomasz.gandor@gmail.com>
2019-01-15 08:13:55 +0100

    missing/how_come.sh - handle symlinks and snaps less naively.

--- missing/how_come.sh
+++ missing/how_come.sh
@@ -13,14 +13,28 @@ fi
 
 cmd_path=`which $1`
 
+# sometimes a command is symlinked via alternatives:
+real_path=`readlink -f $cmd_path`
+if [ "$real_path" != "$cmd_path" ] ; then
+    # special case: snaps
+    if [ "$real_path" == "/usr/bin/snap" ] ; then
+        echo "This comes from a snap package"
+        echo "Probably just:"
+        echo "sudo snap install $1"
+        exit
+    fi
 
-dpkg -S $cmd_path | awk '{ gsub(/:$/, "", $1); print "sudo apt-get install " $1;}'
+    echo "$real_path"
+    cmd_path=$real_path
+fi
+
+dpkg -S $cmd_path | awk '{ gsub(/:$/, "", $1); print "sudo apt install " $1;}'
 echo "---"
 echo 'missing=""'
 dpkg -S $cmd_path | awk '{ gsub(/:$/, "", $1); print "if ! which '$1' >/dev/null; then\n\tmissing=\"$missing " $1 "\"\nfi"; }'
 
 echo 'if [ -n "$missing" ] ; then'
 echo '    echo "Missing packages: $missing"'
-echo '    sudo apt-get install $missing'
+echo '    sudo apt install $missing'
 echo 'fi'
 
