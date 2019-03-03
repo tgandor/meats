5676e1f2c2fa147b4b3bf96b5f3fe6151a255db7
tgandor <tomasz.gandor@gmail.com>
2019-03-03 13:50:24 +0100

    system/list_gpus.sh - added running lshw.

--- system/list_gpus.sh
+++ system/list_gpus.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# https://linuxconfig.org/how-to-install-the-nvidia-drivers-on-ubuntu-18-04-bionic-beaver-linux
+lshw -numeric -C display
+
 # from: https://askubuntu.com/a/503438/309037
 lspci -vnnn | perl -lne 'print if /^\d+\:.+(\[\S+\:\S+\])/' | grep VGA
 
