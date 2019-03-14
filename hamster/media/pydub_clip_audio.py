c22d020ad20a56f2561555f2c2f28f5f7509272a
tgandor <tomasz.gandor@gmail.com>
2019-03-14 20:54:30 +0100

    hamster\media\pydub_clip_audio.py - added multiprocessing to speed things up. Not as simple as it should be.

--- hamster/media/pydub_clip_audio.py
+++ hamster/media/pydub_clip_audio.py
@@ -8,15 +8,12 @@ Script to cut of all mp3 files in a directory by a specified amount of seconds.
 
 import argparse
 import glob
+import itertools
+import multiprocessing
 import os
 
 import pydub
 import tqdm
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--leading', '-ss', default=0, help='Leading seconds to cut off', type=float)
-parser.add_argument('--trailing', '-tt', default=0, help='Trailing seconds to cut off', type=float)
-args = parser.parse_args()
 
 
 def strip_song(song, before_s=0, after_s=0):
@@ -27,12 +24,54 @@ def strip_song(song, before_s=0, after_s=0):
     return stripped
 
 
-os.makedirs('output', exist_ok=True)
-
-files = sorted(glob.glob('*.mp3'))
-
-for name in tqdm.tqdm(files):
+def process_filename(name, leading, trailing):
     song = pydub.AudioSegment.from_file(name)
-    cut = strip_song(song, args.leading, args.trailing)
+    cut = strip_song(song, leading, trailing)
     cut.export(os.path.join('output', name))
-    tqdm.tqdm.write(f'{name}: file cropped from {len(song)//1000} to {len(cut)//1000}')
+    before = len(song) // 1000
+    after = len(cut) // 1000
+    return name, before, after
+
+
+def apply_pf(args):
+    """Dumb hack. Partial doesnt need this. Pool.starmap would also do."""
+    return process_filename(*args)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--leading', '-ss', default=0, help='Leading seconds to cut off', type=float)
+    parser.add_argument('--trailing', '-tt', default=0, help='Trailing seconds to cut off', type=float)
+    args = parser.parse_args()
+
+    os.makedirs('output', exist_ok=True)
+
+    pool = multiprocessing.Pool()
+
+    files = sorted(glob.glob('*.mp3'))
+
+    # Python is dumb: can't pickle closure
+    # and pool.map() should have extra args
+    '''
+    def map_func(name):
+        return process_filename(name, args.leading, args.trailing)
+
+    for name, before, after in tqdm.tqdm(pool.imap_unordered(map_func, files)):
+        tqdm.tqdm.write('{name}: file cropped {before} to {after}'.format(**locals()))
+    '''
+
+    # fortunately, this does work: (passing a partial to pool)
+    import functools
+    map_func = functools.partial(process_filename, leading=args.leading, trailing=args.trailing)
+
+    for name, before, after in tqdm.tqdm(pool.imap_unordered(map_func, files), total=len(files)):
+        tqdm.tqdm.write('{name}: file cropped {before} to {after}'.format(**locals()))
+
+    # so we don't need to go down this path:
+    '''
+    for name, before, after in tqdm.tqdm(pool.imap_unordered(apply_pf, zip(files, itertools.repeat(args.leading), itertools.repeat(args.trailing))), total=len(files)):
+        tqdm.tqdm.write('{name}: file cropped {before} to {after}'.format(**locals()))
+    '''
+
+if __name__ == '__main__':
+    main()
