10ffdf2bbad626d572ba17c6830a3c1482072111
tgandor <tomasz.gandor@gmail.com>
2019-03-14 11:47:57 +0100

    toys/play_pygame_tone.py - like pygame_tone.py, but stripped to the bone.

---
+++ toys/play_pygame_tone.py
@@ -0,0 +1,36 @@
+import math
+import time
+
+import numpy as np
+
+import pygame
+from pygame.locals import *
+
+
+def SineWave(freq=1000, volume=16000, length=1):
+
+    num_steps = length * SAMPLE_RATE
+    s = []
+
+    for n in range(num_steps):
+        value = int(math.sin(n * freq * (6.28318/SAMPLE_RATE) * length) * volume)
+        s.append( [value, value] )
+
+    return np.array(s, dtype=np.int32)
+
+
+def MakeSound(arr):
+    return pygame.sndarray.make_sound(arr)
+
+
+def MakeSineWave(freq=1000):
+    return MakeSound(SineWave(freq))
+
+
+SAMPLE_RATE = 22050 ## This many array entries == 1 second of sound.
+
+pygame.init()
+sound = MakeSineWave(130.81)
+sound.play()
+time.sleep(1.2)
+pygame.quit()
