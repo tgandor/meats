476a281d05549ab21b7557b4c354498721a55bd6
tgandor <tomasz.gandor@gmail.com>
2019-04-17 12:25:44 +0200

    hamster/replace_links.py - new mode of operation: text to url (copy text to href).

--- hamster/replace_links.py
+++ hamster/replace_links.py
@@ -9,11 +9,15 @@ import sys
 
 """
 A script to replace link targets with their basename - i.e. make it point to local files.
+
+Alternatively you can use --text-to-url which replaces the link text ('here', 'link'),
+which can be useless, with the actual link target (prevent phishing and other URL attacks).
 """
 
 parser = argparse.ArgumentParser()
 parser.add_argument('url_or_path')
 parser.add_argument('--suffix', help='Ensure a suffix for the links, eg.: .pdf')
+parser.add_argument('--text-to-url', '-ttu', action='store_true', help='alternative function: replace text with href')
 args = parser.parse_args()
 
 link = re.compile(r'<a (?:\w+="[^"]+" )*href="(?P<href>[^"]+)"(?: \w+="[^"]+")*>(?P<text>.+?)</a>', re.IGNORECASE)
@@ -31,4 +35,12 @@ def callback(match):
         return match.group().replace(match.group(1), os.path.basename(match.group(1)) + args.suffix)
     return match.group().replace(match.group(1), os.path.basename(match.group(1)))
 
-sys.stdout.write(link.sub(callback, data))
+
+def text_to_url(match):
+    return match.group().replace(match.group(2), match.group(1))
+
+
+if args.text_to_url:
+    sys.stdout.write(link.sub(text_to_url, data))
+else:
+    sys.stdout.write(link.sub(callback, data))
