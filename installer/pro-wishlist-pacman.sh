5167bfe89d4b3c7865e3f0b52f73b3b25cd60d8b
tgandor <tomasz.gandor@gmail.com>
2019-05-20 15:47:36 +0200

    installer/pro-wishlist-pacman.sh - a version for Manjaro (incomplete).

---
+++ installer/pro-wishlist-pacman.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo pacman -S "$@" \
+    dstat \
+    fdupes \
+    iotop \
+    mc \
+    mercurial \
+    net-tools \
+    pamac-cli \
+    screen \
+    tmux \
+    unrar \
+    vim \
+    zip \
+# end.
