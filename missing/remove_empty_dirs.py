cb72732a19db8bdef0740368b9397f920dcbe584
tgandor <tomasz.gandor@gmail.com>
2019-05-21 22:33:45 +0200

    missing\remove_empty_dirs.py - tqdm, logging, actual removal, counting. Version 1.0

--- missing/remove_empty_dirs.py
+++ missing/remove_empty_dirs.py
@@ -4,7 +4,13 @@ from __future__ import print_function
 
 from collections import deque
 
+import argparse
 import os
+
+try:
+    from tqdm import tqdm
+except ImportError:
+    tqdm = lambda x: x
 
 EXCLUSIONS = ['.git']
 
@@ -20,26 +26,37 @@ class Node:
         return '{} : {} files, {} subdirectories'.format(self.path, self.files, self.subdirs)
 
 
-filesystem = {}
-leaves = []
+class PrintHandler:
+    def handle(self, node):
+        print(node)
+        return False
 
-for path, subdirs, files in os.walk('.'):
-    if path == '.':
-        # we'll not delete CWD anyway
-        continue
+    def close(self):
+        pass
 
-    node = Node()
-    node.path = path
-    node.parent = os.path.dirname(path)
-    node.files = len(files)
-    node.subdirs = node.subdirs_left = len(subdirs)
 
-    filesystem[path] = node
+class RemoveDirHandler:
+    def __init__(self):
+        self.log = open('removed_directories.txt', 'a')
 
-    if node.files == 0 and node.subdirs == 0:
-        leaves.append(node)
+    def handle(self, node):
+        if any(exclusion in node.path for exclusion in EXCLUSIONS):
+            print('Not removing (excluded):', node.path)
+            return False
 
-    # print(node)
+        print('Removing:', node.path)
+        os.rmdir(node.path)
+
+        if self.log:
+            self.log.write(node.path)
+            self.log.write('\n')
+
+        return True
+
+    def close(self):
+        if self.log:
+            self.log.close()
+            self.log = None
 
 
 def remove_from_parent(filesystem, node, new_leaves):
@@ -48,21 +65,59 @@ def remove_from_parent(filesystem, node, new_leaves):
         return
 
     parent.subdirs_left -= 1
-    if parent.subdirs_left == 0:
+    if parent.subdirs_left == 0 and parent.files == 0:
         new_leaves.append(parent)
 
 
-new_leaves = deque()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rmdir', '-x', action='store_true', help='remove empty directories and log')
+    args = parser.parse_args()
 
-print('Empty directories:')
-for node in leaves:
-    print(node.path)
-    remove_from_parent(filesystem, node, new_leaves)
+    filesystem = {}
+    leaves = []
+    handler = RemoveDirHandler() if args.rmdir else PrintHandler()
 
-if new_leaves:
-    print('Recursively empty directories:')
+    for path, subdirs, files in tqdm(os.walk('.')):
+        if path == '.':
+            # we'll not delete CWD anyway
+            continue
 
-while new_leaves:
-    node = new_leaves.popleft()
-    print(node)
-    remove_from_parent(filesystem, node, new_leaves)
+        node = Node()
+        node.path = path
+        node.parent = os.path.dirname(path)
+        node.files = len(files)
+        node.subdirs = node.subdirs_left = len(subdirs)
+
+        filesystem[path] = node
+
+        if node.files == 0 and node.subdirs == 0:
+            leaves.append(node)
+
+
+    new_leaves = deque()
+    count = len(leaves)
+    removed = 0
+
+    print('Empty directories: ({})'.format(count))
+    for node in leaves:
+        removed += handler.handle(node)
+        remove_from_parent(filesystem, node, new_leaves)
+
+    if new_leaves:
+        print('Recursively empty directories:')
+
+    while new_leaves:
+        node = new_leaves.popleft()
+        removed += handler.handle(node)
+        remove_from_parent(filesystem, node, new_leaves)
+        count += 1
+
+    handler.close()
+    print('{} total empty directories'.format(count))
+    if args.rmdir:
+        print('{} total directories removed'.format(removed))
+
+
+if __name__ == '__main__':
+    main()
