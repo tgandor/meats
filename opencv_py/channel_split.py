e077580b7b1bf584c0f18aac7790b51ac117370e
tgandor <tomasz.gandor@gmail.com>
2019-05-30 07:29:53 +0200

    downsample for HoG (quite bad results for NN downscaling), updated channel_split.py

--- opencv_py/channel_split.py
+++ opencv_py/channel_split.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-import cv2
-import numpy as np
+import argparse
+import glob
 import os
 import sys
 
-SHOW = True
-SAVE = False
+import numpy as np
+import cv2
 
 
-def split_channels(filename):
+def split_channels(filename, show=True, save=False):
     img = cv2.imread(filename)
     if len(img.shape) != 3 or img.shape[2] != 3:
         sys.stderr.write('{0}: not a correct color image'.format(filename))
@@ -19,23 +19,27 @@ def split_channels(filename):
     red_img = cv2.merge([zero_channel, zero_channel, channels[2]])
     green_img = cv2.merge([zero_channel, channels[1], zero_channel])
     blue_img = cv2.merge([channels[0], zero_channel, zero_channel])
-    if SHOW:
+    if show:
         cv2.imshow('Red channel', red_img)
         cv2.imshow('Green channel', green_img)
         cv2.imshow('Blue channel', blue_img)
         cv2.waitKey(0)
-    if SAVE:
+    if save:
         name, extension = os.path.splitext(filename)
         cv2.imwrite(name+'_red'+extension, red_img)
         cv2.imwrite(name+'_green'+extension, green_img)
         cv2.imwrite(name+'_blue'+extension, blue_img)
 
 
-def main():
-    if len(sys.argv) < 2:
-        print('Usage: {0} <rgb_image>...'.format(sys.argv[0]))
-    map(split_channels, sys.argv[1:])
+def main(args):
+    for filename in args.files:
+        split_channels(filename, not args.no_show, args.save)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--save', '-s', action='store_true', help='save channels as separate files')
+    parser.add_argument('--no-show', '-q', action='store_true', help='skip displaying results (useful for --save)')
+    parser.add_argument('files', nargs='+')
+    args = parser.parse_args()
+    main(args)
--- opencv_py/hog.py
+++ opencv_py/hog.py
@@ -5,24 +5,27 @@ from __future__ import print_function
 # Based off of Adrian:
 # https://www.pyimagesearch.com/2015/11/09/pedestrian-detection-opencv/
 
+import argparse
 import sys
 import time
 
 import cv2
 
+parser = argparse.ArgumentParser()
+parser.add_argument('webcam', nargs='?', default='0')
+parser.add_argument('--downsample', '-d', type=int, default=1)
+args = parser.parse_args()
+
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-if len(sys.argv) > 1:
-    try:
-        webcam = int(sys.argv[1])
-    except ValueError:
-        # not a webcam...
-        webcam = sys.argv[1]
+try:
+    webcam = int(args.webcam)
+except ValueError:
+    # not a webcam...
+    webcam = args.webcam
 
-    cap = cv2.VideoCapture(webcam)
-else:
-    cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(webcam)
 
 start = 0
 
@@ -34,6 +37,10 @@ while True:
         break
 
     print('FPS: {:.1f}'.format(1 / (time.time() - start)))
+
+    if args.downsample > 1:
+        # copy to make contiguous, without importing numpy as np
+        image = image[::args.downsample, ::args.downsample, :].copy()
 
     start = time.time()
     rects, weights = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
@@ -53,9 +60,6 @@ while True:
     cv2.imshow('image', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-
-
 
 cap.release()
 cv2.destroyAllWindows()
