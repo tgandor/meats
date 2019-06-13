31b6ec6ffbfe206b9ec9cf3a446cc04da0ceff7b
tgandor <tomasz.gandor@gmail.com>
2019-06-13 14:38:23 +0200

    dates\image_size_stats.py - counting image sizes in CWD (recursively). Impractical because only EXIF based. So far.

---
+++ dates/image_size_stats.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+from collections import Counter
+
+try:
+    import piexif
+except ImportError:
+    print('Missing piexif')
+    os.system('pip install piexif')
+    exit()
+
+size_stats = Counter()
+examples = {}
+
+for path, directories, files in os.walk('.'):
+    for basename in files:
+        if basename.lower().endswith('.jpg'):
+            realname = os.path.join(path, basename)
+            d = piexif.load(realname)
+
+            try:
+                size = (d["0th"][piexif.ImageIFD.ImageWidth], d["0th"][piexif.ImageIFD.ImageLength])
+            except KeyError:
+                print('Error reading size from EXIF:', realname)
+                continue
+
+            size_stats[size] += 1
+            if size not in examples:
+                examples[size] = realname
+
+if len(size_stats) == 0:
+    print('No images found. Sorry.')
+    exit()
+
+for size, count in size_stats.items():
+    print(size, count, 'example:', examples[size])
+
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    print('Missing matplotlib. No plot to show.')
+    exit()
+
+def transpose(tuples):
+    return list(zip(*tuples))
+
+
+w, h, n = transpose([(width, height, count) for (width, height), count in size_stats.items()])
+
+plt.scatter(w, h, s=n)
+
+# keep the visible aspect ratio -
+# sorry, this doesn't work:
+# plt.axis('equal')  # https://stackoverflow.com/a/2935000/1338797
+# but this, does.
+plt.axis('scaled')  # https://stackoverflow.com/a/35994245/1338797
+
+# reset axes min to 0:
+plt.xlim(xmin=0)
+plt.ylim(ymin=0)
+
+plt.show()
