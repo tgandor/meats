b779067c0535e3bd76f960c9943d958dee8fddb7
tgandor <tomasz.gandor@gmail.com>
2019-07-18 10:54:11 +0200

    lang_lawyer/python/awaitable_37.py - fixed indent, added compatiblity between 3.6 and 3.7

--- lang_lawyer/python/awaitable_37.py
+++ lang_lawyer/python/awaitable_37.py
@@ -1,17 +1,30 @@
 # https://stackoverflow.com/questions/33409888/how-can-i-await-inside-future-like-objects-await/
 # super annoying
 
-    import asyncio
+import asyncio
 
 
-    class Waiting:
-        def __await__(self):
-            yield from asyncio.sleep(2).__await__()
-            print('ok')
+def sync_await(gen):
+    if hasattr(gen, '__await__'):
+        # 3.7
+        print('yield from gen.__await__()')
+        yield from gen.__await__()
+    else:
+        # 3.6
+        print('yield from gen')
+        yield from gen
 
-    async def main():
-        await Waiting()
 
-    if __name__ == "__main__":
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
+class Waiting:
+    def __await__(self):
+        yield from sync_await(asyncio.sleep(2))
+        print('ok')
+
+
+async def main():
+    await Waiting()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
