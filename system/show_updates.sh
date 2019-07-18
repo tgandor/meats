c1994d314ef9b2900b2078c477eb24fe2a1ab82e
tgandor <tomasz.gandor@gmail.com>
2019-07-18 11:12:15 +0200

    system/show_updates.sh - now only decluttered names of packages, paged.

--- system/show_updates.sh
+++ system/show_updates.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get --just-print upgrade | egrep -v '^Inst |^Conf '
-apt list --upgradeable
+# apt-get --just-print upgrade | egrep -v '^Inst |^Conf '
+apt list --upgradable | tail -n+2 | cut -d/ -f1 | nl | less
