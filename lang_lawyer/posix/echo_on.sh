e9a2701b77ceba713c19cbd8574eb826ede37699
tgandor <tomasz.gandor@gmail.com>
2019-08-02 06:55:36 +0200

    lang_lawyer/posix/echo_on.sh - showing the difference: shell indirections.

--- lang_lawyer/posix/echo_on.sh
+++ lang_lawyer/posix/echo_on.sh
@@ -9,14 +9,35 @@
 set -x
 sleep 1
 sleep 2
-sleep 3
 # echo off, itself echoed
 set +x
-echo done -x
+echo ---- done -x ----
 
 # a different echo - reports lines, not every step in a pipe, no '+' prefix
 set -v
 sleep 1
 sleep 2
 set +v
-echo done -v
+echo ---- done -v ----
+echo
+
+echo Now: indirections etc. Example: "cat < /dev/null | wc -l > /dev/null"
+echo
+
+echo ---- -x ----
+set -x
+cat < /dev/null | wc -l > /dev/null
+set +x
+echo
+
+echo ---- -v ----
+set -v
+cat < /dev/null | wc -l > /dev/null
+set +v
+echo
+
+echo ---- -vx ----
+set -vx
+cat < /dev/null | wc -l > /dev/null
+set +vx
+echo
