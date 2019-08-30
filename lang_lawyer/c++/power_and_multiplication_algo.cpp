c3cb5b78aee08b401d1f9f6972eb698fa842fc45
tgandor <tomasz.gandor@gmail.com>
2019-08-30 12:52:57 +0200

    power_and_multiplication_algo.cpp - adding bits() template returning an iterator for range-based for.

--- lang_lawyer/c++/power_and_multiplication_algo.cpp
+++ lang_lawyer/c++/power_and_multiplication_algo.cpp
@@ -1,19 +1,40 @@
 // compile (link) with: -lgtest
 
+// why do we need bits_iter class template and bits function template?
+// https://stackoverflow.com/questions/984394/why-not-infer-template-parameter-from-constructor
+
+template <class T>
+class bits_iter
+{
+    T value;
+public:
+    bits_iter(T init_value) : value(init_value) {}
+    bool operator*() { return value & 1 == 1; }
+    bits_iter<T> begin() { return *this; }
+    bits_iter<T> end() { return bits_iter<T>(T()); }
+    void operator++() { value >>= 1; }
+    bool operator!=(const bits_iter<T>& other) { return value != other.value; }
+};
+
+template <class T>
+bits_iter<T> bits(T value)
+{
+    return bits_iter<T>(value);
+}
+
+// well, so this is out of date, and the bits_iter could be renamed to bits,
+// but then it would require mandatory C++17 support.
+
 template <class Base, class Factor>
 Base multiply(Base base, Factor factor)
 {
     Base result = Base();
-    for (;;)
+    for (auto bit : bits(factor))
     {
-        if (factor & 1)
+        if (bit)
         {
             result += base;
         }
-
-        factor >>= 1;
-        if (!factor)
-            break;
 
         base += base;
     }
@@ -25,16 +46,12 @@ template <class Base, class Exponent>
 Base power(Base base, Exponent exponent)
 {
     Base result = Base(1);
-    for (;;)
+    for (auto bit : bits(exponent))
     {
-        if (exponent & 1)
+        if (bit)
         {
             result *= base;
         }
-
-        exponent >>= 1;
-        if (!exponent)
-            break;
 
         base *= base;
     }
@@ -87,6 +104,27 @@ TEST(MultiplyTest, float_by_int)
 TEST(MultiplyTest, string_by_int)
 {
     GTEST_ASSERT_EQ(std::string("aaaaa"), multiply(std::string("a"), 5));
+}
+
+TEST(BitsTest, test_iteration)
+{
+    int v = 234;
+    auto iterator = bits(v);
+    for (auto bit : iterator)
+    {
+        GTEST_ASSERT_EQ(bit, v & 1 == 1);
+        v >>= 1;
+    }
+}
+
+TEST(BitsTest, test_number_of_bits)
+{
+    int num_bits = 0;
+    for (auto bit : bits(8))
+    {
+        ++num_bits;
+    }
+    GTEST_ASSERT_EQ(num_bits, 4);
 }
 
 int main(int argc, char *argv[])
