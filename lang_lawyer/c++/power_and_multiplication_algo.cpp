3824e642c7f7a4145b262b7b64df8803b460bfd4
tgandor <tomasz.gandor@gmail.com>
2019-08-31 23:30:26 +0200

    lang_lawyer/c++/power_and_multiplication_algo.cpp - rudimentary and incomplete Bignum.

--- lang_lawyer/c++/power_and_multiplication_algo.cpp
+++ lang_lawyer/c++/power_and_multiplication_algo.cpp
@@ -59,11 +59,76 @@ Base power(Base base, Exponent exponent)
     return result;
 }
 
+#include <vector>
+
+std::vector<int> segment(int value, int modulus)
+{
+    std::vector<int> result;
+
+    do {
+        result.push_back(value % modulus);
+        value /= modulus;
+    } while (value);
+
+    return result;
+}
+
 class Bignum
 {
-    // void operator*=(const Bignum& other);
+    const int BIN_MOD = 1 << 10;
+    const int DEC_MOD = 1000;
+
+    std::vector<int> bin_segments;
+    std::vector<int> dec_segments;
+public:
+    Bignum(int value = 0) :
+        bin_segments(segment(value, BIN_MOD)),
+        dec_segments(segment(value, DEC_MOD))
+    {}
+
+    Bignum(const Bignum& other) = default;
+
+    Bignum operator+(const Bignum& other);
+
+    bool operator==(const Bignum& other) const
+    {
+        return dec_segments == other.dec_segments && bin_segments == other.bin_segments;
+    }
+
+    const std::vector<int>& chunks() { return dec_segments; }
 };
 
+std::vector<int> sum_vectors(const std::vector<int>& a, const std::vector<int>& b, int modulus)
+{
+    std::vector<int> result;
+    auto ai = a.begin();
+    auto bi = b.begin();
+    int carry = 0;
+    do {
+        if (ai != a.end())
+        {
+            carry += *ai++;
+        }
+
+        if (bi != b.end())
+        {
+            carry += *bi++;
+        }
+
+        result.push_back(carry % modulus);
+        carry /= modulus;
+    } while (carry || ai != a.end() || bi != b.end());
+
+    return result;
+}
+
+Bignum Bignum::operator+(const Bignum& other)
+{
+    Bignum result;
+    result.bin_segments = sum_vectors(this->bin_segments, other.bin_segments, BIN_MOD);
+    result.dec_segments = sum_vectors(this->dec_segments, other.dec_segments, DEC_MOD);
+    return result;
+}
 
 #include <gtest/gtest.h>
 
@@ -125,6 +190,11 @@ TEST(BitsTest, test_number_of_bits)
         ++num_bits;
     }
     GTEST_ASSERT_EQ(num_bits, 4);
+}
+
+TEST(Bignum, addition)
+{
+    GTEST_ASSERT_EQ(Bignum(5511) + Bignum(5522), Bignum(11033));
 }
 
 int main(int argc, char *argv[])
