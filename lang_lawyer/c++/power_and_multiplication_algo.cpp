e2bf8433fea9f5a3207466c584c4a2bc830c0204
tgandor <tomasz.gandor@gmail.com>
2019-09-04 13:47:50 +0200

    lang_lawyer/c++/power_and_multiplication_algo.cpp - Bignum bits_iter, bin() and dec() functions.

--- lang_lawyer/c++/power_and_multiplication_algo.cpp
+++ lang_lawyer/c++/power_and_multiplication_algo.cpp
@@ -1,10 +1,16 @@
 // compile (link) with: -lgtest
 
+#include <gtest/gtest.h>
+
 // why do we need bits_iter class template and bits function template?
 // https://stackoverflow.com/questions/984394/why-not-infer-template-parameter-from-constructor
 
+// https://stackoverflow.com/a/1724039/1338797
+// wow, thanks SO!
+// I almost went through the pain of fully specifying iterator_traits
+// myself. But sometimes it's enough to inherit...
 template <class T>
-class bits_iter
+class bits_iter : public std::iterator<std::input_iterator_tag, bool>
 {
     T value;
 public:
@@ -75,12 +81,12 @@ std::vector<int> segment(int value, int modulus)
 
 class Bignum
 {
-    const int BIN_MOD = 1 << 10;
-    const int DEC_MOD = 1000;
-
     std::vector<int> bin_segments;
     std::vector<int> dec_segments;
 public:
+    static const int BIN_MOD = 1 << 10;
+    static const int DEC_MOD = 1000;
+
     Bignum(int value = 0) :
         bin_segments(segment(value, BIN_MOD)),
         dec_segments(segment(value, DEC_MOD))
@@ -95,8 +101,35 @@ public:
         return dec_segments == other.dec_segments && bin_segments == other.bin_segments;
     }
 
-    const std::vector<int>& chunks() { return dec_segments; }
+    const std::vector<int>& chunks() const { return dec_segments; }
+
+    const std::vector<int>& bin_chunks() const { return bin_segments; }
 };
+
+std::string dec(const Bignum& num)
+{
+    std::string result;
+    for(int i = 0; i < num.chunks().size() - 1; ++i)
+    {
+        int val = num.chunks()[i];
+
+        for (int f = Bignum::DEC_MOD / 10; f > 0; f /= 10)
+        {
+            result += ('0' + val % 10);
+            val /= 10;
+        }
+    }
+
+    int val = num.chunks().back();
+    while (val > 0)
+    {
+        result += ('0' + val % 10);
+        val /= 10;
+    }
+
+    std::reverse(result.begin(), result.end());
+    return result;
+}
 
 std::vector<int> sum_vectors(const std::vector<int>& a, const std::vector<int>& b, int modulus)
 {
@@ -130,7 +163,64 @@ Bignum Bignum::operator+(const Bignum& other)
     return result;
 }
 
-#include <gtest/gtest.h>
+template <>
+class bits_iter<Bignum> : public std::iterator<std::input_iterator_tag, bool>
+{
+    bool done;
+    Bignum value;
+    int chunk_index;
+    int flag_mask;
+    int last_chunk;
+public:
+    bits_iter() : done(true), value() {}
+    bits_iter(Bignum init_value) :
+        done(false), value(init_value),
+        chunk_index(0), flag_mask(1),
+        last_chunk(value.bin_chunks().back())
+    {}
+    bool operator*() {
+        if (chunk_index < value.bin_chunks().size() - 1)
+        {
+            return (value.bin_chunks()[chunk_index] & flag_mask) != 0;
+        }
+
+        return (last_chunk & 1) == 1;
+    }
+    bits_iter<Bignum> begin() { return *this; }
+    bits_iter<Bignum> end() { return bits_iter<Bignum>(); }
+    void operator++() {
+        if (chunk_index < value.bin_chunks().size() - 1)
+        {
+            flag_mask <<= 1;
+            if (flag_mask == Bignum::BIN_MOD)
+            {
+                flag_mask = 1;
+                ++chunk_index;
+            }
+            return;
+        }
+
+        last_chunk >>= 1;
+        if (last_chunk == 0)
+        {
+            done = true;
+        }
+    }
+    bool operator!=(const bits_iter<Bignum>& other) { return done != other.done; }
+};
+
+template <class T>
+std::string bin(const T& value)
+{
+    std::string result;
+    for (auto bit : bits(value))
+    {
+        result.push_back(bit ? '1' : '0');
+    }
+
+    std::reverse(result.begin(), result.end());
+    return result;
+}
 
 // region: power tests
 TEST(PowerTest, something_to_zero_power)
@@ -192,9 +282,44 @@ TEST(BitsTest, test_number_of_bits)
     GTEST_ASSERT_EQ(num_bits, 4);
 }
 
+TEST(BitsTest, bin_to_string)
+{
+    GTEST_ASSERT_EQ("1010", bin(10));
+}
+
 TEST(Bignum, addition)
 {
     GTEST_ASSERT_EQ(Bignum(5511) + Bignum(5522), Bignum(11033));
+}
+
+TEST(Bignum, dec)
+{
+    GTEST_ASSERT_EQ("11033", dec(Bignum(11033)));
+    GTEST_ASSERT_EQ("987654321", dec(Bignum(987654321)));
+}
+
+TEST(BignumBitsIter, compare_with_int_bits_length)
+{
+    const int VALUE = 12345678;
+    auto int_bits = bits(VALUE);
+    auto bignum_bits = bits(Bignum(VALUE));
+    auto len_int = std::distance(int_bits.begin(), int_bits.end());
+    auto len_bignum = std::distance(bignum_bits.begin(), bignum_bits.end());
+    GTEST_ASSERT_EQ(len_int, len_bignum);
+}
+
+TEST(BignumBitsIter, compare_with_string_value)
+{
+    const int VALUE = 12345678;
+    GTEST_ASSERT_EQ("101111000110000101001110", bin(Bignum(VALUE)));
+}
+
+TEST(BignumBitsIter, compare_with_int_bits_values)
+{
+    const int VALUE = 12345678;
+    auto int_bits = bits(VALUE);
+    auto bignum_bits = bits(Bignum(VALUE));
+    GTEST_ASSERT_EQ(true, std::equal(int_bits.begin(), int_bits.end(), bignum_bits.begin()));
 }
 
 int main(int argc, char *argv[])
