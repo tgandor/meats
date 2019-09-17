cf4b6436b5d76808ffed112834bef95d774fd20b
tgandor <tomasz.gandor@gmail.com>
2019-09-17 13:10:10 +0200

    system/show_updates.sh - added Debian/Arch multiplatform-ness.

--- system/show_updates.sh
+++ system/show_updates.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# Old Debian / Ubuntu, before `apt` command:
 # apt-get --just-print upgrade | egrep -v '^Inst |^Conf '
-apt list --upgradable | tail -n+2 | cut -d/ -f1 | nl | less
+
+# Debian / Ubuntu
+if which apt &> /dev/null ; then
+    apt list --upgradable | tail -n+2 | cut -d/ -f1 | nl | less
+fi
+
+# Arch / Manjaro (pacman)
+# please note: pacman -Qu shows nothing when Octopi reports new updates.
+if which checkupdates &> /dev/null ; then
+    checkupdates
+fi
