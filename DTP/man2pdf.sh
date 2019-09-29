9763b1cee341b1d3f85cc56f742d5a7d8dbadc56
tgandor <tomasz.gandor@gmail.com>
2019-09-29 23:27:50 +0200

    DTP/man2pdf.sh - prevent overwriting PDFs if glob hits many files: e.g. mount(2) vs mount(8).

--- DTP/man2pdf.sh
+++ DTP/man2pdf.sh
@@ -2,6 +2,6 @@
 
 for f in /usr/share/man/man*/$1.*.gz ; do
   echo Processing $f
-  zcat $f | groff -t -e -mandoc -Tps | ps2pdf14 - $1.pdf
+  zcat $f | groff -t -e -mandoc -Tps | ps2pdf14 - `basename $f .gz`.pdf
 done
 
