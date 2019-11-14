bea5e83b743ac86f46484b14953bdfd0f8615f02
tgandor <tomasz.gandor@gmail.com>
2019-11-14 23:28:38 +0000

    missing/dtd.sh - adding df to the mix.

--- missing/dtd.sh
+++ missing/dtd.sh
@@ -3,6 +3,8 @@
 # Date - time - date: time a command with start and begin.
 
 date | tee .dtd_last
+df | tee -a .dtd_last
 time "$@" 2>&1 | tee -a .dtd_last
+df | tee -a .dtd_last
 date | tee -a .dtd_last
 
