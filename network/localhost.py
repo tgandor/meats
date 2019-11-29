b41f68a89efe213b6fd047a163ca88f100f2daa5
tgandor <tomasz.gandor@gmail.com>
2019-11-29 16:31:52 +0000

    network/localhost.py - fix regex.

--- network/localhost.py
+++ network/localhost.py
@@ -23,8 +23,8 @@ if sys.platform == 'win32':
     config = os.popen('ipconfig').read()
     regexp = r'IP(?:v4)? Address(?:[ .]*):\s*([0-9.]+)'
 else:
-    regexp = 'inet (?:addr:)([0-9.]+)'
     config = os.popen('ifconfig').read()
+    regexp = 'inet (?:addr:)?([0-9.]+)'
 
 for ip in re.findall(regexp, config):
     if ip == '127.0.0.1':
