1c75b21f0b67e42479fc1a1a42dec612d14d7866
tgandor <tomasz.gandor@gmail.com>
2019-12-06 08:41:12 +0100

    log_analysis\base64_extract.py - added image format recognition (ignoring other for now).

--- log_analysis/base64_extract.py
+++ log_analysis/base64_extract.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import base64
+import imghdr
 import re
 import sys
 
@@ -10,15 +11,22 @@ b64_regex = re.compile(
 )
 
 input_data = sys.stdin.read()
-suffix = '.png'
 
 for i, match in enumerate(b64_regex.finditer(input_data)):
     s = match.span()
     print(i, match, '({:,} characters)'.format(s[1] - s[0]))
     data = match.group().encode()
-    out_filename = 'b64_decoded_{}{}'.format(i, suffix)
+    decoded = base64.decodebytes(data)
+
+    extension = imghdr.what(None, decoded)
+    if extension is None:
+        print('Not a known image format, skipping.')
+        continue
+
+    out_filename = 'b64_decoded_{}.{}'.format(i, extension)
+
     with open(out_filename, 'wb') as out_file:
-        decoded = base64.decodebytes(data)
         out_file.write(decoded)
+
     print('{:,} decoded bytes written to {}'.format(len(decoded), out_filename))
     print('---')
