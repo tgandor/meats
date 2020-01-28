dd597a666fc8bef494638325199b5e4c3efa7c4e
tgandor <tomasz.gandor@gmail.com>
2020-01-28 19:32:26 +0100

    media\pylisten_volume.py - dump volumes to text file, print value, sqrt.

--- media/pylisten_volume.py
+++ media/pylisten_volume.py
@@ -3,6 +3,7 @@
 # https://github.com/MycroftAI/pylisten
 
 import argparse
+import math
 
 from pylisten import Listener, WindowListener, FeatureListener
 
@@ -10,11 +11,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--window', action='store_true')
 parser.add_argument('--feature', action='store_true')
 parser.add_argument('--listener', action='store_true')
+parser.add_argument('--output', '-o', help='output file for volume')
+parser.add_argument('--limit', '-l', type=int, help='max samples to process / save')
+parser.add_argument('--sqrt', action='store_true', help='sqare root of volume (stars)')
 args = parser.parse_args()
 
 if args.listener:
-    for chunk in Listener(frames_per_buffer=512, rate=24100):
-        print(chunk.shape, 'Current volume:', abs(chunk).mean())
+    for chunk in Listener(frames_per_buffer=1024, rate=44100):
+        print(chunk.shape, 'Current volume:', abs(chunk).mean(), 'span:', chunk.min(), chunk.max())
 elif args.feature:
     for features in FeatureListener(lambda x: [abs(x).mean()], 1024, 20):
         print(features.shape, 'Past 20 volumes:', features)
@@ -24,6 +28,21 @@ elif args.window:
 else:
     # inspired by:
     # https://www.swharden.com/wp/2016-07-19-realtime-audio-visualization-in-python/
-    for chunk in Listener(frames_per_buffer=512, rate=24100):
-        vol = int(abs(chunk).mean() * 1000)
-        print('*' * vol)
+    count = 0
+    output = None
+    try:
+        if args.output:
+            output = open(args.output, 'w')
+        for chunk in Listener(frames_per_buffer=512, rate=24100):
+            vol = int(abs(chunk).mean() * 1000)
+            if args.sqrt:
+                vol = int(math.sqrt(vol))
+            print('{:3d}'.format(vol), '*' * vol)
+            count += 1
+            if count == args.limit:
+                break
+            if output:
+                output.write('{}\n'.format(vol))
+    finally:
+        if output:
+            output.close()
