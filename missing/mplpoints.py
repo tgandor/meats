3404983547642696aa64b861af18c02548e4e455
tgandor <tomasz.gandor@gmail.com>
2020-01-28 19:29:12 +0100

    missing\mplpoints.py - renewed and added option to not annotate; plot single column.

--- missing/mplpoints.py
+++ missing/mplpoints.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 
-import matplotlib.pyplot as plt
+import argparse
 import sys
+
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-labels', '-n', action='store_true')
+args = parser.parse_args()
 
 x = []
 y = []
 
-for line in sys.stdin.readlines():
+for i, line in enumerate(sys.stdin.readlines()):
     chunks = line.split()[:2]
-    if len(chunks) < 2:
+    if len(chunks) < 1:
         continue
-    xi, yi = map(float, chunks)
+
+    if len(chunks) == 1:
+        xi = i
+        yi = float(chunks[0])
+    else:
+        xi, yi = map(float, chunks)
+
     x.append(xi)
     y.append(yi)
 
 plt.scatter(x, y)
-for i in xrange(len(x)):
-    plt.annotate(str(i), xy=(x[i], y[i]), xytext=(x[i]-0.3, y[i]-0.3))
+
+if not args.no_labels:
+    # limit labels to about 100 (by skipping)
+    for i in range(0, len(x), len(x) // 100 + 1):
+        plt.annotate(str(i), xy=(x[i], y[i]), xytext=(x[i]-0.3, y[i]-0.3))
+
 plt.show()
