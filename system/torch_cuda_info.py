dab88f750f38cc61231fca47eb65674e8e379214
tgandor <tomasz.gandor@gmail.com>
2020-02-20 07:18:10 +0100

    system/torch_cuda_info.py - show info about GPU.

---
+++ system/torch_cuda_info.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# https://stackoverflow.com/a/53374933/1338797
+
+import torch
+
+print('Device count:', torch.cuda.device_count())
+
+# setting device on GPU if available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+print()
+
+#Additional Info when using cuda
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
