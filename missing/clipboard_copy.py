ce904a0bbddb6f482b7cd5863136cbe180ce40c2
tgandor <tomasz.gandor@gmail.com>
2020-03-26 06:42:54 +0100

    missing/clipboard_copy.py - fixed 'simple' mode (no window, press enter).

--- missing/clipboard_copy.py
+++ missing/clipboard_copy.py
@@ -2,7 +2,9 @@
 
 from __future__ import print_function
 
+import queue
 import sys
+import threading
 
 
 def copy(data, wait_gui=False):
@@ -13,16 +15,36 @@ def copy(data, wait_gui=False):
         # welcome to Python3
         import tkinter as tk
 
+    pill_q = queue.Queue()
+
     r = tk.Tk()
     r.withdraw()
     r.clipboard_clear()
     r.clipboard_append(data)
 
+    def read_stdin(q):
+        sys.stdin.read(1)  # not all, just whatever
+        q.put(True)  # sending death pill
+
+    def poll():
+        if pill_q.empty():
+            r.after(100, poll)
+        else:
+            r.destroy()
+
     if not wait_gui:
         print('Data was copied into clipboard. Paste and press ENTER to exit...')
-        for _ in sys.stdin:
-            break
-        r.destroy()
+        # This won't work: RuntimeError: Calling Tcl from different apartment
+        '''
+        gui_thread = threading.Thread(target=r.mainloop)
+        gui_thread.setDaemon(True)
+        gui_thread.start()
+        '''
+        # such an overkill, but this is how it can work:
+        # https://effbot.org/zone/tkinter-threads.htm
+        threading.Thread(target=read_stdin, args=(pill_q,)).start()
+        r.after(100, poll)
+        r.mainloop()
     else:
         # stdin already read; use GUI to exit
         print('Data was copied into clipboard. Paste, then close popup to exit...')
