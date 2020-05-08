8bca72ba831840debd4650cd5c3a3c2c75ee68ce
tgandor <tomasz.gandor@gmail.com>
2020-05-08 18:05:27 +0200

    vcs/dump_office_documents.sh - handle UTF-8 in XLS, dump spreadsheets as CSV (txt fails).

--- vcs/dump_office_documents.sh
+++ vcs/dump_office_documents.sh
@@ -12,19 +12,21 @@ convert() {
     # not so good for nested stuff, so:
     working_dir=`dirname "$1"`
     name=`basename "$1"`
-    pushd "$working_dir"
-    libreoffice --headless --convert-to "txt:Text (encoded):UTF8" "$name"
-    popd
+    pushd "$working_dir" >/dev/null
+    libreoffice --headless --convert-to "$2" $3 "$name"
+    popd >/dev/null
 }
 
 find_and_convert() {
     echo "Searching for $1 ..."
     find -name "$1" | while read f; do
-        convert "$f"
+        convert "$f" "$2" $3
     done
 }
 
-find_and_convert '*.docx'
-find_and_convert '*.doc'
-find_and_convert '*.xlsx'
-find_and_convert '*.xls'
+find_and_convert '*.docx' 'txt:Text (encoded):UTF8'
+find_and_convert '*.doc' 'txt:Text (encoded):UTF8'
+# this is so ghasty:
+# https://unix.stackexchange.com/questions/259361/specify-encoding-with-libreoffice-convert-to-csv
+find_and_convert '*.xlsx' csv --infilter=CSV:44,34,76,1
+find_and_convert '*.xls' csv --infilter=CSV:44,34,76,1
