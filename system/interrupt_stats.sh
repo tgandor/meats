a43ca9495d88dd2ee65a59a30c35a97e90026a27
tgandor <tomasz.gandor@gmail.com>
2020-05-22 01:13:31 +0200

    system/interrupt_stats.sh - show interrupts and their info (frequency...).

---
+++ system/interrupt_stats.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Some interrupts eating your battery? Check what they are:
+# https://unix.stackexchange.com/questions/398520/kworker-consumes-high-cpu-for-external-hard-drive
+# this is a nice abuse of grep, to print filenames and their content on a single line:
+# grep . -r /sys/firmware/acpi/interrupts/
+
+# but we want to sort it right away, so...
+
+for int in /sys/firmware/acpi/interrupts/* ; do echo `cat $int` $int ; done | sort -n
+
+# See also:
+# https://forum.manjaro.org/t/kworker-kacpid-cpu-100/131532
+# perf record -g -a sleep 10
+# perf report
+
