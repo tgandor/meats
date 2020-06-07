96368713f86d3c62a019b050460413ef45b318c3
tgandor <tomasz.gandor@gmail.com>
2020-06-07 14:23:17 +0200

    network/how_long_to_download.py - handling lowercase units, showing results.

--- network/how_long_to_download.py
+++ network/how_long_to_download.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 
 import argparse
+import datetime
 import re
 import time
-
-parser = argparse.ArgumentParser()
-parser.add_argument('size')
-parser.add_argument('speed_per_second')
-args = parser.parse_args()
 
 FACTORS = {
     suffix: 1024**(i+1)
     for i, suffix in enumerate('KMGTP')
 }
+# no generator possible: dict size changed during iteration
+FACTORS.update([(k.lower(), v) for k, v in FACTORS.items()])
 FACTORS[None] = 1
 
 
@@ -27,7 +25,7 @@ def parse_human(size):
 
     m = re.match(r'(\d+)(\.\d*)?([kmgtp]?)$', size, re.IGNORECASE)
     assert m, r'size must match (\d+)(\.\d*)?([kmgtp]?)$'
-    print(m, m.groups())
+    # print(m, m.groups())
     whole, frac, unit = m.groups()
 
     result = int(whole)
@@ -39,5 +37,15 @@ def parse_human(size):
     return result
 
 
-num_bytes = parse_human(args.size)
-bauds = parse_human(args.speed_per_second)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('size')
+    parser.add_argument('speed_per_second')
+    args = parser.parse_args()
+
+    num_bytes = parse_human(args.size)
+    bauds = parse_human(args.speed_per_second)
+
+    seconds = num_bytes / bauds
+    print('ETA: {:.1f} seconds:\n=    {}'.format(seconds, datetime.timedelta(seconds=seconds)))
+    print('ETD: {}'.format(datetime.datetime.now() + datetime.timedelta(seconds=seconds)))
