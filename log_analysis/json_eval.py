a20db25a263fbda1760e777f12d3e781682a31a4
tgandor <tomasz.gandor@gmail.com>
2020-06-12 18:53:40 +0200

    log_analysis/json_eval.py - new flag: -q - be like Python, don't print None.

--- log_analysis/json_eval.py
+++ log_analysis/json_eval.py
@@ -7,13 +7,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument('expression', help='The expression to evaluate on the JSON object, e.g. x["name"]')
 parser.add_argument('files', nargs='+')
 parser.add_argument('--name', '-n', action='store_true', help='print filename before value')
+parser.add_argument('--skip-none', '-q', action='store_true', help='print only when result != None')
 args = parser.parse_args()
 
 for f in args.files:
     with open(f) as stream:
         x = json.load(stream)
+
+    result = eval(args.expression)
+
+    if args.skip_none and result is None:
+        continue
+
     if args.name:
-        print(f, eval(args.expression))
+        print(f, result)
     else:
-        print(eval(args.expression))
+        print(result)
 
