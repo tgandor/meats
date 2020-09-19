6e43b416cace64173cbb75d6b3459bba8d38773a
tgandor <tomasz.gandor@gmail.com>
2020-09-19 22:46:34 +0200

    WiP: music_index: add ids to players.

--- www-php/music_index.php
+++ www-php/music_index.php
@@ -57,11 +57,11 @@
         <div class="row">
 <?php foreach($files as $i => $music_file): ?>
             <div class="col-sm-6 col-md-4 col-lg-3 py-3">
-                <p><?php echo $i + 1; ?>. <a href="<?php echo $music_file ?>"><?php echo $music_file ?></a></p>
+                <p><?php echo $i + 1 ?>. <a href="<?php echo $music_file ?>"><?php echo $music_file ?></a></p>
 <?php if (file_exists(basename("$music_file", "mp3") . "txt")): ?>
                 <p class="small"><?php readfile(basename("$music_file", "mp3") . "txt") ?></p>
 <?php endif ?>
-                <audio controls="controls" preload="none"<?php if($loop): ?> loop="loop"<?php endif ?>>
+                <audio controls="controls" preload="none"<?php if($loop): ?> loop="loop"<?php endif ?> id="a_<?php echo $i + 1 ?>">
                     <source src="<?php echo $music_file ?>" type="audio/mpeg">
                     Your browser does not support the audio element.
                 </audio>
@@ -88,6 +88,11 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script>
+    /* https://stackoverflow.com/questions/2551859/html-5-video-or-audio-playlist */
+    var audio = null;
+    // TODO: "auto play"
+    </script>
   </body>
 </html>
 <!-- Acknowledgements:
