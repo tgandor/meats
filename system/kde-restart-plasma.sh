0db6408120eb464c461d2c91559ab5dd6fca06b1
tgandor <tomasz.gandor@gmail.com>
2020-09-29 20:30:45 +0200

    system/kde-restart-plasma.sh - another emergency hammer.

---
+++ system/kde-restart-plasma.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://askubuntu.com/a/481738/309037
+# this may also fix the "window decoration" problems
+
+kquitapp5 plasmashell  # may not find any process
+kstart5 plasmashell
