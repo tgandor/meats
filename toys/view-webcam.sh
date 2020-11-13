23b8e9cdf30446f8ad4e8358aa85785ceecc2f26
tgandor <tomasz.gandor@gmail.com>
2020-11-13 10:31:44 +0100

    toys/view-webcam.sh - added mpv, without any options.

--- toys/view-webcam.sh
+++ toys/view-webcam.sh
@@ -8,8 +8,19 @@ elif [ $device == 1 ]
 elif [ $device == 2 ]
   then device=/dev/video2
 fi
+
 if [ -z $w ]; then w=1280; fi
 if [ -z $h ]; then h=720; fi
 if [ -z $fps ]; then fps=30; fi
-mplayer -fps $fps -vf screenshot -tv driver=v4l2:width=$w:height=$h:device=$device tv://
+
+if which mplayer ; then
+    mplayer -fps $fps -tv driver=v4l2:width=$w:height=$h:device=$device tv://
+elif which mpv ; then
+    echo FPS, w and h not supported...
+    mpv $device
+else
+    echo No supported player found.
+fi
+
+
 
