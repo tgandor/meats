5080caa9de4bf788373086506a60d48a746cfb8f
tgandor <tomasz.gandor@gmail.com>
2020-12-08 13:22:07 +0100

    missing/tohms.py - pushing it far towards its limits.

--- missing/tohms.py
+++ missing/tohms.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import re
 import sys
 
 
@@ -11,11 +12,13 @@ def print_secs(secs):
 
 if len(sys.argv) != 2:
     print("Usage:")
-    print("  %s [[H:]M:]S[.sss]      hours, minutes, seconds; M and S can be greater than 60" % sys.argv[0])
-    print("                              examples: 10 - 10s, 2:65 - 3m5s, 641 - 10m41s")
-    print("  %s <M>m<S.sss>s         <M> minutes <S.sss> seconds, <M> can be greater than 60" % sys.argv[0])
-    print("                              examples: 0m1.000s, 234m57.234s")
-    print("  %s -                    read time expression from standard input" % sys.argv[0])
+    print("  %s [[H:]M:]S[.sss]" % sys.argv[0])
+    print("      hours, minutes, seconds; M and S can be greater than 60")
+    print("      examples: 10 - 10s, 2:65 - 3m5s, 641 - 10m41s")
+    print("  %s [[<D>d]<H>h]<M>m<S.sss>s" % sys.argv[0])
+    print("      <M> minutes <S.sss> seconds, <M> can be greater than 60")
+    print("      examples: 0m1.000s, 234m57.234s")
+    print("  %s - read time expression from standard input" % sys.argv[0])
     exit()
 
 if sys.argv[1] == '-':
@@ -23,14 +26,33 @@ if sys.argv[1] == '-':
 else:
     data = sys.argv[1]
 
-if data.endswith('s'):
+# for real - you should use the 'dateparser' library
+# with one minor nuisance: 2d 3h 5m will be 2 days, 3 hours plus 5 months
+
+m = re.search(
+    r'(?:(?P<days>\d+)\s*d\s*)?(?:(?P<hours>\d+)\s*h\s*)?(?P<mins>\d+)\s*m\s*(?:(?P<secs>\d+)\s*s)?',
+    data
+)
+if m:
+    gd = m.groupdict()
+    hms = [0, 0, 0]
+    if gd['days']:
+        hms[0] += 24 * int(gd['days'])
+    if gd['hours']:
+        hms[0] += int(gd['hours'])
+    hms[1] = int(gd['mins'])
+    if gd['secs']:
+        hms[2] += int(gd['secs'])
+    # print(gd, hms)
+    hms = [str(x) for x in hms]
+elif data.endswith('s'):
     hms = data[:-1].split('m')
     if 'h' in hms[0]:
         hms = hms[0].split('h') + [hms[1]]
 else:
     hms = data.split(':')
 
-secs = float(hms[0])
+secs = float(hms[0].replace(',', '.'))
 for limb in hms[1:]:
     secs *= 60
     secs += float(limb.replace(',', '.'))
