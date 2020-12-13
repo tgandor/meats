571a8a929ec2ba6437f78e840f9f3c69437ba07f
tgandor <tomasz.gandor@gmail.com>
2020-12-13 22:18:21 +0100

    missing/gcd.py - fix division, add information about higher GCD in number pairs.

--- missing/gcd.py
+++ missing/gcd.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
 import sys
-from fractions import gcd
+from itertools import combinations
+try:
+    from math import gcd
+except ImportError:
+    from fractions import gcd
 
 nums = list(map(int, sys.argv[1:]))
 
@@ -14,5 +18,11 @@ for n in nums[1:]:
     result = gcd(result, n)
 
 print('GCD = {}'.format(result))
-print(', '.join('{} = {} * {}'.format(n, n/result, result) for n in nums))
+print(', '.join('{} = {} * {}'.format(n, n//result, result) for n in nums))
 
+for a, b in combinations(nums, 2):
+    sub_res = gcd(a, b)
+    if sub_res > result:
+        print('... but, gcd({}, {}) = {} = gcd({}*{}, {}*{})'.format(
+            a, b, sub_res, a//sub_res, sub_res, b//sub_res, sub_res
+        ))
