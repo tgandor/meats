a6e55545b77fbf7c8ab45af0db849c1525eea71c
tgandor <tomasz.gandor@gmail.com>
2021-01-04 11:55:06 +0100

    www-php/music/index.php - removed unneeded JS, added local CSS option, refactored path.

--- www-php/music/index.php
+++ www-php/music/index.php
@@ -3,6 +3,13 @@
     // mp3 and m4a, case insensitive (false positives: mpa, m43)
     $files = glob('*.[Mm][Pp4][Aa3]');
     natsort($files);
+    $crumbs = array();
+    $path = '';
+    foreach(explode('/', dirname($_SERVER['SCRIPT_NAME'])) as $dir)
+    {
+        $path .= "$dir/";
+        $crumbs[$path] = $dir;
+    }
 ?><!doctype html>
 <html lang="en">
   <head>
@@ -12,7 +19,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+<?php if(file_exists('bootstrap.min.css')): ?>
+    <link rel="stylesheet" href="bootstrap.min.css">
+<?php else: ?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+<?php endif ?>
 
     <style>
     .container {
@@ -38,10 +49,8 @@
             <div class="col-12">
                 <p>
                     Path:
-<?php $path = ''; ?>
-<?php foreach(explode('/', dirname($_SERVER['SCRIPT_NAME'])) as $i => $dir): ?>
-<?php $path .= "$dir/" ?>
-                    / <a href="<?php echo $path ?>"><?php echo $dir ? $dir : '&#8962;'  ?></a>
+<?php foreach($crumbs as $path => $dir): ?>
+                    / <a href="<?php echo $path ?>"><?php echo $dir ?: '&#8962;'  ?></a>
 <?php endforeach ?>
                 </p>
             </div>
@@ -79,10 +88,8 @@
             <div class="col-12">
                 <p>
                     Path:
-<?php $path = ''; ?>
-<?php foreach(explode('/', dirname($_SERVER['SCRIPT_NAME'])) as $i => $dir): ?>
-<?php $path .= "$dir/" ?>
-                    / <a href="<?php echo $path ?>"><?php echo $dir ? $dir : '&#8962;'  ?></a>
+<?php foreach($crumbs as $path => $dir): ?>
+                    / <a href="<?php echo $path ?>"><?php echo $dir ?: '&#8962;'  ?></a>
 <?php endforeach ?>
                 </p>
             </div>
@@ -90,11 +97,6 @@
         </div>
     </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     <script>
     /* https://stackoverflow.com/questions/2551859/html-5-video-or-audio-playlist */
     const n = <?php echo count($files) ?>;
