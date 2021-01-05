b9e5b4d13f853ab302eaf292017cfafeee8b6ea1
tgandor <tomasz.gandor@gmail.com>
2021-01-05 16:54:56 +0100

    dates/jfif_markers.py - added selection, modernized.

--- dates/jfif_markers.py
+++ dates/jfif_markers.py
@@ -8,13 +8,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument('file')
 parser.add_argument('--verbose', '-v', action='store_true')
 parser.add_argument('--skip-scan', '-s', action='store_true')
+parser.add_argument('--select', '-m', help='markers to show')
+parser.add_argument('--name', '-n', action='store_true',
+                    help='prefix with filename')
 args = parser.parse_args()
 
 names = {
     0xc4: 'DHT',
     0xd8: 'SOI',
+    0xd9: 'EOI',
     0xda: 'SOS',
     0xdb: 'DQT',
+    0xfe: 'COM',
 }
 markers = {}  # reverse names
 for d, m in names.items():
@@ -27,16 +32,18 @@ for i, d in enumerate(range(0xe0, 0xef+1)):
     markers[m] = d
 
 standalone = {
-    0xd8, # SOI
-    0x01, # TEM
+    0,  # not a marker
+    0x01,  # TEM
+    0xd8,  # SOI
+    0xd9,  # EOI
     # *range(0xd0, 0xd7+1), # RST
 }
 
 # SOF_x - Start of Frame x
 not_sof = {
-    0xc4, # DHT Define Huffman Table
-    0xc8, # JPG JPEG Extensions
-    0xcc, # DAC Define Arithmetic Coding
+    0xc4,  # DHT Define Huffman Table
+    0xc8,  # JPG JPEG Extensions
+    0xcc,  # DAC Define Arithmetic Coding
 }
 for i, d in enumerate(range(0xc0, 0xcf+1)):
     if d in not_sof:
@@ -52,43 +59,46 @@ for i, d in enumerate(range(0xd0, 0xd7+1)):
     names[d] = m
     markers[m] = d
 
+to_show = None
+if args.select:
+    if args.select not in markers:
+        print('Unsupported JFIF marker:', args.select)
+        exit()
+    to_show = markers[args.select]
+
+
 with open(args.file, 'rb') as f:
     while True:
-        c = ord(f.read(1))
+        ch = f.read(1)
+        if not ch:
+            break
+        c = ord(ch)
         if c == 0xff:
             d = ord(f.read(1))
-            if d == 0xd8:
-                print('SOI', '@', f.tell())
-                continue
-            if d == 1:
-                print('TEM', '@', f.tell())
-                continue
-            if 0xd0 <= d <= 0xd7:
-                print('RST', d-0xd0, '@', f.tell())
-                continue
-            if d == 0xd9:
-                print('EOI', '@', f.tell())
-                break
-            if d == 0:
-                # print('FF00 - not a marker', f.tell())
+
+            if d in standalone:
+                if d == to_show:
+                    print(
+                        "%6d" % f.tell(),
+                        hex(256*c+d)[2:],
+                        '%5s' % names.get(d, ' ? '),
+                        '(standalone)',
+                    )
                 continue
 
             h = ord(f.read(1))
             l = ord(f.read(1))
             size = 256 * h + l
             data = f.read(size - 2)  # 2 bytes already there
-
-            if d == 0xc0:  # SOF 0
-                print('SOF', 'size:', size, '@', f.tell(), 'data:', data)
-                continue
-
-            print(
-                "%6d" % f.tell(),
-                hex(256*c+d)[2:],
-                '%5s' % names.get(d, ' ? '),
-                'size: {:,}'.format(size),
-                repr(data) if args.verbose else ''
-            )
+            if (to_show is None or d == to_show) and d:
+                print(
+                    args.file if args.name else '',
+                    "%6d" % f.tell(),
+                    hex(256*c+d)[2:],
+                    '%5s' % names.get(d, ' ? '),
+                    'size: {:,}'.format(size),
+                    repr(data) if args.verbose else ''
+                )
 
             if d == markers['SOS'] and args.skip_scan:
                 break
