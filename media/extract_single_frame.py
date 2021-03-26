43dd77b88503c0eefc6465809034c71d911a7320
tgandor <tomasz.gandor@gmail.com>
2021-03-26 23:59:30 +0100

    media/extract_single_frame.py - wrapper for ffmpeg, 1 frame to JPG.

---
+++ media/extract_single_frame.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('video_file')
+parser.add_argument('--pos', type=float, default=3.5, help='time offset of frame')
+args = parser.parse_args()
+
+output = os.path.splitext(args.video_file)[0] + '.jpg'
+os.system(f'ffmpeg -ss {args.pos} -i "{args.video_file}" -vframes 1 -f image2 "{output}"')
