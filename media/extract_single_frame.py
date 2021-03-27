89d746966d780e5537b5b8d60a1135b9a7fdeb07
tgandor <tomasz.gandor@gmail.com>
2021-03-27 08:51:49 +0100

    media/extract_single_frame.py - hide banner.

--- media/extract_single_frame.py
+++ media/extract_single_frame.py
@@ -9,4 +9,7 @@ parser.add_argument('--pos', type=float, default=3.5, help='time offset of frame
 args = parser.parse_args()
 
 output = os.path.splitext(args.video_file)[0] + '.jpg'
-os.system(f'ffmpeg -ss {args.pos} -i "{args.video_file}" -vframes 1 -f image2 "{output}"')
+os.system(
+    f'ffmpeg -hide_banner -ss {args.pos} -i "{args.video_file}" '
+    f'-vframes 1 -f image2 "{output}"'
+)
