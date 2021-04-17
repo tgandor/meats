3e4f570a834a6ee01853e4b899f613cfc95bf593
tgandor <tomasz.gandor@gmail.com>
2021-04-17 16:44:34 +0200

    DTP/latex_highligh_best_2nd.py - better handling of numbers with spaces or commas.

--- DTP/latex_highligh_best_2nd.py
+++ DTP/latex_highligh_best_2nd.py
@@ -7,9 +7,16 @@ def transpose(nested):
     return [list(x) for x in zip(*nested)]
 
 
+def _parse(item):
+    if "," in item:
+        thousand_comma = item.count(",") > 1 or "." in item
+        item = item.replace(",", "" if thousand_comma else ".")
+    return float(item.replace(" ", ""))
+
+
 def is_float(item):
     try:
-        float(item)
+        _parse(item)
     except ValueError:
         return False
     return True
@@ -21,12 +28,12 @@ def highlight(col, reverse=False):
 
     def markup(item, rank):
         if rank == 1:
-            return '\\textbf{' + item + '}'
+            return "\\textbf{" + item + "}"
         if rank == 2:
-            return '\\underline{' + item + '}'
+            return "\\underline{" + item + "}"
         return item
 
-    sorted_vals = sorted(set(col), key=float, reverse=not reverse)
+    sorted_vals = sorted(set(col), key=_parse, reverse=not reverse)
     ranks = {val: rank + 1 for rank, val in enumerate(sorted_vals)}
 
     return [markup(item, ranks[item]) for item in col]
@@ -44,4 +51,3 @@ new_rows = transpose(highlight(col) for col in cols)
 
 for row in new_rows:
     print(" & ".join(row) + r" \\")
-
