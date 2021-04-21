03ae7fbbf776738534939145d731cb26bd954e66
tgandor <tomasz.gandor@gmail.com>
2021-04-21 16:49:11 +0200

    log_analysis/line_length_stats.py - --max option.

--- log_analysis/line_length_stats.py
+++ log_analysis/line_length_stats.py
@@ -8,27 +8,49 @@ from pprint import pprint
 
 def plot_counter_bar(counter):
     import matplotlib.pyplot as plt
+
     items, counts = zip(*sorted(counter.items()))
     x = range(len(counter))
-    plt.bar(x, counts, align='center')
+    plt.bar(x, counts, align="center")
     plt.xticks(x, items)
     plt.show()
 
 
+def gen_lines(root=".", verbose=False):
+    for (
+        path,
+        _,
+        files,
+    ) in os.walk(root):
+        for file in files:
+            if verbose:
+                print(path, file)
+            try:
+                with open(os.path.join(path, file)) as f:
+                    for line in f:
+                        yield line, file
+            except:
+                print("Error with {}/{}".format(path, file))
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--text', '-t', action='store_true')
+parser.add_argument("--max", "-m", action="store_true", help="find max")
+parser.add_argument("--verbose", "-v", action="store_true")
+parser.add_argument("root", type=str, nargs="?", default=".")
+
 args = parser.parse_args()
 
 stats = Counter()
 
-for _, __, files, in os.walk('.'):
-    stats.update([len(line) for file in files for line in open(file)])
-
+stats.update(len(line) for line, _ in gen_lines(args.root))
 
 if len(stats) == 0:
-    print('No files.')
+    print("No files.")
     exit()
 
 pprint(sorted(stats.items()))
+
+if args.max:
+    print(max((len(line), line, file) for line, file in gen_lines(args.root)))
 
 plot_counter_bar(stats)
