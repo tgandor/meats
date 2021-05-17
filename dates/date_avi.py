404f1330bb263e2c016412fc3d07d95c69ffc32a
tgandor <tomasz.gandor@gmail.com>
2021-05-17 17:38:25 +0200

    dates/date_avi.py - not found message.

--- dates/date_avi.py
+++ dates/date_avi.py
@@ -10,12 +10,19 @@ import time
 
 
 def info(f):
-    some_data = open(f, 'rb').read(2 ** 11)
-    date_string = re.search(b'([A-Z][a-z]{2} ){2}\d\d?([ :]\d\d){4}\d\d', some_data).group().decode()
+    some_data = open(f, 'rb').read(2 ** 12)
+    date_string = re.search(b'([A-Z][a-z]{2} ){2}\d\d?([ :]\d\d){4}\d\d', some_data)
+
+    if date_string is None:
+        print("{} - not found".format(f))
+        return
+
+    date_string = date_string.group().decode()
     # print(date_string)
     parsed_date = time.strptime(date_string, "%a %b %d %H:%M:%S %Y")
     print('{} ; {}'.format(f, time.strftime("%Y-%m-%d (%a) %H:%M:%S", parsed_date)))
 
 
 if __name__ == '__main__':
-    list(map(info, sys.argv[1:]))
+    for arg in sys.argv[1:]:
+        info(arg)
