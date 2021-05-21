396315c63afa4b867c70e1b10192a3f643645b62
tgandor <tomasz.gandor@gmail.com>
2021-05-21 22:50:40 +0200

    network/outlook_save_folder.py - handle sender; print subjects in verbose.

--- network/outlook_save_folder.py
+++ network/outlook_save_folder.py
@@ -5,10 +5,11 @@ import json
 import win32com.client
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--folder', '-f')
+parser.add_argument("--folder", "-f")
+parser.add_argument("--verbose", "-v", action="store_true")
 args = parser.parse_args()
 
-outlook = win32com.client.Dispatch('outlook.application')
+outlook = win32com.client.Dispatch("outlook.application")
 mapi = outlook.GetNamespace("MAPI")
 
 inbox = mapi.GetDefaultFolder(6)
@@ -19,12 +20,16 @@ if args.folder:
 data = []
 
 for message in inbox.items:
-    data.append({
-        'subject': message.subject,
-        'sender': getattr(message, 'sender', 'unknown sender'),
-        'date': message.senton.strftime('%Y-%m-%d %H:%M:%S'),
-        'body': message.body,
-    })
+    data.append(
+        {
+            "subject": message.subject,
+            "sender": str(getattr(message, "sender", "unknown sender")),
+            "date": message.senton.strftime("%Y-%m-%d %H:%M:%S"),
+            "body": message.body,
+        }
+    )
+    if args.verbose:
+        print(f"{len(data)}\t{message.senton}\t{message.subject}")
 
 json_file = f"{args.folder or 'Inbox'}.json"
 
