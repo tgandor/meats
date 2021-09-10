644c3a4b488c686e346abc5ba0062dc6526c7179
tgandor <tomasz.gandor@gmail.com>
2021-09-11 01:53:16 +0200

    system/df.py - deltas for windows --loop.

--- system/df.py
+++ system/df.py
@@ -23,25 +23,27 @@ def open_database():
     cursor = conn.cursor()
     # initialization
     if initialize:
-        cursor.executescript("""
-create table df
-(
-    id integer not null primary key autoincrement,
-    filesystem varchar not null,
-    size integer not null,
-    used integer not null,
-    available integer not null,
-    use varchar not null,
-    mountpoint varchar not null,
-    df_date datetime not null
-);
-""")
+        cursor.executescript(
+            """
+            create table df
+            (
+                id integer not null primary key autoincrement,
+                filesystem varchar not null,
+                size integer not null,
+                used integer not null,
+                available integer not null,
+                use varchar not null,
+                mountpoint varchar not null,
+                df_date datetime not null
+            );
+            """
+        )
     conn.text_factory = str  # which == unicode on Py3, and works!
     return conn, cursor
 
 
 def normal_df():
-    data = os.popen('df -m').read().split('\n')[1:]
+    data = os.popen("df -m").read().split("\n")[1:]
     df_date = datetime.datetime.now()
 
     conn, cursor = open_database()
@@ -51,35 +53,67 @@ def normal_df():
         if len(columns) != 6:
             continue
         cursor.execute(
-            'insert into df (filesystem, size, used, available, use, mountpoint, df_date) values (?,?,?,?,?,?,?)',
-            columns + [df_date]
+            "insert into df (filesystem, size, used, available, use, mountpoint, df_date)"
+            " values (?,?,?,?,?,?,?)",
+            columns + [df_date],
         )
     conn.commit()
     cursor.close()
     conn.close()
 
 
+_DELTAS = {}
+
+
 def windows_emulation():
     df_date = datetime.datetime.now()
 
     conn, cursor = open_database()
-    print(time.strftime('%D %T'))
+    print(time.strftime("%D %T"))
     for drive in enumerate_windows_drives():
-        print('Checking:', drive)
+        print(drive, end=" ")
 
         try:
             usage = shutil.disk_usage(drive)
         except FileNotFoundError:
-            print('Failed to get usage for', drive, '- a disconnected network drive? (FileNotFoundError)')
+            print(
+                "Failed to get usage for",
+                drive,
+                "- a disconnected network drive? (FileNotFoundError)",
+            )
             continue
         except PermissionError:
-            print('Failed to get usage for', drive, '- not ready USB storage? (PermissionError)')
+            print(
+                "Failed to get usage for",
+                drive,
+                "- not ready USB storage? (PermissionError)",
+            )
             continue
 
-        print('{:.1f}% used, {}, {:,} Bytes free'.format(usage.used * 100 / usage.total, usage, usage.free))
+        print(
+            "{:, } B = {:.1f}% used, {:,} B free".format(
+                usage.used, usage.used * 100 / usage.total, usage.free
+            )
+        )
+
+        delta = _DELTAS.get(drive)
+        if delta:
+            print("Delta: {:,} B".format(usage.used - delta))
+        else:
+            _DELTAS[drive] = usage.used
+
         cursor.execute(
-            'insert into df (filesystem, size, used, available, use, mountpoint, df_date) values (?,?,?,?,?,?,?)',
-            (drive, usage.total, usage.used, usage.free, usage.used / usage.total * 100, drive, df_date)
+            "insert into df (filesystem, size, used, available, use, mountpoint, df_date)"
+            " values (?,?,?,?,?,?,?)",
+            (
+                drive,
+                usage.total,
+                usage.used,
+                usage.free,
+                usage.used / usage.total * 100,
+                drive,
+                df_date,
+            ),
         )
     conn.commit()
     cursor.close()
@@ -89,24 +123,26 @@ def windows_emulation():
 def enumerate_windows_drives():
     try:
         import win32api
+
         # print(repr(win32api.GetLogicalDriveStrings()))
-        for drive in win32api.GetLogicalDriveStrings().split('\0'):
+        for drive in win32api.GetLogicalDriveStrings().split("\0"):
             if drive:
                 yield drive
     except ImportError:
-        print('Missing win32api', file=sys.stderr)
+        print("Missing win32api", file=sys.stderr)
         import string
+
         for letter in string.ascii_uppercase:
-            path = letter + ':\\'
+            path = letter + ":\\"
             if os.path.exists(path):
                 yield path
 
 
-if __name__ == '__main__':
-    if os.name == 'nt':
+if __name__ == "__main__":
+    if os.name == "nt":
         parser = argparse.ArgumentParser()
-        parser.add_argument('--loop', '-l', action='store_true')
-        parser.add_argument('--period', '-t', type=float, default=60)
+        parser.add_argument("--loop", "-l", action="store_true")
+        parser.add_argument("--period", "-t", type=float, default=60)
         args = parser.parse_args()
         if args.loop:
             while True:
