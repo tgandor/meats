206ac31fafbab53f5fa89fc617d68e1d325a6bfd
tgandor <tomasz.gandor@gmail.com>
2021-09-11 02:04:59 +0200

    system/plot_usage.py - C: default for windows, --used,-u shorthand.

--- system/plot_usage.py
+++ system/plot_usage.py
@@ -17,80 +17,85 @@ def fake_date(dt):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--ascii', '-t', action='store_true')
-parser.add_argument('--column', '-c', default="available")
-parser.add_argument('--fake-date', '-d', action='store_true')
-parser.add_argument('--mountpoint', '-m',
-                    default=('/',), help='mountpoint to show', nargs='+')
-parser.add_argument('--absolute', '-a', action='store_true',
-                    help='Y axis from 0 to size')
-parser.add_argument('--style', default='-', help='matplotlib plotting style')
-parser.add_argument('--list-mountpoints', '-l', action='store_true')
-parser.add_argument('--days', '-D', type=int,
-                    help='number of last days to show')
-parser.add_argument('--width', '-w', type=int,
-                    help='width of plot (--ascii only)', default=170)
-parser.add_argument('--height', '-H', type=int,
-                    help='height of plot (--ascii only)', default=60)
-parser.add_argument('--debug', '-v', action='store_true')
+parser.add_argument("--ascii", "-t", action="store_true")
+parser.add_argument("--column", "-c", default="available")
+parser.add_argument("--used", "-u", action="store_true", help="show 'used' column")
+parser.add_argument("--fake-date", "-d", action="store_true")
+parser.add_argument(
+    "--mountpoint",
+    "-m",
+    default=("C:\\" if os.name == "nt" else "/",),
+    help="mountpoint to show",
+    nargs="+",
+)
+parser.add_argument(
+    "--absolute", "-a", action="store_true", help="Y axis from 0 to size"
+)
+parser.add_argument("--style", default="-", help="matplotlib plotting style")
+parser.add_argument("--list-mountpoints", "-l", action="store_true")
+parser.add_argument("--days", "-D", type=int, help="number of last days to show")
+parser.add_argument(
+    "--width", "-w", type=int, help="width of plot (--ascii only)", default=170
+)
+parser.add_argument(
+    "--height", "-H", type=int, help="height of plot (--ascii only)", default=60
+)
+parser.add_argument("--debug", "-v", action="store_true")
 args = parser.parse_args()
+if args.used:
+    args.column = "used"
 
-conn = sqlite3.connect(os.path.expanduser('~/usage.db'))
+conn = sqlite3.connect(os.path.expanduser("~/usage.db"))
 
 if args.list_mountpoints:
     df = pd.read_sql_query(
-        'select distinct mountpoint from df order by mountpoint', conn)
-    for row in df['mountpoint']:
+        "select distinct mountpoint from df order by mountpoint", conn
+    )
+    for row in df["mountpoint"]:
         print(row)
     conn.close()
     exit()
 
 params = ()
-query = 'select * from df where mountpoint in ({})'.format(
-    ', '.join("'" + mp + "'" for mp in args.mountpoint)
+query = "select * from df where mountpoint in ({})".format(
+    ", ".join("'" + mp + "'" for mp in args.mountpoint)
 )
 
 if args.days:
-    query += ' and df_date > ?'
+    query += " and df_date > ?"
     params += (datetime.datetime.now() - datetime.timedelta(days=args.days),)
 
 if args.debug:
     print(query, params)
 
 df = pd.read_sql_query(query, conn, params=params)
-df.df_date = pd.to_datetime(df['df_date'])
+df.df_date = pd.to_datetime(df["df_date"])
 # import code; code.interact(local=locals())
 
 if args.ascii:
     import asciiplotlib
 
     if args.fake_date:
-        df.df_date = df[['df_date']].apply(fake_date, axis=1)
-        xlabel = 'day.fraction'
+        df.df_date = df[["df_date"]].apply(fake_date, axis=1)
+        xlabel = "day.fraction"
     else:
-        df.df_date = df[['df_date']].apply(
-            (lambda x: int(x[0].timestamp())), axis=1)
-        xlabel = 'timestamp'
+        df.df_date = df[["df_date"]].apply((lambda x: int(x[0].timestamp())), axis=1)
+        xlabel = "timestamp"
 
-    kwargs = dict(
-        label='used GB',
-        xlabel=xlabel,
-        width=args.width,
-        height=args.height
-    )
+    kwargs = dict(label="used GB", xlabel=xlabel, width=args.width, height=args.height)
 
     if args.absolute:
-        kwargs['ylim'] = (0, df['size'].max() / 1024)
+        kwargs["ylim"] = (0, df["size"].max() / 1024)
 
     fig = asciiplotlib.figure()
-    fig.plot(x=df['df_date'], y=df['used'] / 1024, **kwargs)
+    fig.plot(x=df["df_date"], y=df["used"] / 1024, **kwargs)
     fig.show()
 else:
     from matplotlib import pyplot as plt
 
-    df.set_index('df_date')[args.column].plot(style=args.style)
+    df.set_index("df_date")[args.column].plot(style=args.style)
     plt.title(f"{args.column}  on {', '.join(args.mountpoint)}")
     if args.absolute:
         # 'size' is a bad column name in Pandas. df.size returns int, df['size'] - Series
-        plt.ylim(bottom=0, top=df['size'].max())
+        plt.ylim(bottom=0, top=df["size"].max())
     plt.show()
