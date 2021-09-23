be9080c250b4f72c63f6ab68c1bea3ad95141b3c
tgandor <tomasz.gandor@gmail.com>
2021-09-23 19:19:18 +0200

    get_thumbnails.sh - simplify filenames.

--- hamster/get_thumbnails.sh
+++ hamster/get_thumbnails.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # usually gets WebP, but there is another way of getting JPGs
-xargs -n1 youtube-dl --skip-download --write-thumbnail
+xargs -n1 youtube-dl --skip-download --write-thumbnail --output "%(id)s"
 
