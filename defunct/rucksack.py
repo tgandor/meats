f1e8d8a065f10273ec1b59efa36164520819dd7e
tgandor <tomasz.gandor@gmail.com>
2021-10-10 21:58:39 +0200

    defunct/rucksack.py - 2to3. Works, but need to | sort -n | tail.

--- defunct/rucksack.py
+++ defunct/rucksack.py
@@ -2,7 +2,7 @@
 import sys
 
 if len(sys.argv) < 3:
-    print sys.argv, 'Usage: %s limit item1 ... itemN' % sys.argv[0]
+    print(sys.argv, 'Usage: %s limit item1 ... itemN' % sys.argv[0])
     exit()
 
 def search(money_left, item_prices, bag=[]):
@@ -11,7 +11,7 @@ def search(money_left, item_prices, bag=[]):
         return
     current_price = item_prices.pop()
     max_current_count = int(money_left / current_price)
-    for i in xrange(max_current_count, 0, -1):
+    for i in range(max_current_count, 0, -1):
         for result in search(money_left-i*current_price, item_prices[:],
                 bag+[(current_price, i)]):
             yield result
@@ -23,10 +23,10 @@ items = sorted(set(map(float, sys.argv[2:])), reverse=True)
 
 if items[-1] < 0:
     # min price specified
-    items = filter(lambda x: x >= -items[-1], items)
+    items = [x for x in items if x >= -items[-1]]
     sys.stderr.write('Selected items: %s\n' % str(items))
 
 for result in search(limit, items):
-    print sum(price * count for price, count in result), len(result), sum(count for _, count
-            in result), result
+    print(sum(price * count for price, count in result), len(result), sum(count for _, count
+            in result), result)
 
