ef46914fb7ee8d595a632209b16403a77e0f626c
tgandor <tomasz.gandor@gmail.com>
2021-11-03 20:51:56 +0100

    media/video_duration_ffprobe.py -- fix globbing problem, reformat.

--- media/video_duration_ffprobe.py
+++ media/video_duration_ffprobe.py
@@ -17,31 +17,34 @@ except ImportError:
 # https://stackoverflow.com/a/3844467/1338797
 
 parser = argparse.ArgumentParser()
-parser.add_argument('files', nargs='+')
-parser.add_argument('--verbose', '-v', action='store_true')
-parser.add_argument('--separator', '-s', help='Output separator for CSV', default=';')
+parser.add_argument("files", nargs="+")
+parser.add_argument("--verbose", "-v", action="store_true")
+parser.add_argument("--separator", "-s", help="Output separator for CSV", default=";")
 
 
 def parse_duration(s):
     # credits: https://stackoverflow.com/a/42543326/1338797
     from datetime import datetime as dtt
-    FORMAT = '%H:%M:%S'
-    return dtt.strptime(s, FORMAT) - dtt.strptime('00:00:00', FORMAT)
+
+    FORMAT = "%H:%M:%S"
+    return dtt.strptime(s, FORMAT) - dtt.strptime("00:00:00", FORMAT)
 
 
 def get_duration(filename, verbose=False):
     result = subprocess.Popen(
-        ['ffprobe', '-hide_banner', filename],
+        ["ffprobe", "-hide_banner", filename],
         stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT
+        stderr=subprocess.STDOUT,
     )
 
-    durations = [x.decode() for x in result.stdout.readlines() if b'duration' in x.lower()]
+    durations = [
+        x.decode() for x in result.stdout.readlines() if b"duration" in x.lower()
+    ]
     if verbose:
-        print(filename, 'durations:\n', durations)
+        print(filename, "durations:\n", durations)
 
     for duration in durations:
-        time_match = re.search(r'Duration:\s*([\d:]+)', duration, re.IGNORECASE)
+        time_match = re.search(r"Duration:\s*([\d:]+)", duration, re.IGNORECASE)
 
         if not time_match:
             # sometimes there is metadata about segmented streaming...
@@ -49,27 +52,35 @@ def get_duration(filename, verbose=False):
 
         time_string = time_match.group(1)
         if verbose:
-            print('extracted time:', time_string)
+            print("extracted time:", time_string)
         return time_string
 
     return None
 
 
-if __name__ == '__main__':
+def _iter_files(files_or_globs):
+    for pattern in files_or_globs:
+        if "*" in pattern:
+            for path in glob.glob(pattern):
+                yield pattern
+        else:
+            yield pattern
+
+
+if __name__ == "__main__":
     args = parser.parse_args()
     deltas = []
 
-    for pattern in args.files:
-        for name in natsorted(glob.glob(pattern)):
-            duration = get_duration(name, args.verbose)
+    for name in _iter_files(args.files):
+        duration = get_duration(name, args.verbose)
 
-            if duration is None:
-                print('{}{}{}'.format(name, args.separator, 'N/A'))
-                continue
+        if duration is None:
+            print("{}{}{}".format(name, args.separator, "N/A"))
+            continue
 
-            deltas.append(parse_duration(duration))
-            print('{}{}{}'.format(name, args.separator, duration))
+        deltas.append(parse_duration(duration))
+        print("{}{}{}".format(name, args.separator, duration))
 
     if len(deltas):
         total = sum(deltas, datetime.timedelta())
-        print('Total{}{}'.format(args.separator, total))
+        print("Total{}{}".format(args.separator, total))
