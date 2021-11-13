6b346897966b1c0ec6cce0782c9bc5d6154f6f27
tgandor <tomasz.gandor@gmail.com>
2021-11-13 07:32:17 +0100

    media/remove_subs.sh -- remove intermediate files.

--- media/remove_subs.sh
+++ media/remove_subs.sh
@@ -6,4 +6,6 @@ bname=`basename $1 .mp4`
 ~/meats/media/video_to_images.py $1
 ~/meats/media/detect_subtitles.py -p "$bname/*.png"
 ~/meats/media/images_to_mp4.py -nv -q 29 -r 23.976024 -o "img_$bname.mp4" "$bname/*.png"
+rm -r $bname
 ~/meats/media/replace_audio_in_video.sh "img_$bname.mp4" $1
+rm "img_$bname.mp4"
