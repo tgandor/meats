bdd50a297143e7f57cafaba75d80558c69eab8ea
tgandor <tomasz.gandor@gmail.com>
2021-11-23 02:08:38 +0100

    media/video_to_images.py -- add --nvdec,-nv; changes ~nothing.

--- media/video_to_images.py
+++ media/video_to_images.py
@@ -6,6 +6,7 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument("video")
 parser.add_argument("--dedup", "-d", action="store_true", help="drop duplicate frames")
+parser.add_argument("--nvdec", "-nv", action="store_true")
 parser.add_argument("--output", "-o", help="output directory for images")
 parser.add_argument("--frames", "-n", type=int, help="limit frames number")
 parser.add_argument("--format", "-f", default="png")
@@ -14,7 +15,12 @@ args = parser.parse_args()
 out_dir = args.output if args.output else os.path.splitext(args.video)[0]
 os.makedirs(out_dir, exist_ok=True)
 
-command = f'ffmpeg -hide_banner -i "{args.video}"'
+command = "ffmpeg -hide_banner"
+
+if args.nvdec:
+    command += " -hwaccel nvdec"
+
+command += f' -i "{args.video}"'
 
 if args.dedup:
     command += " -vf mpdecimate"
