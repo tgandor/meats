5a63d5c14d07d212cc430e89ace6a716c2096f05
tgandor <tomasz.gandor@gmail.com>
2021-12-26 18:19:00 +0100

    missing/copy_date.sh -- also echo date after copying.

--- missing/copy_date.sh
+++ missing/copy_date.sh
@@ -3,10 +3,13 @@
 if [ "$1" == "-f" ] ; then
     # filename-appropriate
     date +%F_%a_%H.%M | xargs ~/meats/missing/clipboard_copy.py
+    date +%F_%a_%H.%M
 elif [ "$1" == "-r" ] ; then
     # 'raw' date (no time)
     date +%F | xargs ~/meats/missing/clipboard_copy.py
+    date +%F
 else
     date +%F_%a_%R | xargs ~/meats/missing/clipboard_copy.py
+    date +%F_%a_%R
 fi
 
