69f430f4e96debf61a3c71b03c36a93155f14096
tgandor <tomasz.gandor@gmail.com>
2022-02-22 18:33:39 +0100

    log_analysis/xml_list.py -- add -d for depth and -a for viewing attributes.

--- log_analysis/xml_list.py
+++ log_analysis/xml_list.py
@@ -12,17 +12,35 @@ import argparse
 from xml.etree import ElementTree as ET
 
 
-def list(element, level=0):
-    print(". " * level, "<" + element.tag + ">")
-    # import code; code.interact(local=locals())
+def render(element, options):
+    result = "<" + element.tag
+    if options.get("attribute", False):
+        for name in options['attribute']:
+            if name in element.attrib:
+                result += f" {name}={element.attrib[name]}"
+    result += ">"
+    return result
+
+
+def list(element, level=0, options=None):
+    if options is None:
+        options = {}
+
+    print(". " * level, render(element, options), sep="")
+
+    if level == options.get("max_level", -1):
+        return
+
     for child in element:
-        list(child, level + 1)
+        list(child, level + 1, options)
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("xml_file")
+parser.add_argument("--attribute", "-a", nargs="*")
+parser.add_argument("--max-level", "-d", type=int, default=-1)
 args = parser.parse_args()
 
 tree = ET.parse(args.xml_file)
 root = tree.getroot()
-list(root)
+list(root, options=vars(args))
