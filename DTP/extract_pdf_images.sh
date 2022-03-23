9e73ae2c623829db68513449b863ccd2379238b9
tgandor <tomasz.gandor@gmail.com>
2022-03-23 09:56:40 +0100

    DTP/extract_pdf_images.sh -- add first argument -png to convert to PNG instead.

--- DTP/extract_pdf_images.sh
+++ DTP/extract_pdf_images.sh
@@ -2,9 +2,16 @@
 
 if ! which pdfimages >/dev/null; then
     sudo apt install poppler-utils
+    sudo pacman -S poppler
+fi
+
+format="-j"
+if [ "$1" == "-png" ] ; then
+    format="-png"
+    shift
 fi
 
 for i in "$@" ; do
     echo $i
-    pdfimages -j "$i" "`basename "$i" .pdf`"
+    pdfimages $format "$i" "`basename "$i" .pdf`"
 done
