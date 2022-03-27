b49b1b9e632e827335fd3e24696d75c5f5a3c7ec
tgandor <tomasz.gandor@gmail.com>
2022-03-27 08:48:43 +0200

    installer/jupyterlab_matplotlib.sh.bat -- replacing %matplotlib notebook.

---
+++ installer/jupyterlab_matplotlib.sh.bat
@@ -0,0 +1,4 @@
+echo "https://stackoverflow.com/questions/51922480/javascript-error-ipython-is-not-defined-in-jupyterlab/"
+pip install ipympl
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter labextension install jupyter-matplotlib
