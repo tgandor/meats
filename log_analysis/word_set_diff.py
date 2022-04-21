8a30eb402a30524abae652922849dceae81cf2ba
tgandor <tomasz.gandor@gmail.com>
2022-04-22 01:17:38 +0200

    log_analysis/word_set_diff.py -- show result length.

--- log_analysis/word_set_diff.py
+++ log_analysis/word_set_diff.py
@@ -5,17 +5,20 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("file1")
 parser.add_argument("file2")
-parser.add_argument("--common", "-c", action="store_true", help="set intersection (default: difference)")
+parser.add_argument(
+    "--common", "-c", action="store_true", help="set intersection (default: difference)"
+)
 args = parser.parse_args()
 
 words1 = set(open(args.file1).read().split())
 words2 = set(open(args.file2).read().split())
 
 if args.common:
-    print("Common words:")
-    for word in sorted(words1 & words2):
-        print(word)
+    result = sorted(words1 & words2)
+    print("Common words ({}):".format(len(result)))
 else:
-    print("Words only in {}:".format(args.file1))
-    for word in sorted(words1 - words2):
-        print(word)
+    result = sorted(words1 - words2)
+    print("Words only in {} ({}):".format(args.file1, len(result)))
+
+for word in result:
+    print(word)
