dd6f2700b46d840c624075fda0b8803dad3f84df
tgandor <tomasz.gandor@gmail.com>
2022-04-24 20:13:22 +0200

    log_analysis/word_set_diff.py -- add --reverse,-r for inversion of difference.

--- log_analysis/word_set_diff.py
+++ log_analysis/word_set_diff.py
@@ -8,17 +8,25 @@ parser.add_argument("file2")
 parser.add_argument(
     "--common", "-c", action="store_true", help="set intersection (default: difference)"
 )
+parser.add_argument("--reverse", "-r", action="store_true")
 args = parser.parse_args()
 
 words1 = set(open(args.file1).read().split())
 words2 = set(open(args.file2).read().split())
+
+if args.reverse:
+    words1, words2 = words2, words1
 
 if args.common:
     result = sorted(words1 & words2)
     print("Common words ({}):".format(len(result)))
 else:
     result = sorted(words1 - words2)
-    print("Words only in {} ({}):".format(args.file1, len(result)))
+    print(
+        "Words only in {} ({}):".format(
+            args.file2 if args.reverse else args.file1, len(result)
+        )
+    )
 
 for word in result:
     print(word)
