af3ad73676cf09461f2de30940b841e090804731
tgandor <tomasz.gandor@gmail.com>
2022-04-28 11:53:15 +0200

    system/why_pacman.sh -- add coloring of the relevant row header.

--- system/why_pacman.sh
+++ system/why_pacman.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Highlight: https://stackoverflow.com/questions/981601/colorized-grep-viewing-the-entire-file-with-highlighted-matches
+
+LANG=C pacman -Qi "$@" | grep --color "Required By\|$"
 echo "Try also: pamac info <package>"
-pacman -Qi "$@"
