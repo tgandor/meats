e6d6b3158bb406a9450da2b102fab747dab4b238
tgandor <tomasz.gandor@gmail.com>
2022-05-10 16:41:57 +0200

    hamster/gramtification.py -- sort, skip old, --sleep between.

--- hamster/gramtification.py
+++ hamster/gramtification.py
@@ -2,7 +2,10 @@
 
 import argparse
 import codecs
+import glob
+import random
 import re
+import time
 import requests
 
 PATTERN = codecs.encode("uggcf://jjj.vafgntenz.pbz/c/([^/]+)/", "rot_13")
@@ -10,6 +13,7 @@ PATTERN = codecs.encode("uggcf://jjj.vafgntenz.pbz/c/([^/]+)/", "rot_13")
 parser = argparse.ArgumentParser()
 parser.add_argument("url")
 parser.add_argument("--list", "-l", action="store_true")
+parser.add_argument("--sleep", "-s", type=int, default=10)
 args = parser.parse_args()
 
 data = requests.get(args.url).text
@@ -17,10 +21,16 @@ data = requests.get(args.url).text
 if not args.list:
     import instalooter.cli
 
-    tokens = set(re.findall(PATTERN, data))
+    tokens = sorted(set(re.findall(PATTERN, data)))
     il_args = ["-T", "{username}-{datetime}-{code}", "post"]
     for token in tokens:
-        instalooter.cli.main(il_args + [token])
+        if glob.glob(f"*{token}*"):
+            print(f"Skipping {token}")
+            continue
+        res = instalooter.cli.main(il_args + [token])
+        print(f"for {token=}, {res=}")
+        nap = random.randint(args.sleep, args.sleep * 2)
+        print(f"Sleeping, {nap=}")
+        time.sleep(nap)
 else:
-
     print("\n".join(x for x, _ in sorted(set(re.findall(f"({PATTERN})", data)))))
