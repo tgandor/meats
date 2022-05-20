1790150b2ee7c81915de353eb901090a40671e2b
tgandor <tomasz.gandor@gmail.com>
2022-05-20 22:19:03 +0200

    media/remove_subs -- get actual framerate for re-encoding.

--- media/remove_subs_cpu.sh
+++ media/remove_subs_cpu.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -xe
-# warning: application specific... (framerate, quality...)
+# warning: application specific... (quality...)
 
 bname=`basename $1 .mp4`
+rate=`ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate $1`
 ~/meats/media/video_to_images.py $1
 ~/meats/media/detect_subtitles.py -p "$bname/*.png"
-~/meats/media/images_to_mp4.py -r 23.976024 -o "img_$bname.mp4" "$bname/*.png"
+~/meats/media/images_to_mp4.py -r $rate -o "img_$bname.mp4" "$bname/*.png"
 rm -r $bname
 ~/meats/media/replace_audio_in_video.sh "img_$bname.mp4" $1
 rm "img_$bname.mp4"
