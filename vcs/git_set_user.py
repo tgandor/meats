662fb2e9050df4e499273fcc1dbcece57a9c4d61
tgandor <tomasz.gandor@gmail.com>
2022-06-08 23:36:34 +0200

    vcs/git_set_user.py -- add -g flag for global. (Python keyword..)

--- vcs/git_set_user.py
+++ vcs/git_set_user.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
 
+import argparse
 import os
-import sys
 
 
 def main():
-    if len(sys.argv) > 1:
-        source_dir = sys.argv[1]
-        target_dir = os.getcwd()
-    else:
-        source_dir = target_dir = os.getcwd()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source_dir", nargs="?")
+    parser.add_argument("-g", action="store_true")
+    args = parser.parse_args()
+
+    target_dir = os.getcwd()
+    source_dir = args.source_dir or target_dir
 
     os.chdir(source_dir)
     last_commit = os.popen("git log -1").readlines()
     author_line = [line for line in last_commit if line.startswith('Aut')][0]
     author = author_line.split()
 
+    g = '--global' if args.g else ''
     os.chdir(target_dir)
-    cmd = 'git config user.name "{}"'.format(' '.join(author[1:-1]))
+    cmd = 'git config {} user.name "{}"'.format(g, ' '.join(author[1:-1]))
     print(cmd)
     os.system(cmd)
-    cmd = 'git config user.email "{}"'.format(author[-1][1:-1])
+    cmd = 'git config {} user.email "{}"'.format(g, author[-1][1:-1])
     print(cmd)
     os.system(cmd)
 
