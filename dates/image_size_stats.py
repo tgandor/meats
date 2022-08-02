cd43dc66ddfcd59c0a27c69310a19cfa086badda
tgandor <tomasz.gandor@gmail.com>
2022-08-02 19:38:06 +0200

    dates/image_size_stats.py -- print aspect; face model relative to source.

--- dates/image_size_stats.py
+++ dates/image_size_stats.py
@@ -11,23 +11,27 @@ import datetime
 try:
     import piexif
 except ImportError:
-    print('Missing piexif')
-    os.system('pip install piexif')
+    print("Missing piexif")
+    os.system("pip install piexif")
     exit()
 
 try:
     import imagesize
 except ImportError:
-    print('Missing imagesize')
-    os.system('pip install imagesize')
+    print("Missing imagesize")
+    os.system("pip install imagesize")
     exit()
 
 parser = argparse.ArgumentParser()
-parser.add_argument('directory', nargs='?', default='.')
-parser.add_argument('--no-plot', '-n', action='store_true')
-parser.add_argument('--quantize', '-q', type=int, default=1, help='Truncate dimensions to multiples')
-parser.add_argument('--top', '-t', type=int, default=10, help='Number of most popular sizes to list')
-parser.add_argument('--verbose', '-v', action='store_true')
+parser.add_argument("directory", nargs="?", default=".")
+parser.add_argument("--no-plot", "-n", action="store_true")
+parser.add_argument(
+    "--quantize", "-q", type=int, default=1, help="Truncate dimensions to multiples"
+)
+parser.add_argument(
+    "--top", "-t", type=int, default=10, help="Number of most popular sizes to list"
+)
+parser.add_argument("--verbose", "-v", action="store_true")
 args = parser.parse_args()
 
 size_stats = Counter()
@@ -39,23 +43,26 @@ start = datetime.datetime.now()
 
 for path, directories, files in os.walk(args.directory):
     for basename in files:
-        if basename.lower().endswith('.jpg'):
+        if basename.lower().endswith(".jpg"):
             realname = os.path.join(path, basename)
 
             try:
                 d = piexif.load(realname)
 
                 try:
-                    size = (d["0th"][piexif.ImageIFD.ImageWidth], d["0th"][piexif.ImageIFD.ImageLength])
+                    size = (
+                        d["0th"][piexif.ImageIFD.ImageWidth],
+                        d["0th"][piexif.ImageIFD.ImageLength],
+                    )
                     exif += 1
                 except KeyError:
                     # print('Error reading size from EXIF:', realname)
                     size = None
             except piexif.InvalidImageDataError:
-                print('Error reading EXIF (invalid file [type]?):', realname)
+                print("Error reading EXIF (invalid file [type]?):", realname)
                 size = None
             except Exception as e:
-                print('Other error reading EXIF:', e, 'in', realname)
+                print("Other error reading EXIF:", e, "in", realname)
                 size = None
 
             size2 = imagesize.get(realname)
@@ -66,10 +73,14 @@ for path, directories, files in os.walk(args.directory):
             if size != size2:
                 # imagesize seems to be right on the money
                 exif_mismatch += 1
-                print('Size differs for {}: {} vs {} ({})'.format(realname, size, size2, exif_mismatch))
+                print(
+                    "Size differs for {}: {} vs {} ({})".format(
+                        realname, size, size2, exif_mismatch
+                    )
+                )
                 size = size2
             elif args.verbose:
-                print(realname, 'OK')
+                print(realname, "OK")
 
             if args.quantize > 1:
                 size = tuple(x - x % args.quantize for x in size)
@@ -81,37 +92,62 @@ for path, directories, files in os.walk(args.directory):
 total = sum(size_stats.values())
 
 if total == 0:
-    print('No images found. Sorry. Wall time:', datetime.datetime.now() - start)
+    print("No images found. Sorry. Wall time:", datetime.datetime.now() - start)
     exit()
 
 print(
-    'Finished processing', total, 'files,',
-    exif, 'had EXIF metadata, in', datetime.datetime.now() - start
+    "Finished processing",
+    total,
+    "files,",
+    exif,
+    "had EXIF metadata, in",
+    datetime.datetime.now() - start,
 )
 
 ordered_keys = sorted(size_stats.keys(), key=lambda x: (x[0] * x[1], x))
 
-print('Number of different sizes{}: {}'.format(
-    ' (quantized)' if args.quantize > 1 else '', len(size_stats)
-))
+print(
+    "Number of different sizes{}: {}".format(
+        " (quantized)" if args.quantize > 1 else "", len(size_stats)
+    )
+)
 
 # all sizes
 for size in ordered_keys:
     count = size_stats[size]
-    print('{!r:12} {:4.2f} MPix, {:4d} files, {:5.1f} %, example: {}'.format(
-        size, size[0] * size[1] / 1e6, count, 100 * count / total, examples[size]))
+    print(
+        "{!r:12} {:4.2f} MPix, {:4d} files, {:5.1f} %, aspect: {:.2f}, example: {}".format(
+            size,
+            size[0] * size[1] / 1e6,
+            count,
+            100 * count / total,
+            size[0] / size[1],
+            examples[size],
+        )
+    )
 
 if args.top > 0:
-    print('Top sizes (up to {} most common):'.format(args.top))
+    print("Top sizes (up to {} most common):".format(args.top))
     top_total = 0
 
     for size, count in size_stats.most_common(args.top):
-        print('{!r:12} {:4.2f} MPix, {:4d} files, {:5.1f} %, example: {}'.format(
-            size, size[0] * size[1] / 1e6, count, 100 * count / total, examples[size]))
+        print(
+            "{!r:12} {:4.2f} MPix, {:4d} files, {:5.1f} %, aspect: {:.2f}, example: {}".format(
+                size,
+                size[0] * size[1] / 1e6,
+                count,
+                100 * count / total,
+                size[0] / size[1],
+                examples[size],
+            )
+        )
         top_total += count
 
-    print('These combined make up {:.1f} % ({:,} / {:,}) of all files.'.format(
-        100 * top_total / total, top_total, total))
+    print(
+        "These combined make up {:.1f} % ({:,} / {:,}) of all files.".format(
+            100 * top_total / total, top_total, total
+        )
+    )
 
 if args.no_plot:
     exit()
@@ -119,15 +155,18 @@ if args.no_plot:
 try:
     import matplotlib.pyplot as plt
 except ImportError:
-    print('Missing matplotlib. Plot will not be shown.\nConsider:')
-    print('pip install matplotlib')
+    print("Missing matplotlib. Plot will not be shown.\nConsider:")
+    print("pip install matplotlib")
     exit()
+
 
 def transpose(tuples):
     return list(zip(*tuples))
 
 
-w, h, n = transpose([(width, height, count) for (width, height), count in size_stats.items()])
+w, h, n = transpose(
+    [(width, height, count) for (width, height), count in size_stats.items()]
+)
 
 plt.scatter(w, h, s=n)
 
@@ -135,7 +174,7 @@ plt.scatter(w, h, s=n)
 # sorry, this doesn't work:
 # plt.axis('equal')  # https://stackoverflow.com/a/2935000/1338797
 # but this, does.
-plt.axis('scaled')  # https://stackoverflow.com/a/35994245/1338797
+plt.axis("scaled")  # https://stackoverflow.com/a/35994245/1338797
 
 # reset axes min to 0:
 plt.xlim(left=0)
--- opencv_py/detect_face_anti_slouch.py
+++ opencv_py/detect_face_anti_slouch.py
@@ -20,22 +20,27 @@ cap = cv2.VideoCapture(0)
 
 # Create the haar cascade
 cascades = [
-    '/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml',
-    'haarcascade_frontalface_default.xml',
+    "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml",
+    "haarcascade_frontalface_default.xml",
+    os.path.join(os.path.dirname(__file__), "haarcascade_frontalface_default.xml"),
 ]
 
-if hasattr(cv2, 'data'):
-    cascades.insert(0, os.path.join(cv2.data.haarcascades, 'haarcascade_frontalface_default.xml'))
+if hasattr(cv2, "data"):
+    cascades.insert(
+        0, os.path.join(cv2.data.haarcascades, "haarcascade_frontalface_default.xml")
+    )
 
 for path in cascades:
     if os.path.isfile(path):
-        print('Loading face model:', path)
+        print("Loading face model:", path)
         faceCascade = cv2.CascadeClassifier(path)
         break
 else:
-    print('No haarcascade_frontalface_default.xml found.')
-    print('Try:\nsudo apt install opencv-data\nor:')
-    print('wget https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml')
+    print("No haarcascade_frontalface_default.xml found.")
+    print("Try:\nsudo apt install opencv-data\nor:")
+    print(
+        "wget https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml"
+    )
     exit()
 
 
@@ -46,11 +51,13 @@ def face_area(rect):
     return w * h
 
 
+cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
+
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
     if not ret:
-        print('Error grabbing')
+        print("Error grabbing")
         exit()
 
     h, w = frame.shape[:2]
@@ -64,31 +71,39 @@ while True:
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30)
-        #flags = cv2.CV_HAAR_SCALE_IMAGE
+        # flags = cv2.CV_HAAR_SCALE_IMAGE
     )
 
     if len(faces) == 0:
-        print(time.strftime('%H:%M:%S'), 'No faces found')
+        print(time.strftime("%H:%M:%S"), "No faces found")
     else:
         # pick biggest face (a bit heuristic)
         x, y, w, h = max(faces, key=face_area)
 
         if initial_face_position is None:
-            initial_face_position = y + h//2
-            print(time.strftime('%H:%M:%S'), 'Noted initial face middle:', initial_face_position)
-        elif (y + h//2) - initial_face_position > MAX_SLOUCH_RATIO * h:
+            initial_face_position = y + h // 2
+            print(
+                time.strftime("%H:%M:%S"),
+                "Noted initial face middle:",
+                initial_face_position,
+            )
+        elif (y + h // 2) - initial_face_position > MAX_SLOUCH_RATIO * h:
             is_slouching = True
-            print('{} STOP SLOUCHING! (Face middle {}, from initial {})'.format(time.strftime('%H:%M:%S'), y, initial_face_position))
+            print(
+                "{} STOP SLOUCHING! (Face middle {}, from initial {})".format(
+                    time.strftime("%H:%M:%S"), y, initial_face_position
+                )
+            )
         elif is_slouching:
-            print(time.strftime('%H:%M:%S'), 'Back to normal')
+            print(time.strftime("%H:%M:%S"), "Back to normal")
             is_slouching = False
 
         # Draw a rectangle around the faces
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 # When everything done, release the capture
