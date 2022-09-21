ae9c69200d0658d18fb36a7505fee0ee9b1ea05c
tgandor <tomasz.gandor@gmail.com>
2022-09-21 11:46:07 +0200

    missing/pandas_diff.py -- diff dataframes from commandline.

---
+++ missing/pandas_diff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("data1")
+parser.add_argument("data2")
+args = parser.parse_args()
+
+df1 = pd.read_excel(args.data1)
+df2 = pd.read_excel(args.data2)
+
+print(df1.diff(df2))
