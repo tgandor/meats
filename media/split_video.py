f4361fdf5d39a75b856ee7c07fa8811c9c3dd65b
tgandor <tomasz.gandor@gmail.com>
2022-10-04 14:39:03 +0200

    media/split_video.py -- add --fps and --reencode for sometimes precise cutting.

--- media/split_video.py
+++ media/split_video.py
@@ -6,6 +6,8 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument('video_file')
 parser.add_argument('splits', nargs='+')
+parser.add_argument('--fps', '-f', type=float, help="treat splits as frame numbers, compute times")
+parser.add_argument('--reencode', "-r", action="store_true", help="don't use -c copy (exact splitting possible)")
 args = parser.parse_args()
 
 basename, ext = os.path.splitext(args.video_file)
@@ -13,9 +15,15 @@ basename, ext = os.path.splitext(args.video_file)
 startpos = '0'
 chunk = 1
 
-for split in args.splits:
-    command = 'ffmpeg -hide_banner -ss {} -to {} -i "{}" -c copy -map_metadata 0 "{}_{}{}"'.format(
-        startpos, split, args.video_file, basename, chunk, ext
+splits = args.splits
+if args.fps:
+    splits = [int(split) / args.fps for split in splits]
+
+codec = '' if args.reencode else '-c copy'
+
+for split in splits:
+    command = 'ffmpeg -hide_banner -ss {} -to {} -i "{}" {} -map_metadata 0 "{}_{}{}"'.format(
+        startpos, split, args.video_file, codec, basename, chunk, ext
     )
     print(command)
     os.system(command)
