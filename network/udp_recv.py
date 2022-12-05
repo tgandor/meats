37a30e77cb2809a6bbc8f86e79ddbeb2a7f72975
tgandor <tomasz.gandor@gmail.com>
2022-12-05 17:08:46 +0100

    network/udp_recv.py -- minor refresh.

--- network/udp_recv.py
+++ network/udp_recv.py
@@ -7,10 +7,14 @@ import argparse
 import platform
 
 parser = argparse.ArgumentParser()
-parser.add_argument('addr', help='IP to bind to', default='0.0.0.0', nargs='?')
-parser.add_argument('port', help='UDP port to bind to', type=int, default=5005, nargs='?')
-parser.add_argument('--reuse', '-r', action='store_true', help='Set SO_REUSEADDR on socket')
-parser.add_argument('--echo', '-e', action='store_true', help='Echo the message back after receiving')
+parser.add_argument("addr", help="IP to bind to", default="0.0.0.0", nargs="?")
+parser.add_argument("--port", "-p", help="UDP port to bind to", type=int, default=5005)
+parser.add_argument(
+    "--reuse", "-r", action="store_true", help="Set SO_REUSEADDR on socket"
+)
+parser.add_argument(
+    "--echo", "-e", action="store_true", help="Echo the message back after receiving"
+)
 args = parser.parse_args()
 
 UDP_IP = args.addr
@@ -18,26 +22,25 @@ UDP_PORT = args.port
 
 sock = None
 try:
-    sock = socket.socket(socket.AF_INET,  # Internet
-                         socket.SOCK_DGRAM)  # UDP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
 
     if args.reuse:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     sock.bind((UDP_IP, UDP_PORT))
 
-    print('Listening on {}:{}'.format(UDP_IP, UDP_PORT))
+    print("Listening on {}:{}".format(UDP_IP, UDP_PORT))
 
     while True:
         data, addr = sock.recvfrom(1024)
         print("received message: >%s<" % data)
         print("  received from:", addr)
         if args.echo:
-            prefix = platform.uname()[1].encode() + b' echoing: '
-            print('Sending back:', prefix+data)
-            sock.sendto(prefix+data, addr)
-        if data == b'bye':
-            print('Exit command issued.')
+            prefix = platform.uname()[1].encode() + b" echoing: "
+            print("Sending back:", prefix + data)
+            sock.sendto(prefix + data, addr)
+        if data == b"bye":
+            print("Exit command issued.")
             break
 finally:
     sock.close()
