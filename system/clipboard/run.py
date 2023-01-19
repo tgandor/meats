102d3703225092b916ed951cc7cbc13fba7c5fa1
tgandor <tomasz.gandor@gmail.com>
2023-01-19 19:25:43 +0100

    system/clipboard/run.py -- add verbose.

--- system/clipboard/run.py
+++ system/clipboard/run.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
+import argparse
 import subprocess
-import sys
 import time
 
 from pyperclip import paste
 
 MARKER = "{}"
 
-args = sys.argv[1:]
+parser = argparse.ArgumentParser()
+parser.add_argument("--verbose", "-v", action="store_true")
+parser.add_argument("args", nargs=argparse.REMAINDER)
+opts = parser.parse_args()
+
+args = opts.args
+
 if not any(MARKER in arg for arg in args):
     args.append(MARKER)
 
@@ -18,5 +24,8 @@ while True:
     time.sleep(0.1)
     new = paste()
     if new != old:
-        subprocess.call([arg.replace(MARKER, new) for arg in args])
+        command = [arg.replace(MARKER, new) for arg in args]
+        if opts.verbose:
+            print(command)
+        subprocess.call(command)
         old = new
