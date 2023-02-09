d771fb27e5fbbe114d3fcf1c2b2601265d5407de
tgandor <tomasz.gandor@gmail.com>
2023-02-10 00:14:32 +0100

    media/detect_silences.py -- improve formatting, count silences, -a for --min-amp (should be --max).

--- media/detect_silences.py
+++ media/detect_silences.py
@@ -2,6 +2,7 @@
 
 import argparse
 import itertools as it
+from datetime import datetime, timedelta
 from os import system
 from os.path import splitext, exists
 
@@ -12,12 +13,21 @@ from scipy.io import wavfile
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--min-silence", "-m", type=float, default=0.2)
-parser.add_argument("--min-amp", type=int, default=1000)
+parser.add_argument("--min-amp", "-a", type=int, default=1000)
 parser.add_argument("file")
 args = parser.parse_args()
 
-if not args.file.lower().endswith('.wav'):
-    wave_file = splitext(args.file)[0] + '.wav'
+
+def fmt_pos(pos, rate):
+    s = pos / rate
+    fmt = "%M:%S.%f"
+    if s > 3600:
+        fmt = "%H:" + fmt
+    return (datetime.fromordinal(1) + timedelta(seconds=s)).strftime(fmt)[:-3]
+
+
+if not args.file.lower().endswith(".wav"):
+    wave_file = splitext(args.file)[0] + ".wav"
     if not exists(wave_file):
         print(f"Warning: {wave_file} does not exist, creating...")
         system(f'ffmpeg -i "{args.file}" "{wave_file}"')
@@ -29,10 +39,12 @@ low = np.abs(data[:, 0]) // args.min_amp
 limit = args.min_silence * rate
 
 L = pos = 0
+num_sil = 0
 
 for k, v in it.groupby(low):
     pos += L
     L = len(list(v))
     if L < limit:
         continue
-    print(f"{pos}:{pos+L}: [{k}] * {L} (at {pos/rate:.2f}, {L/rate:.2f}s)")
+    num_sil += 1
+    print(f"{num_sil:4d} {fmt_pos(pos, rate)} - {fmt_pos(pos + L, rate)}: {L/rate:.3f} s ([{k}] * {L})")
