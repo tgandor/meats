866063c5707d179f27d6bc88f358534262a1d798
tgandor <tomasz.gandor@gmail.com>
2023-02-14 14:10:39 +0100

    installer/entrypoint/install.py -- fix is_writable(), add --dest,-d for custom.

--- installer/entrypoint/install.py
+++ installer/entrypoint/install.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import uuid
+import argparse
 import os
 import stat
 import string
 import sys
+import uuid
 
 
 def is_writable(dirpath):
@@ -16,33 +17,43 @@ def is_writable(dirpath):
         with open(test_file, "w") as test:
             test.write("hello")
         os.unlink(test_file)
-    except OSError:
+        return True
+    except OSError as e:
+        print(e)
         return False
 
 
-# find writable path
-path = os.getenv("PATH").split(os.pathsep)
-good = []
+def find_writable_path():
+    path = os.getenv("PATH").split(os.pathsep)
+    good = []
 
-for dirpath in path:
-    writable = is_writable(dirpath)
-    print(dirpath, ":", writable)
-    if writable:
-        good.append(dirpath)
+    for dirpath in path:
+        writable = is_writable(dirpath)
+        print(dirpath, ":", writable)
+        if writable:
+            good.append(dirpath)
 
-if not good:
-    exit("No directories writable.")
+    if not good:
+        exit("No directories writable.")
 
-if len(good) > 1:
-    print("Choose path destination:")
-    paths = dict(zip(string.ascii_letters, good))
-    for c, target in paths.items():
-        print(c, "-", target)
-    ans = input("Choice (default: a):")
-    target = paths.get(ans, paths["a"])
-else:
-    target = good[0]
+    if len(good) > 1:
+        print("Choose path destination:")
+        paths = dict(zip(string.ascii_letters, good))
+        for c, target in paths.items():
+            print(c, "-", target)
+        ans = input("Choice (default: a):")
+        target = paths.get(ans, paths["a"])
+    else:
+        target = good[0]
 
+    return target
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dest", "-d")
+args = parser.parse_args()
+
+
+target = args.dest or find_writable_path()
 print("Installing to directory:", target)
 
 LOCAL = os.path.join(target, "mts")
