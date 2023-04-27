9971b45c4dbf7bc8ec2b30208dc7e63fb1a92f83
tgandor <tomasz.gandor@gmail.com>
2023-04-27 22:52:39 +0200

    missing/wcgrep.py -- add -l (list) and -q (quiet) options.

--- missing/wcgrep.py
+++ missing/wcgrep.py
@@ -9,26 +9,32 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument("search")
 parser.add_argument("--all", "-a", action="store_true")
+parser.add_argument("--list", "-l", action="store_true")
+parser.add_argument("--quiet", "-q", action="store_true")
 args = parser.parse_args()
 
 search = args.search
 
 for directory, _, files in os.walk("."):
-    if directory[2:].startswith('.'):  # skip .\
+    if directory[2:].startswith("."):  # skip .\
         continue
     for f in files:
-        if f.startswith('.') and not args.all:
+        if f.startswith(".") and not args.all:
             continue
         full_name = os.path.join(directory, f)
-        # print(full_name)
         with open(full_name) as lines:
             nl = 0
             try:
                 for line in lines:
                     nl += 1
                     if search in line:
-                        print(full_name, ':', nl, ':', line.rstrip())
+                        if args.list:
+                            print(full_name)
+                            break
+                        print(full_name, ":", nl, ":", line.rstrip())
             except UnicodeDecodeError:
-                print('Error decoding:', full_name, file=sys.stderr)
+                if not args.quiet:
+                    print("Error decoding:", full_name, file=sys.stderr)
             except PermissionError:
-                print('Error accessing:', full_name, file=sys.stderr)
+                if not args.quiet:
+                    print("Error accessing:", full_name, file=sys.stderr)
