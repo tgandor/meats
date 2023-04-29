9decabbeea79493efb36f95e9b8fdd2977b77054
tgandor <tomasz.gandor@gmail.com>
2023-04-29 23:01:02 +0200

    media/splits_from_whisper.py -- new approach, for multiple .json files; -p now specifies audio extension to process.

--- media/splits_from_whisper.py
+++ media/splits_from_whisper.py
@@ -6,35 +6,49 @@ import os
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument("json_file")
 parser.add_argument("split_keyword")
-parser.add_argument("--exclude", "-x")
-parser.add_argument("--process", "-p", help="file_to_process using ~/split_video.py")
-parser.add_argument("--ci", "-i", action="store_true")
+parser.add_argument("json_files", nargs="+")
+parser.add_argument("--exclude", "-x", help="negative keyword to ignore when ")
+parser.add_argument(
+    "--process", "-p", help="file extension to replace .json and run ./split_video.py"
+)
+parser.add_argument("--case-sensitive", "-c", action="store_true")
 parser.add_argument("--verbose", "-v", action="store_true")
+parser.add_argument(
+    "--text", "-t", action="store_true", help="print matching segment texts"
+)
 args = parser.parse_args()
+ci = not args.case_sensitive
+key = args.split_keyword.lower() if ci else args.split_keyword
 
-key = args.split_keyword.lower() if args.ci else args.split_keyword
+for json_file in args.json_files:
+    if args.text:
+        print(json_file)
+    with open(json_file) as jsf:
+        data = json.load(jsf)
 
-with open(args.json_file) as jsf:
-    data = json.load(jsf)
+    splits = []
 
-splits = []
+    for segment in data["segments"]:
+        text = segment["text"].lower() if ci else segment["text"]
+        if args.exclude and args.exclude in text:
+            continue
+        if key in text:
+            splits.append(segment["start"])
+            if args.verbose:
+                print(segment)
+            if args.text:
+                print(segment["text"])
 
-for segment in data["segments"]:
-    text = segment["text"].lower() if args.ci else segment["text"]
-    if args.exclude and args.exclude in text:
-        continue
-    if key in text:
-        splits.append(segment["start"])
-        if args.verbose:
-            print(segment)
+    print(" ".join(str(s) for s in splits))
 
-print(" ".join(str(s) for s in splits))
+    if args.process:
+        # Is this really how code re-use should work?
+        splitter = os.path.join(os.path.dirname(__file__), "split_video.py")
+        target = json_file.replace("json", args.process)
+        if not os.path.exists(target):
+            raise ValueError(f"Target file {target} not found!")
 
-if args.process:
-    # Is this really how code re-use should work?
-    splitter = os.path.join(os.path.dirname(__file__), "split_video.py")
-    os.system(
-        " ".join([sys.executable, splitter, args.process] + [str(s) for s in splits])
-    )
+        os.system(
+            " ".join([sys.executable, splitter, target] + [str(s) for s in splits])
+        )
