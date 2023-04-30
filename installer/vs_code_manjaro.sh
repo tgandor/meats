52444d5bb4347e64867d59ae7a581c1f36333cef
tgandor <tomasz.gandor@gmail.com>
2023-04-30 02:01:27 +0200

    installer/vs_code_manjaro.sh -- when  != '' download and build for offline installation.

--- installer/vs_code_manjaro.sh
+++ installer/vs_code_manjaro.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-pamac build visual-studio-code-bin
+if [ "$1" == "" ] ; then
+    pamac build visual-studio-code-bin
+else
+    [ -d visual-studio-code-bin ] || git clone https://aur.archlinux.org/visual-studio-code-bin.git
+    cd visual-studio-code-bin
+    makepkg -s
+    mv visual-studio-code-bin*.pkg.tar.zst ..
+    git clean -dfx
+fi
