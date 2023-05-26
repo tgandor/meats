3a7088f124d26c71be427d4cccb3cecfdbc28948
tgandor <tomasz.gandor@gmail.com>
2023-05-26 08:28:27 +0200

    splits_from_whisper.py -- extended -x option to files / multiple values.

--- media/splits_from_whisper.py
+++ media/splits_from_whisper.py
@@ -8,7 +8,12 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument("split_keyword")
 parser.add_argument("json_files", nargs="+")
-parser.add_argument("--exclude", "-x", help="negative keyword to ignore when ")
+parser.add_argument(
+    "--exclude",
+    "-x",
+    help="negative [filenames with] substring(s) to ignore",
+    nargs="*",
+)
 parser.add_argument(
     "--process", "-p", help="file extension to replace .json and run ./split_video.py"
 )
@@ -27,6 +32,25 @@ args = parser.parse_args()
 ci = not args.case_sensitive
 key = args.split_keyword.lower() if ci else args.split_keyword
 
+
+class Excludes:
+    def __init__(self, excludes, case_sensitive) -> None:
+        self.excludes = []
+        for item in excludes:
+            if os.path.exists(item):
+                self.excludes.extend(line.strip() for line in open(item))
+            else:
+                self.excludes.append(item)
+        if not case_sensitive:
+            self.excludes = [item.lower() for item in self.excludes]
+
+    def match(self, candidate):
+        return any(item in candidate for item in self.excludes)
+
+
+excludes = Excludes(args.exclude, args.case_sensitive)
+
+
 for json_file in args.json_files:
     if args.text:
         print(json_file)
@@ -38,7 +62,9 @@ for json_file in args.json_files:
 
     for segment in data["segments"]:
         text = segment["text"].lower() if ci else segment["text"]
-        if args.exclude and args.exclude in text:
+        if excludes.match(text):
+            if args.verbose:
+                print(f"IGNORING: {segment['text']}")
             continue
         if key in text:
             splits.append(segment["start"])
@@ -52,7 +78,7 @@ for json_file in args.json_files:
 
     if args.descriptions:
         basename, ext = os.path.splitext(json_file)
-        assert ext == '.json'
+        assert ext == ".json"
         for chunk, description in enumerate(descriptions, start=1):
             filename = f"{basename}_{chunk}.txt"
             print(f"{filename}: {description}")
