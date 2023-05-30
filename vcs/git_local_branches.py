075518a98a6b006ed643589170e9b19b7132211a
tgandor <tomasz.gandor@gmail.com>
2023-05-30 17:17:59 +0200

    vcs/git_local_branches.py -- add --force/-f switch for hard delete.

--- vcs/git_local_branches.py
+++ vcs/git_local_branches.py
@@ -9,31 +9,30 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--run', action='store_true')
+parser.add_argument("--force", "-f", action="store_true", help="use -D for deleting")
+parser.add_argument("--run", action="store_true", help="execute commands directly")
 args = parser.parse_args()
 
 branches = {
-    line[2:].strip()
-    for line in os.popen('git branch -a')
-    if ' -> ' not in line
+    line[2:].strip() for line in os.popen("git branch -a") if " -> " not in line
 }
 
 remotes = {
-    '/'.join(branch.split('/')[2:])
+    "/".join(branch.split("/")[2:])
     for branch in branches
-    if branch.startswith('remotes/')
+    if branch.startswith("remotes/")
 }
 
-local_branches = set(filter(lambda x: not x.startswith('remotes/'), branches))
+local_branches = set(filter(lambda x: not x.startswith("remotes/"), branches))
 
-commands = ['git branch -d {}'.format(branch) for branch in (local_branches - remotes)]
+commands = ["git branch -d {}".format(branch) for branch in (local_branches - remotes)]
 
 if not args.run:
     if commands:
-        print('\n'.join(commands))
+        print("\n".join(commands))
 else:
     for cmd in commands:
         print(cmd)
         os.system(cmd)
     if not commands:
-        print('Nothing to do.')
+        print("Nothing to do.")
