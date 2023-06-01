a7895b102ea6ea2fccb531fa31c91879ba256f78
tgandor <tomasz.gandor@gmail.com>
2023-06-01 15:38:15 +0200

    system/pac/*_cache.sh -- add common continuations (with confirmation).

--- system/pac/pack_cache.sh
+++ system/pac/pack_cache.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+confirm() {
+    while true; do
+        read -p "$1 [Y/N]: " response
+        case "$response" in
+            [Yy]) return 0 ;;
+            [Nn]) return 1 ;;
+            *) echo "Invalid response. Please enter Y or N." ;;
+        esac
+    done
+}
+
 tar cf pacman-`date --iso`.tar /var/cache/pacman/pkg /var/lib/pacman/sync
 echo "Packages tarred into:" pacman-`date --iso`.tar
 ls -lh pacman-`date --iso`.tar
-echo "Clean cache e.g. with ~/meats/system/pac/clean_cache.sh --all"
+
+if confirm "Do you want to run: ~/meats/system/pac/clean_cache.sh --all"; then
+    ~/meats/system/pac/clean_cache.sh --all
+    ls -lh pacman-`date --iso`.tar
+else
+    echo "Done."
+fi
--- system/pac/put_cache.sh
+++ system/pac/put_cache.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
 
+confirm() {
+    while true; do
+        read -p "$1 [Y/N]: " response
+        case "$response" in
+            [Yy]) return 0 ;;
+            [Nn]) return 1 ;;
+            *) echo "Invalid response. Please enter Y or N." ;;
+        esac
+    done
+}
+
+shopt -s nullglob
+
 if [ "$1" == "" ] ; then
     echo "Usage: $0 pacman-<date>.tar"
-    exit
+    pattern="pacman-20??-??-??.tar"
+    matches=($pattern)
+    if [[ ${#matches[@]} -eq 1 ]]; then
+        echo "Guessing: $matches" 
+        tar_path=`realpath $matches`
+    else
+        echo "No files matched or more than one file matched: $matched"
+        exit
+    fi
+else
+    tar_path=`realpath $1`
 fi
 
-tar_path=`realpath $1`
 cd /
 sudo tar xvf "$tar_path"
 echo "Finished unpacking $tar_path"
-echo "You can now run:"
-echo "~/meats/system/pac/upgrade.sh"
+
+if confirm "Do you want to run: ~/meats/system/pac/upgrade.sh"; then
+    ~/meats/system/pac/upgrade.sh
+    if confirm "~/meats/system/pac/clean_cache.sh --all"; then
+        ~/meats/system/pac/clean_cache.sh --all
+    fi
+else
+    echo "Done."
+fi
+
