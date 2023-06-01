dcebd76b4c34f3a58acad40aad53c4d9ba608e68
tgandor <tomasz.gandor@gmail.com>
2023-06-01 15:10:37 +0200

    system/pac/pack_cache.sh -- also store package databases.

--- system/pac/pack_cache.sh
+++ system/pac/pack_cache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar cf pacman-`date --iso`.tar /var/cache/pacman/pkg/*
+tar cf pacman-`date --iso`.tar /var/cache/pacman/pkg /var/lib/pacman/sync
 echo "Packages tarred into:" pacman-`date --iso`.tar
 ls -lh pacman-`date --iso`.tar
 echo "Clean cache e.g. with ~/meats/system/pac/clean_cache.sh --all"
