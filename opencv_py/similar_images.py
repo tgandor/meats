028a02b7cbd6ce88271e08162942bb54786de3c6
tgandor <tomasz.gandor@gmail.com>
2023-06-05 18:05:33 +0200

    opencv_py/similar_images.py -- add --threshold and --delete options.

--- opencv_py/similar_images.py
+++ opencv_py/similar_images.py
@@ -2,6 +2,7 @@
 
 import argparse
 import multiprocessing
+import os
 
 import cv2
 from skimage.metrics import structural_similarity
@@ -24,12 +25,22 @@ def main():
         description="Calculate SSIM values for consecutive image pairs"
     )
     parser.add_argument("files", nargs="+", help="Image files to calculate SSIM")
+    parser.add_argument("--threshold", "-t", type=float, default=1.01)
+    parser.add_argument("--delete", "-d", action="store_true")
     args = parser.parse_args()
 
     p = multiprocessing.Pool()
+    copies = []
 
     for image1, image2, ssim_value in p.imap(ssim, zip(args.files, args.files[1:])):
-        print(f"SSIM value between {image1} and {image2}: {ssim_value}")
+        if ssim_value < args.threshold:
+            print(f"SSIM value between {image1} and {image2}: {ssim_value}")
+        else:
+            copies.append(image2)
+
+    if args.delete:
+        for image in copies:
+            os.remove(image)
 
 
 if __name__ == "__main__":
