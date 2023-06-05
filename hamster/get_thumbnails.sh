13e206c9894825071c66d63319b1c37a542b4440
tgandor <tomasz.gandor@gmail.com>
2023-06-05 17:51:46 +0200

    opencv_py/similar_images.py -- SSIM for consecutive args.

--- hamster/get_thumbnails.sh
+++ hamster/get_thumbnails.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" == "-n" ] ; then
     # default name (title)-(id)
-    xargs -n1 youtube-dl --skip-download --write-thumbnail
+    xargs -n1 yt-dlp --skip-download --write-thumbnail
 else
     # usually gets WebP, but there is another way of getting JPGs
-    xargs -n1 youtube-dl --skip-download --write-thumbnail --output "%(id)s"
+    xargs -n1 yt-dlp --skip-download --write-thumbnail --output "%(id)s"
 fi
---
+++ opencv_py/similar_images.py
@@ -0,0 +1,35 @@
+#! /usr/bin#!/usr/bin/env python
+
+import argparse
+import multiprocessing
+
+import cv2
+from skimage.metrics import structural_similarity as ssim
+
+
+def calculate_ssim(image1, image2):
+    img1 = cv2.imread(image1, cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread(image2, cv2.IMREAD_GRAYSCALE)
+
+    score, _ = ssim(img1, img2, full=True)
+    return score
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Calculate SSIM values for consecutive image pairs"
+    )
+    parser.add_argument("files", nargs="+", help="Image files to calculate SSIM")
+    args = parser.parse_args()
+
+    p = multiprocessing.Pool()
+
+    for image1, image2 in p.imap(
+        lambda t: calculate_ssim(*t), zip(args.files, args.files[1:])
+    ):
+        ssim_value = calculate_ssim(image1, image2)
+        print(f"SSIM value between {image1} and {image2}: {ssim_value}")
+
+
+if __name__ == "__main__":
+    main()
