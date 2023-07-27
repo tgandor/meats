18562a60e399fdee593f9506bbb738de3d3399c7
tgandor <tomasz.gandor@gmail.com>
2023-07-27 15:22:03 +0200

    hamster/get_thumbnails.sh -- prefix with date.

--- hamster/get_thumbnails.sh
+++ hamster/get_thumbnails.sh
@@ -5,5 +5,5 @@ if [ "$1" == "-n" ] ; then
     xargs -n1 yt-dlp --skip-download --write-thumbnail
 else
     # usually gets WebP, but there is another way of getting JPGs
-    xargs -n1 yt-dlp --skip-download --write-thumbnail --output "%(id)s"
+    xargs -n1 yt-dlp --skip-download --write-thumbnail --output "%(upload_date>%Y%m%d)s_%(id)s"
 fi
