912ce040bc6374fdb6d327cef1a243569f5e3813
tgandor <tomasz.gandor@gmail.com>
2023-09-25 10:31:20 +0200

    installer/vs_code_manjaro.sh -- fix for existing repository.

--- installer/vs_code_manjaro.sh
+++ installer/vs_code_manjaro.sh
@@ -5,6 +5,7 @@ if [ "$1" == "" ] ; then
 else
     [ -d visual-studio-code-bin ] || git clone https://aur.archlinux.org/visual-studio-code-bin.git
     cd visual-studio-code-bin
+    git pull
     makepkg -s
     mv visual-studio-code-bin*.pkg.tar.zst ..
     git clean -dfx
