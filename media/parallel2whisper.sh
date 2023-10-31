5980d0305e9a29b3efa90202aa689ad526a9a622
tgandor <tomasz.gandor@gmail.com>
2023-10-31 01:55:12 +0100

    media/parallel2whisper.sh -- fix condition, pass arg in old parallel.

--- media/parallel2whisper.sh
+++ media/parallel2whisper.sh
@@ -3,10 +3,10 @@
 ngpu=`nvidia-smi -L | wc -l`
 echo "Using $ngpu process slots."
 pver=`parallel --version | awk 'NR==1 {print $NF}'`
-if [ "$pver" -ge "20230922"] ; then
+if [ "$pver" -ge "20230922" ] ; then
     echo "Using parallel $pver and --process-slot-var="
     time parallel --jobs $ngpu --process-slot-var=CUDA_VISIBLE_DEVICES whisper ::: "$@"
 else
     echo "Using old parallel $pver and hacking through env and perl expr with slot()."
-    time parallel --jobs $ngpu env 'CUDA_VISIBLE_DEVICES={= $_=slot()-1 =}' whisper ::: "$@"
+    time parallel --jobs $ngpu env 'CUDA_VISIBLE_DEVICES={= $_=slot()-1 =}' whisper {} ::: "$@"
 fi
