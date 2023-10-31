b9a8df91ac8db37cf823d8be22752ab3032deb73
tgandor <tomasz.gandor@gmail.com>
2023-10-31 01:50:53 +0100

    media/parallel2whisper.sh -- hack around old parallel with slot().

--- media/parallel2whisper.sh
+++ media/parallel2whisper.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-ngpu = `nvidia-smi -L | wc -l`
+ngpu=`nvidia-smi -L | wc -l`
 echo "Using $ngpu process slots."
-time parallel --jobs $ngpu --process-slot-var=CUDA_VISIBLE_DEVICES whisper ::: "$@"
-
+pver=`parallel --version | awk 'NR==1 {print $NF}'`
+if [ "$pver" -ge "20230922"] ; then
+    echo "Using parallel $pver and --process-slot-var="
+    time parallel --jobs $ngpu --process-slot-var=CUDA_VISIBLE_DEVICES whisper ::: "$@"
+else
+    echo "Using old parallel $pver and hacking through env and perl expr with slot()."
+    time parallel --jobs $ngpu env 'CUDA_VISIBLE_DEVICES={= $_=slot()-1 =}' whisper ::: "$@"
+fi
