3b418c72e04f2488678a3a7ac2a664367937f18a
tgandor <tomasz.gandor@gmail.com>
2024-07-25 09:13:13 +0200

    even_circles.py -- add --round/-r parameter for centers.

--- toys/even_circles.py
+++ toys/even_circles.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser(description="Print positions of circle centers 
 parser.add_argument("length", type=float)
 parser.add_argument("diameter", type=float)
 parser.add_argument("num_circles", type=int, nargs="?", help="number of circles; <= length/diameter")
+parser.add_argument("--round", "-r", type=int, help="decimal places to round")
 
 args = parser.parse_args()
 
@@ -19,6 +20,10 @@ pad = (args.length - args.diameter * args.num_circles) / (args.num_circles + 1)
 w = args.length / args.num_circles
 r = args.diameter / 2
 centers = [pad + r + i * (pad + 2 * r) for i in range(args.num_circles)]
+
+if args.round is not None:
+    centers = [round(c, args.round) for c in centers]
+
 print(centers)
 for c in centers:
     print((c-r, c+r))
