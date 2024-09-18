28ed043d859c915d94ee467453ceb476c2daea9e
tgandor <tomasz.gandor@gmail.com>
2024-09-18 12:11:46 +0200

    missing/clipboard_copy.py -- add argparse: --strip and --gui.

--- missing/clipboard_copy.py
+++ missing/clipboard_copy.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
+import argparse
 import queue
 import sys
 import threading
@@ -10,6 +9,7 @@ import threading
 def copy(data, wait_gui=False):
     try:
         import Tkinter as Tk
+
         tk = Tk
     except ImportError:
         # welcome to Python3
@@ -33,13 +33,13 @@ def copy(data, wait_gui=False):
             r.destroy()
 
     if not wait_gui:
-        print('Data was copied into clipboard. Paste and press ENTER to exit...')
+        print("Data was copied into clipboard. Paste and press ENTER to exit...")
         # This won't work: RuntimeError: Calling Tcl from different apartment
-        '''
+        """
         gui_thread = threading.Thread(target=r.mainloop)
         gui_thread.setDaemon(True)
         gui_thread.start()
-        '''
+        """
         # such an overkill, but this is how it can work:
         # https://effbot.org/zone/tkinter-threads.htm
         threading.Thread(target=read_stdin, args=(pill_q,)).start()
@@ -47,16 +47,28 @@ def copy(data, wait_gui=False):
         r.mainloop()
     else:
         # stdin already read; use GUI to exit
-        print('Data was copied into clipboard. Paste, then close popup to exit...')
-        tk.Button(r, text='Click or press key to exit', command=r.destroy, width=40, height=10).pack(fill=tk.BOTH)
-        r.title('Clipboard Copy')
-        r.bind('<Key>', lambda e: r.destroy() if e.keysym != 'Alt_L' else None)
+        print("Data was copied into clipboard. Paste, then close popup to exit...")
+        tk.Button(
+            r, text="Click or press key to exit", command=r.destroy, width=40, height=10
+        ).pack(fill=tk.BOTH)
+        r.title("Clipboard Copy")
+        r.bind("<Key>", lambda e: r.destroy() if e.keysym != "Alt_L" else None)
         r.deiconify()
         r.mainloop()
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        copy(sys.stdin.read(), True)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--strip", "-s", action="store_true")
+    parser.add_argument(
+        "--gui", "-g", action="store_true", help="Use GUI (always when reading stdin)."
+    )
+    parser.add_argument("args", nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+    data = " ".join(args.args) if args.args else sys.stdin.read()
+    if args.strip:
+        data = data.strip()
+    if not args.args:
+        copy(data, True)
     else:
-        copy(' '.join(sys.argv[1:]))
+        copy(data, args.gui)
