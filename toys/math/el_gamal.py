a4fef3cd6cc77aae919d89062ae9381200105724
tgandor <tomasz.gandor@gmail.com>
2024-10-22 10:19:44 +0200

    opencv_py/motion_detection_cv2.py -- MOG2 example for videos.

--- toys/math/el_gamal.py
+++ toys/math/el_gamal.py
@@ -1,6 +1,7 @@
 from Cryptodome.Util.number import getPrime, bytes_to_long, long_to_bytes  # , inverse
 import random
 
+
 def generate_keys(bits=256):
     p = getPrime(bits)
     g = random.randint(2, p - 2)
@@ -8,12 +9,14 @@ def generate_keys(bits=256):
     y = pow(g, x, p)  # Public key (y = g^x mod p)
     return (p, g, y), x  # (public_key, private_key)
 
+
 def encrypt(public_key, plaintext):
     p, g, y = public_key
     k = random.randint(1, p - 2)
     a = pow(g, k, p)
     b = (pow(y, k, p) * bytes_to_long(plaintext)) % p
     return a, b
+
 
 def decrypt(private_key, public_key, ciphertext):
     a, b = ciphertext
@@ -25,8 +28,9 @@ def decrypt(private_key, public_key, ciphertext):
     plaintext = b * pow(a, -x, p) % p
     return long_to_bytes(plaintext)
 
+
 public_key, private_key = generate_keys()
-print("Keys:", public_key, private_key) 
+print("Keys:", public_key, private_key)
 plaintext = b"Hello, ElGamal!"
 ciphertext = encrypt(public_key, plaintext)
 decrypted_plaintext = decrypt(private_key, public_key, ciphertext)
