3b8f137e3f8be0291f94c013cdc176da0d5efbb8
tgandor <tomasz.gandor@gmail.com>
2025-03-14 01:18:09 +0100

    toys/test_compression.py -- modernized, new compressors: zstd, pigz.

---
+++ installer/compression-wishlist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt install -y \
+    brotli \
+    lzip \
+    lzma \
+    lzop \
+    p7zip-full \
+    pigz \
+    plzip \
+    rar \
+    xz-utils \
+    zip \
+    zstd \
+# end.
--- toys/test_compression.py
+++ toys/test_compression.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
+import argparse
 import os
-import sys
 import time
 
 
@@ -17,27 +17,79 @@ def human(x):
     for suffix in ["", "K", "M", "G", "T"]:
         if x < 1024:
             return "%.1f %s" % (x, suffix)
-        x /= 1024.0
+        x /= 1024
     return "%.1f P" % x
 
 
-compressors = [
+try:
+    from shutil import which
+except ImportError:
+
+    def which(program):
+        def is_exe(file_path):
+            return os.path.isfile(file_path) and os.access(file_path, os.X_OK)
+
+        fpath, _ = os.path.split(program)
+        if fpath:
+            if is_exe(program):
+                return program
+        else:
+            for path in os.environ["PATH"].split(os.pathsep):
+                path = path.strip('"')
+                exe_file = os.path.join(path, program)
+                if is_exe(exe_file):
+                    return exe_file
+
+        return None
+
+
+known_compressors = [
     ("bzip2", "bzip2 %(f)s", "%s.bz2", "bunzip2 %(f)s.bz2"),
     ("gzip", "gzip -9 %(f)s", "%s.gz", "gunzip %(f)s.gz"),
     ("lzip", "lzip -9 %(f)s", "%s.lz", "lzip -d %(f)s.lz"),
-    ("xz", "xz -9 %(f)s", "%s.xz", "xz -d %(f)s.xz"),
-    ("lzop", "lzop -9 %(f)s", "%s.lzo", "lzop -d -f %(f)s.lzo"),
-    ("plzip", "plzip -9 %(f)s", "%s.lz", "plzip -d %(f)s.lz"),
     ("lzma", "lzma -9 %(f)s", "%s.lzma", "lzma -d %(f)s.lzma"),
+    ("lzop", "lzop -3 %(f)s", "%s.lzo", "lzop -d -f %(f)s.lzo; rm %(f)s.lzo"),
+    ("lzop", "lzop -9 %(f)s", "%s.lzo", "lzop -d -f %(f)s.lzo; rm %(f)s.lzo"),
+    ("pigz", "pigz -9 %(f)s", "%s.gz", "pigz -d %(f)s.gz"),
+    ("plzip", "plzip -0 %(f)s", "%s.lz", "plzip -d %(f)s.lz"),
+    ("plzip", "plzip -6 %(f)s", "%s.lz", "plzip -d %(f)s.lz"),
+    ("plzip", "plzip -9 %(f)s", "%s.lz", "plzip -d %(f)s.lz"),
+    ("xz", "xz -9 %(f)s", "%s.xz", "xz -d %(f)s.xz"),
     (
         "zip",
         "zip -q -9 %(f)s.zip %(f)s",
         "%s.zip",
         "unzip -q -o %(f)s.zip; rm %(f)s.zip",
     ),
+    ("zstd", "zstd -q %(f)s; rm %(f)s", "%s.zst", "unzstd -q %(f)s.zst; rm %(f)s.zst"),
 ]
 
-for f in sys.argv[1:]:
+FAST = {
+    "pigz -9 %(f)s",
+    "lzop -3 %(f)s",
+    "zstd",
+    "plzip -0 %(f)s",
+    "plzip -6 %(f)s",  # quite slow, but acceptable
+    "zstd -q %(f)s; rm %(f)s",
+}
+parser = argparse.ArgumentParser()
+parser.add_argument("files", nargs="+")
+parser.add_argument("--all", "-a", action="store_true", help="include slow / redundant")
+args = parser.parse_args()
+
+compressors = []
+for comp in known_compressors:
+    if not which(comp[0]):
+        print("Skipping %s (program not found)" % comp[0])
+        continue
+
+    if comp[1] not in FAST and not args.all:
+        print("... skipping %s (use --all to include in tests)" % comp[1])
+    else:
+        compressors.append(comp)
+print()
+
+for f in args.files:
     if not os.path.exists(f):
         print("Skipping %s: file does not exist" % f)
         continue
@@ -54,7 +106,7 @@ for f in sys.argv[1:]:
     )
     for name, comp, out, decompress in compressors:
         if os.system("which %s > /dev/null" % name) == 0:
-            print("  Testing", name)
+            print("  Testing", name, ":", comp % dict(f=f), "...")
             elapsed = timed(comp % dict(f=f))
             compressed = os.path.getsize(out % f)
             print(
