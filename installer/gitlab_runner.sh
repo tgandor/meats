b9413ba94d190bde3630cc9e0937885c0ec00689
tgandor <tomasz.gandor@gmail.com>
2025-04-03 18:07:40 +0200

    installer/gitlab_runner.sh -- non-docker CI runner installer.

---
+++ installer/gitlab_runner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Thu Apr  3 18:06:04 CEST 2025
+# Snippet to install as per GitLab
+
+# Download the binary for your system
+sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+
+# Give it permission to execute
+sudo chmod +x /usr/local/bin/gitlab-runner
+
+# Create a GitLab Runner user
+sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+
+# Install and run as a service
+sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+sudo gitlab-runner start
+
