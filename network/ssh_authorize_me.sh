dee32ace3f3d3216367a7de1929dfefdf25dc4e6
tgandor <tomasz.gandor@gmail.com>
2025-06-27 19:38:57 +0200

    .\network\ssh_authorize_me.sh -- add support for id_ed25519.

--- network/ssh_authorize_me.sh
+++ network/ssh_authorize_me.sh
@@ -2,23 +2,38 @@
 
 if [ -z "$1" ] ; then
     echo "Usage: $0 user@host [-p port etc.]"
-    echo "Adds id_rsa.pub (generates if missing) to user's .ssh/authorized_keys"
+    echo "Adds your public SSH key (id_rsa.pub, id_ed25519.pub, etc.) to user's .ssh/authorized_keys"
     exit
 fi
 
-if [ ! -f $HOME/.ssh/id_rsa ] ; then
-    echo "Generating Keys, press enter a couple of times..."
-    ssh-keygen
+# List of supported key types (add more if needed)
+KEYTYPES=(id_ed25519 id_rsa)
+KEYFOUND=""
+
+for keytype in "${KEYTYPES[@]}"; do
+    if [ -f "$HOME/.ssh/${keytype}" ]; then
+        KEYFOUND="$keytype"
+        break
+    fi
+done
+
+if [ -z "$KEYFOUND" ]; then
+    # No key found, generate ed25519 by default
+    echo "No SSH key found. Generating id_ed25519 (recommended)..."
+    ssh-keygen -t ed25519 -f "$HOME/.ssh/id_ed25519"
+    KEYFOUND="id_ed25519"
 fi
 
-if ( ssh -o PasswordAuthentication=no $* pwd ) ; then
+PUBKEY="$HOME/.ssh/${KEYFOUND}.pub"
+
+if ( ssh -o PasswordAuthentication=no "$@" pwd ) ; then
     echo "You are already authorized!"
     exit
 fi
 
-cat $HOME/.ssh/id_rsa.pub | ssh $* "mkdir -p .ssh ; tee -a .ssh/authorized_keys"
+cat "$PUBKEY" | ssh "$@" "mkdir -p .ssh ; tee -a .ssh/authorized_keys"
 
-if ( ssh -o PasswordAuthentication=no $* pwd ) ; then
+if ( ssh -o PasswordAuthentication=no "$@" pwd ) ; then
     echo "Success!"
 else
     echo "Something doesn't work. Change to 0600?"
