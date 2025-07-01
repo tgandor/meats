a8e82fbad657601fa556ea29e40f351c40e34d56
tgandor <tomasz.gandor@gmail.com>
2025-07-01 05:06:44 +0200

    command autodetection.

--- hamster/get_playlist.sh
+++ hamster/get_playlist.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if which youtube-dl >/dev/null 2>&1; then
+    dl=youtube-dl
+elif which yt-dlp >/dev/null 2>&1; then
+    dl=yt-dlp
+else
+    echo "Neither youtube-dl nor yt-dlp found, please install one of them first."
+    exit 1
+fi
+
 # .%(ext)s required because of -x, error otherwise.
 # would like to --embed-thumbnail, but it only works for mp3 and m4a (not opus)
-youtube-dl -x --write-thumbnail -o "%(playlist_index)02d-%(title)s.%(ext)s" "$@"
+$dl -x --write-thumbnail -o "%(playlist_index)02d-%(title)s.%(ext)s" "$@"
--- hamster/get_playlist_mp3.sh
+++ hamster/get_playlist_mp3.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+if which youtube-dl >/dev/null 2>&1; then
+    dl=youtube-dl
+elif which yt-dlp >/dev/null 2>&1; then
+    dl=yt-dlp
+else
+    echo "Neither youtube-dl nor yt-dlp found, please install one of them first."
+    exit 1
+fi
+
 # to consider: --write-info-json
-youtube-dl --extract-audio --audio-format mp3 --embed-thumbnail --output "%(playlist_index)02d-%(title)s.%(ext)s" "$@"
+$dl --extract-audio --audio-format mp3 --embed-thumbnail --output "%(playlist_index)02d-%(title)s.%(ext)s" "$@"
