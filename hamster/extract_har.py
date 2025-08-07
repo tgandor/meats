60c770926187aaf09295edc59391059c1e038923
tgandor <tomasz.gandor@gmail.com>
2025-08-07 10:00:18 +0200

    .\hamster\extract_har.py -- UTF-8, filename sanitization, reformatted.

--- hamster/extract_har.py
+++ hamster/extract_har.py
@@ -1,60 +1,68 @@
 #!/usr/bin/env python
 
-from __future__ import  print_function
+from __future__ import print_function
 
 import argparse
 import base64
 import json
 import os
+import re
 
 parser = argparse.ArgumentParser()
-parser.add_argument('har_files', nargs='+')
+parser.add_argument("har_files", nargs="+")
 args = parser.parse_args()
+
 
 def unpack_single(har_filename):
     total_output = 0
 
-    with open(har_filename) as har_file:
+    with open(har_filename, encoding="utf-8") as har_file:
         har = json.load(har_file)
 
-    for i, entry in enumerate(har['log']['entries']):
-        filename = os.path.basename(entry['request']['url'])
+    for i, entry in enumerate(har["log"]["entries"]):
+        filename = os.path.basename(entry["request"]["url"])
         if not filename:
-            print('Bad filename in entry', i)
+            print("Bad filename in entry", i)
             continue
 
-        mime_type = entry['response']['content'].get('mimeType')
-        print('Entry', i, mime_type, filename, 'status', entry['response']['status'])
-        data = entry['response']['content'].get('text')
+        mime_type = entry["response"]["content"].get("mimeType")
+        print("Entry", i, mime_type, filename, "status", entry["response"]["status"])
+        data = entry["response"]["content"].get("text")
 
         if not data:
-            print('No text in response')
+            print("No text in response")
             continue
 
-        if entry['response']['content'].get('encoding') == 'base64':
+        if entry["response"]["content"].get("encoding") == "base64":
             data = base64.b64decode(data)
         else:
             data = data.encode()
 
         assert type(data) is bytes
 
+        sanitized = re.sub(r'[\\/*?:"<>|]', "_", filename)
+        if sanitized != filename:
+            print("  Sanitized filename:", sanitized)
+            filename = sanitized
+
         if os.path.exists(filename):
-            print(filename, 'EXISTS! Not overwriting.')
+            print(filename, "EXISTS! Not overwriting.")
             continue
 
         try:
-            with open(filename, 'wb') as data_file:
+            with open(filename, "wb") as data_file:
                 data_file.write(data)
             total_output += len(data)
-            print('File {} - {:,} bytes written.'.format(filename, len(data)))
+            print("File {} - {:,} bytes written.".format(filename, len(data)))
         except OSError:
-            print('Failed to write: {} - {:,} bytes.'.format(filename, len(data)))
+            print("Failed to write: {} - {:,} bytes.".format(filename, len(data)))
 
-    print('Done extracting:', har_filename, 'total: {:,} B.'.format(total_output))
+    print("Done extracting:", har_filename, "total: {:,} B.".format(total_output))
     return total_output
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     total = 0
     for har in args.har_files:
         total += unpack_single(har)
-    print('Finished. Extracted {:,} B'.format(total))
+    print("Finished. Extracted {:,} B".format(total))
