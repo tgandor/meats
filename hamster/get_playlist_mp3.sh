a4d90cc64a3aabfa0c94940ab0ecc0d3e6783f7b
tgandor <tomasz.gandor@gmail.com>
2025-09-12 01:33:41 +0200

    hamster/get_playlist_mp3.sh -- save metadata.

--- hamster/get_playlist_mp3.sh
+++ hamster/get_playlist_mp3.sh
@@ -9,5 +9,16 @@ else
     exit 1
 fi
 
-# to consider: --write-info-json
-$dl --extract-audio --audio-format mp3 --embed-thumbnail --output "%(playlist_index)02d-%(title)s.%(ext)s" "$@"
+$dl --write-info-json --write-description --windows-filenames --extract-audio --audio-format mp3 --embed-thumbnail --output "%(playlist_index)02d-%(title)s.%(ext)s" "$@"
+
+mkdir -p .meta
+mv *.description .meta
+if which jq >/dev/null 2>&1; then
+    find -name "*.info.json" | while read ij ; do
+	jq . "$ij" > ".meta/$ij"
+        rm "$ij"
+    done
+else
+    echo "Missing jq, no formatting of .info.json-s"
+    mv *.info.json .meta
+fi
