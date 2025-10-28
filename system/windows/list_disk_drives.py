4b5ca0eb437624a52eebe6c0f1c14b1e59496e99
tgandor <tomasz.gandor@gmail.com>
2025-10-28 13:12:10 +0100

    .\system\windows\list_disk_drives.py -- fix enumeration in pywin32.

--- system/windows/list_disk_drives.py
+++ system/windows/list_disk_drives.py
@@ -3,8 +3,8 @@ from __future__ import print_function
 import os
 import sys
 
-if os.name != 'nt':
-    print('This program requres Microsoft Windows')  # remember DOS?
+if os.name != "nt":
+    print("This program requres Microsoft Windows")  # remember DOS?
     exit()
 
 # some inspiration:
@@ -12,12 +12,17 @@ if os.name != 'nt':
 
 try:
     import win32api
-    for drive in win32api.GetLogicalDriveStrings():
+
+    drives_str = win32api.GetLogicalDriveStrings()
+    drives = drives_str.split("\x00")[:-1]
+
+    for drive in drives:
         print(drive)
 except ImportError:
-    print('Missing win32api', file=sys.stderr)
+    print("Missing win32api", file=sys.stderr)
     import string
+
     for letter in string.ascii_uppercase:
-        path = letter + ':\\'
+        path = letter + ":\\"
         if os.path.exists(path):
             print(path)
