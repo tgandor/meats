e0e93496d5c3adb37d5a5395b83b133dcaa82085
tgandor <tomasz.gandor@gmail.com>
2025-10-29 20:19:18 +0100

    .\system\windows\last_wakeup.py -- add sleep/wakeup events, not just deep wakeup.

--- system/windows/last_wakeup.py
+++ system/windows/last_wakeup.py
@@ -3,30 +3,36 @@ import json
 import subprocess
 
 ps = r"""
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8;
 Get-WinEvent -FilterHashtable @{
 LogName='System';
-ProviderName='Microsoft-Windows-Power-Troubleshooter';
-Id=1
+ProviderName='Microsoft-Windows-Power-Troubleshooter','Microsoft-Windows-Kernel-Power';
+Id=1,506,507
 } -MaxEvents {n} | Select-Object @{
 Name='TimeCreated';
 Expression={$_.TimeCreated.ToString('yyyy-MM-dd HH:mm:ss')}
-},Id,ProviderName | ConvertTo-Json -Depth 3
+},Id,Message | ConvertTo-Json -Depth 3
 """
+
+
+def _format_event(event):
+    message = " ".join(event["Message"].split())
+    print(f"ID: {event['Id']}, {event['TimeCreated']}, {message}")
 
 
 def powershell_fallback(n: int = 1):
     result = subprocess.run(
         ["powershell", "-NoProfile", "-Command", ps.replace("{n}", str(n))],
         capture_output=True,
-        text=True,
+        text=False,
         check=True,
     )
     data = json.loads(result.stdout)
     if isinstance(data, list):
         for event in data:
-            print(event)
+            _format_event(event)
     else:
-        print(data)
+        _format_event(data)
 
 
 def get_last_wakeup(n: int = 1):
@@ -52,10 +58,23 @@ def get_last_wakeup(n: int = 1):
                 event.EventID == 1
                 and event.SourceName == "Microsoft-Windows-Power-Troubleshooter"
             ):
-                print(f"Last Wakeup Time: {event.TimeGenerated.Format()}")  # type: ignore
+                print(f"Deep Sleep Wakeup: {event.TimeGenerated.Format()}")  # type: ignore
                 found += 1
-                if found == n:
-                    return
+            elif (
+                event.EventID == 507
+                and event.SourceName == "Microsoft-Windows-Kernel-Power"
+            ):
+                print(f"Wakeup Time: {event.TimeGenerated.Format()}")  # type: ignore
+                found += 1
+            elif (
+                event.EventID == 506
+                and event.SourceName == "Microsoft-Windows-Kernel-Power"
+            ):
+                print(f"Sleep Time: {event.TimeGenerated.Format()}")  # type: ignore
+                found += 1
+
+            if found == n:
+                return
     print("No wakeup event found.")
 
 
