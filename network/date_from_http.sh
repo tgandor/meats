8d9f4b959e2cfcba2a0cb3eb7dd1fd0f0020ba02
tgandor <tomasz.gandor@gmail.com>
2026-01-04 09:42:28 +0100

    network/date_from_http.sh -- check wget exit status.

--- network/date_from_http.sh
+++ network/date_from_http.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo "Usage: $0 [-s] URL"
-	exit
+    echo "Usage: $0 [-s] URL"
+    exit
 fi
 
 
 if [ "$2" == "-s" ]; then
-	# warm up sudo
-	sudo pwd > /dev/null
-	elevate=sudo
-	switch="-u -s"
+    # warm up sudo
+    sudo pwd > /dev/null
+    elevate=sudo
+    switch="-u -s"
 else
-	elevate=""
-	switch="-d"
+    elevate=""
+    switch="-d"
 fi
 
-utcdate=`wget -S --spider $1 2>&1 | awk '/Date:/ { sub(" *Date: +", ""); print; exit }'`
+if ! wget -S --spider $1 2>/tmp/timestamp ; then
+    echo "Error: no network?"
+    rm /tmp/timestamp
+    exit 1
+fi
+
+utcdate=`awk '/Date:/ { sub(" *Date: +", ""); print; exit }' /tmp/timestamp`
 
 echo "Local: "`date`
 $elevate date $switch "$utcdate"
+rm /tmp/timestamp
