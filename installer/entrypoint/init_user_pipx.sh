3f0b984e677a044822029f025d377d1f3c5ba916
tgandor <tgandor@users.noreply.github.com>
2026-04-21 14:12:13 +0200

    installer/entrypoint/init_user_pipx.sh -- make idempot.

--- installer/entrypoint/init_user_pipx.sh
+++ installer/entrypoint/init_user_pipx.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-python3 -m venv poet
-source poet/bin/activate
-pip install pipx
-pipx install poetry
-pipx install pipx
-pipx ensurepath
+if ! which pipx > /dev/null ; then
+    echo "pipx is not installed, starting setup"
+    python3 -m venv poet
+    source poet/bin/activate
+    pip install pipx
+    pipx install poetry
+    pipx install pipx
+    pipx ensurepath
+else
+    echo "pipx is already installed. (seems)"
+fi
 
 if [ ! -f $HOME/.gitconfig ] ; then
     echo "BTW, initializing $HOME/.gitconfig"
-    cp $(dirname "$0")/../configs/gitconfig $HOME/.gitconfig
+    cp $(dirname "$0")/../../configs/gitconfig $HOME/.gitconfig
 fi
