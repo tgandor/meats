c73d094126015b18f3ce6da4cbfbb5cda2babe1a
tgandor <tgandor@users.noreply.github.com>
2026-04-28 11:49:19 +0200

    media/any2mp3.sh - retabbed.

--- media/any2mp3.sh
+++ media/any2mp3.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 if which avconv ; then
-	conv=avconv
+    conv=avconv
 else
-	conv=ffmpeg
+    conv=ffmpeg
 fi
 
 if [ "$MP3" == "" ] ; then
     EXT=mp3
-	CODEC="-acodec libmp3lame -qscale:a 2"
+    CODEC="-acodec libmp3lame -qscale:a 2"
 else
     EXT=$MP3
 fi
 
 for f in "$@"; do
-	basename="${f%.*}"
-	new_name=${basename}.$EXT
-# this approach was limited:
-# new_name=`echo $f | sed 's/.ogg$\|.m4a$\|.flv$\|.mp4$\|.opus$\|.mkv$/.mp3/'`
-	if [ "$f" == "$new_name" ] ; then
-		echo "$f - target same as source"
-	else
-		echo "$f -> $new_name"
-		$conv -hide_banner -i "$f" -vn $CODEC -y "`basename "$new_name"`" 2>&1
-	fi
+    basename="${f%.*}"
+    new_name=${basename}.$EXT
+    # this approach was limited:
+    # new_name=`echo $f | sed 's/.ogg$\|.m4a$\|.flv$\|.mp4$\|.opus$\|.mkv$/.mp3/'`
+    if [ "$f" == "$new_name" ] ; then
+        echo "$f - target same as source"
+    else
+        echo "$f -> $new_name"
+        $conv -hide_banner -i "$f" -vn $CODEC -y "`basename "$new_name"`" 2>&1
+    fi
 done
