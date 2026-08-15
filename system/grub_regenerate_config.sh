539b2da5284de6d5e7884480610536b5c177fd5a
tgandor <tomasz.gandor@gmail.com>
2026-08-15 16:11:28 +0200

    system/grub_regenerate_config.sh -- more info, but probably worked before anyway.

--- system/grub_regenerate_config.sh
+++ system/grub_regenerate_config.sh
@@ -2,5 +2,15 @@
 
 # Thanks: https://superuser.com/a/1581482
 
-sudo cp -v /boot/grub/grub.cfg /boot/grub/grub.cfg-pre-`date +%Y-%m-%d_%H%M`
+old_cfg=/boot/grub/grub.cfg-pre-`date +%Y-%m-%d_%H%M`
+sudo cp -v /boot/grub/grub.cfg $old_cfg
+echo "Saved grub.cfg as $old_cfg"
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+read -rp "update-grub now? [y/N] " answer
+
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+    sudo update-grub
+else
+    echo "Run 'sudo update-grub' yourself (maybe mkconfig did it anyway)."
+fi
