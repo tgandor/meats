a42224a28eeec861a8a4dc2869fb1009097e5223
tgandor <tgandor@users.noreply.github.com>
2026-09-03 16:38:08 +0200

    hamster/wget_mirror.sh -- add wait and pass all args.

--- hamster/wget_mirror.sh
+++ hamster/wget_mirror.sh
@@ -3,5 +3,5 @@
 # Originally from here:
 # https://www.guyrutenberg.com/2014/05/02/make-offline-mirror-of-a-site-using-wget/
 
-# maybe some further tweaking will follo
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent $1
+# maybe some further tweaking will follow
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --wait=2 --random-wait "$@"
