5aec6e95539fd207457c1ab69501935ae1a9a3c4
tgandor <tomasz.gandor@gmail.com>
2026-09-08 09:30:30 +0200

    hamster/wget_mirror.sh -- restrict filenames to windows.

--- hamster/wget_mirror.sh
+++ hamster/wget_mirror.sh
@@ -4,4 +4,5 @@
 # https://www.guyrutenberg.com/2014/05/02/make-offline-mirror-of-a-site-using-wget/
 
 # maybe some further tweaking will follow
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --wait=2 --random-wait "$@"
+wget --mirror --convert-links --adjust-extension --page-requisites \
+    --no-parent --wait=2 --random-wait --restrict-file-names=windows "$@"
